32d86d5e7c066720218d8bd40a4d693a80667442
nichenqin <nichenqin@hotmail.com>
2022-11-19 20:50:08 +0800

    chore: add nano id value object

---
+++ packages/domain/id.vo.ts
@@ -0,0 +1,11 @@
+import { ValueObject } from './value-object'
+
+export abstract class ID extends ValueObject<string> {
+  constructor(value: string) {
+    super({ value })
+  }
+
+  public get value(): string {
+    return this.props.value
+  }
+}
