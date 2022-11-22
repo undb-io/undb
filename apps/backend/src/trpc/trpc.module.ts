35027e5f23ea6b6840cfd5b20a22bc6eae006158
nichenqin <nichenqin@hotmail.com>
2022-11-22 22:12:49 +0800

    chore: add some logging

--- apps/backend/src/trpc/trpc.module.ts
+++ apps/backend/src/trpc/trpc.module.ts
@@ -1,5 +1,5 @@
 import { swaggerServe } from '@egodb/trpc'
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
+import { Logger, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
 import { providers } from './providers'
 import { TrpcOpenApiMiddleware } from './trpc-open-api.middleware'
@@ -11,10 +11,27 @@ import { TrpcMiddleware } from './trpc.middleware'
   providers,
 })
 export class TrpcModule implements NestModule {
+  static TRPC_ENDPOINT = '/api/trpc'
+  static TRPC_OPEN_API_ENDPOINT = '/api'
+  static TRPC_API_DOCS_ENDPOINT = '/api-docs'
+
+  static logger = new Logger(TrpcModule.name)
+
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(TrpcMiddleware).forRoutes('/api/trpc')
-    consumer.apply(TrpcOpenApiMiddleware).forRoutes('/api')
-    consumer.apply(...swaggerServe).forRoutes('/api-docs')
-    consumer.apply(TrpcSwaggerUISetupMiddleware).forRoutes({ path: '/api-docs', method: RequestMethod.GET })
+    {
+      consumer.apply(TrpcMiddleware).forRoutes(TrpcModule.TRPC_ENDPOINT)
+      TrpcModule.logger.debug('Mounting Trpc middleware to ' + TrpcModule.TRPC_ENDPOINT)
+    }
+    {
+      consumer.apply(TrpcOpenApiMiddleware).forRoutes(TrpcModule.TRPC_OPEN_API_ENDPOINT)
+      TrpcModule.logger.debug('Mounting TrpcOpenApi middleware to ' + TrpcModule.TRPC_OPEN_API_ENDPOINT)
+    }
+    {
+      consumer.apply(...swaggerServe).forRoutes(TrpcModule.TRPC_API_DOCS_ENDPOINT)
+      consumer
+        .apply(TrpcSwaggerUISetupMiddleware)
+        .forRoutes({ path: TrpcModule.TRPC_API_DOCS_ENDPOINT, method: RequestMethod.GET })
+      TrpcModule.logger.debug('Mounting TrpcSwaggerUI middleware to ' + TrpcModule.TRPC_API_DOCS_ENDPOINT)
+    }
   }
 }
