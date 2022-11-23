c912e007c6f01cbbd867dc8d1236531e18bb2ebe
nichenqin <nichenqin@hotmail.com>
2022-11-23 18:31:42 +0800

    test: add vitest for lib testing

--- apps/backend/src/user-id.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserIdInterceptor } from './user-id.interceptor';
-
-describe('UserIdInterceptor', () => {
-  it('should be defined', () => {
-    expect(new UserIdInterceptor()).toBeDefined();
-  });
-});
