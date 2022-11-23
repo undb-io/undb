7d24d440f0ce65535bf3b56abf4056853e1f76e1
nichenqin <nichenqin@hotmail.com>
2022-11-23 09:55:16 +0800

    feat: create table form ui

---
+++ packages/core/commands/create-table/create-table.command.input.ts
@@ -0,0 +1,5 @@
+import * as z from 'zod'
+
+export const createTableCommandInput = z.object({
+  name: z.string().min(2),
+})
---
+++ packages/ui/theme.ts
@@ -0,0 +1,1 @@
+export { useMantineTheme as useEgoUITheme } from '@mantine/core'
