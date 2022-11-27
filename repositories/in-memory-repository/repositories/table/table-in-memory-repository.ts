d796df1ee9fe148c8f0f7a643e2564a612185b04
nichenqin <nichenqin@hotmail.com>
2022-11-27 21:51:07 +0800

    fix: fix in memory throw error

--- repositories/in-memory-repository/repositories/table/table-in-memory-repository.ts
+++ repositories/in-memory-repository/repositories/table/table-in-memory-repository.ts
@@ -38,14 +38,6 @@ export class TableInMemoryRepository implements ITableRepository {
   }
 
   async insert(table: Table): Promise<void> {
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        resolve(undefined)
-      }, 3000),
-    )
-
-    throw new Error('Ooops')
-
     db.data?.tables.push(TableInMemoryMapper.toInMemory(table))
   }
 }
