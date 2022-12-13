aba49cc06ecb10c9de18bcb0dc7aef2c7da8e676
Chenqin Nee <chenqinnee@ChenqindeMacBook-Pro.local>
2022-12-13 13:55:09 +0800

    chore: update dependencies version

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,14 +8,13 @@ declare const module: any
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true })
   app.useLogger(app.get(Logger))
-
   app.enableCors()
-
-  await app.listen(4000)
 
   if (module.hot) {
     module.hot.accept()
     module.hot.dispose(() => app.close())
   }
+
+  await app.listen(4000)
 }
 bootstrap()
