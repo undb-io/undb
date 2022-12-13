8f8b63523d49cfcbfc4a4c5bdd3b967e667c2d85
Chenqin Nee <chenqinnee@ChenqindeMacBook-Pro.local>
2022-12-13 22:33:44 +0800

    feat(core): filter record by view spec

--- packages/core/queries/get-records/get-records.query.handler.ts
+++ packages/core/queries/get-records/get-records.query.handler.ts
@@ -9,7 +9,12 @@ export class GetRecordsQueryHandler implements IQueryHandler<GetRecordsQuery, IG
   constructor(protected readonly tableRepo: ITableRepository, protected readonly rm: IRecordQueryModel) {}
 
   async execute(query: GetRecordsQuery): Promise<IGetRecordsOutput> {
-    const spec = WithRecordTableIdS(query.tableId).unwrap()
+    const table = (await this.tableRepo.findOneById(query.tableId)).unwrap()
+    const filter = table.getSpec(query.viewName)
+
+    const spec = WithRecordTableIdS(query.tableId)
+      .map((s) => (filter.isNone() ? s : s.and(filter.unwrap())))
+      .unwrap()
 
     const records = await this.rm.find(spec)
 
--- packages/core/queries/get-records/get-records.query.ts
+++ packages/core/queries/get-records/get-records.query.ts
@@ -1,10 +1,12 @@
 import { Query } from '@egodb/domain'
 import type { IGetRecordsQuery } from './get-records.query.interface'
 
-export class GetRecordsQuery extends Query {
+export class GetRecordsQuery extends Query implements IGetRecordsQuery {
   readonly tableId: string
+  readonly viewName?: string
   constructor(query: IGetRecordsQuery) {
     super()
     this.tableId = query.tableId
+    this.viewName = query.viewName
   }
 }
