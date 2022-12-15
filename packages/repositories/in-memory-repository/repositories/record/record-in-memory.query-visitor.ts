f830841cc5fe822255ed0725810b5f0d51b9e728
Chenqin Nee <chenqinnee@ChenqindeMacBook-Pro.local>
2022-12-15 16:03:59 +0800

    feat(core): record created at value use date field value

--- packages/repositories/in-memory-repository/repositories/record/record-in-memory.query-visitor.ts
+++ packages/repositories/in-memory-repository/repositories/record/record-in-memory.query-visitor.ts
@@ -143,7 +143,7 @@ export class RecordInMemoryQueryVisitor implements IRecordVisitor {
   createdAt(s: WithRecordCreatedAt): void {
     this.predicate = (r) => {
       const value = r.createdAt
-      return isDate(value) && isEqual(value, s.date.value)
+      return isDate(value) && isEqual(value, s.date.unpack())
     }
   }
 
