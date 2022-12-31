8238970be3de4eb97ffb9ad338f4c582c89f1a27
nichenqin <nichenqin@hotmail.com>
2022-12-31 16:52:42 +0800

    test(core): add create options schema test

--- packages/core/field/select.field.test.ts
+++ packages/core/field/select.field.test.ts
@@ -1,4 +1,6 @@
+import { SafeParseError } from 'zod'
 import { SelectField } from './select-field'
+import { createSelectFieldSchema, ICreateSelectFieldSchema } from './select-field.type'
 
 describe('SelectField', () => {
   describe('#create()', () => {
@@ -16,6 +18,34 @@ describe('SelectField', () => {
       expect(selectField.name.value).toBe('select')
       expect(selectField).toHaveProperty('options')
       expect(selectField.options.options).toHaveLength(1)
+    })
+  })
+
+  describe('createSelectFieldSchema', () => {
+    test('should return false if no options', () => {
+      const { success, error } = createSelectFieldSchema.safeParse({
+        id: '1',
+        name: 'name',
+        type: 'select',
+        options: [],
+      }) as SafeParseError<ICreateSelectFieldSchema>
+
+      expect(success).toBe(false)
+      expect(error).toMatchInlineSnapshot(`
+        [ZodError: [
+          {
+            "code": "too_small",
+            "minimum": 1,
+            "type": "array",
+            "inclusive": true,
+            "exact": false,
+            "message": "Array must contain at least 1 element(s)",
+            "path": [
+              "options"
+            ]
+          }
+        ]]
+      `)
     })
   })
 })
