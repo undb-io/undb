29dc4002fde93bc32952747cb129740c6e0e4341
nichenqin <nichenqin@hotmail.com>
2023-01-06 15:30:36 +0800

    style(web): kanban lane title select option stype

--- apps/web/components/kanban-ui/kanban-card.tsx
+++ apps/web/components/kanban-ui/kanban-card.tsx
@@ -1,6 +1,6 @@
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import { Card, Group } from '@egodb/ui'
+import { Card, Group, Stack } from '@egodb/ui'
 import type { SortableProps } from '../sortable.interface'
 import type { ITableBaseProps } from '../table/table-base-props'
 import type { Record } from '@egodb/core'
@@ -21,27 +21,29 @@ export const KanbanCard: React.FC<IProps & SortableProps> = ({
 }) => {
   return (
     <Card py="sm" shadow="xs" radius="sm" {...attributes} {...listeners} ref={setNodeRef} style={style}>
-      {Object.entries(record.values.valueJSON).map(([key, value]) => {
-        const field = table.schema.getField(key)
-        if (field.isNone()) return null
-        const f = field.unwrap()
-        if (f.type === 'select') {
-          const option = (value as SelectFieldValue).getOption(f).into()
-          if (!option) return null
+      <Stack spacing="xs">
+        {Object.entries(record.values.valueJSON).map(([key, value]) => {
+          const field = table.schema.getField(key)
+          if (field.isNone()) return null
+          const f = field.unwrap()
+          if (f.type === 'select') {
+            const option = (value as SelectFieldValue).getOption(f).into()
+            if (!option) return null
 
-          return (
-            <Group key={key}>
-              <Option
-                id={option.id.value}
-                name={option.name.value}
-                colorName={option.color.name}
-                shade={option.color.shade}
-              />
-            </Group>
-          )
-        }
-        return <Group key={key}>{value.unpack()?.toString()}</Group>
-      })}
+            return (
+              <Group key={key}>
+                <Option
+                  id={option.id.value}
+                  name={option.name.value}
+                  colorName={option.color.name}
+                  shade={option.color.shade}
+                />
+              </Group>
+            )
+          }
+          return <Group key={key}>{value.unpack()?.toString()}</Group>
+        })}
+      </Stack>
     </Card>
   )
 }
