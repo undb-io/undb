24879f1638221d11cd7d198607db564f0561d4a2
nichenqin <nichenqin@hotmail.com>
2023-01-07 12:44:14 +0800

    style(web): calendar day indicator position

--- apps/web/components/calendar-ui/day.tsx
+++ apps/web/components/calendar-ui/day.tsx
@@ -1,9 +1,8 @@
 import type { ICalendarField, Records } from '@egodb/core'
 import { DateEqual } from '@egodb/core'
-import { Box, Indicator, Space, Stack, Text } from '@egodb/ui'
+import { Box, Indicator, Stack, Text } from '@egodb/ui'
+import { isToday } from 'date-fns'
 import { useMemo } from 'react'
-
-const today = new Date()
 
 interface IProps {
   records: Records
@@ -25,37 +24,30 @@ export const Day: React.FC<IProps> = ({ date, field, records }) => {
         lineHeight: theme.fontSizes.md + 'px',
       })}
     >
-      <Indicator
-        dot
-        inline
-        position="top-end"
-        size={6}
-        color="red"
-        offset={-2}
-        disabled={date.getDate() !== today.getDate()}
-      >
-        <Box sx={{ textAlign: 'start' }}>{date.getDate()}</Box>
-        <Space h={3} />
-        <Stack spacing={5}>
-          {filteredRecords.map((r) => (
-            <Box
-              key={r.id.value}
-              bg="white"
-              px="xs"
-              w="100%"
-              sx={(theme) => ({
-                textAlign: 'start',
-                borderRadius: theme.radius.xs,
-                border: `1px ${theme.colors.gray[2]} solid`,
-                boxShadow: theme.shadows.xs,
-                lineHeight: theme.fontSizes.sm + 'px',
-              })}
-            >
-              <Text color="dark">{r.id.value}</Text>
-            </Box>
-          ))}
-        </Stack>
-      </Indicator>
+      <Box maw={10} sx={{ textAlign: 'start' }}>
+        <Indicator position="top-end" offset={-2} size={6} color="red" disabled={!isToday(date)}>
+          <span>{date.getDate()}</span>
+        </Indicator>
+      </Box>
+      <Stack spacing={5}>
+        {filteredRecords.map((r) => (
+          <Box
+            key={r.id.value}
+            bg="white"
+            px="xs"
+            w="100%"
+            sx={(theme) => ({
+              textAlign: 'start',
+              borderRadius: theme.radius.xs,
+              border: `1px ${theme.colors.gray[2]} solid`,
+              boxShadow: theme.shadows.xs,
+              lineHeight: theme.fontSizes.sm + 'px',
+            })}
+          >
+            <Text color="dark">{r.id.value}</Text>
+          </Box>
+        ))}
+      </Stack>
     </Stack>
   )
 }
