46c8e94ad718e035905bcb7c3167e8aa6594b44e
Sophie <wanshufen0414@gmail.com>
2023-01-09 15:48:11 +0800

    :sparkles:

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,12 +1,16 @@
 'use client'
-import { AppShell } from '@egodb/ui'
+import { AppShell, Button, IconPlus, Center } from '@egodb/ui'
+import { useAtom } from 'jotai'
 import { CreateTableFormDrawer } from '../components/create-table-form'
+import { createTableFormDrawerOpened } from '../components/create-table-form/drawer-opened.atom'
 import { TableNavList } from '../components/tables-nav-list/table-nav-list'
 import { AtomsDevtools } from './atom-devtool'
 import RootStyleRegistry from './emotion'
 import Trpc from './trpc'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [, setOpened] = useAtom(createTableFormDrawerOpened)
+  debugger
   return (
     <html lang="zh-CH">
       <head />
@@ -20,6 +24,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 sx={(theme) => ({ backgroundColor: theme.colors.gray[0] })}
               >
                 {children}
+                {!children?.props?.current && (
+                  <Center style={{ height: '100%' }}>
+                    <Button leftIcon={<IconPlus size={14} />} variant="outline" onClick={() => setOpened(true)}>
+                      New table
+                    </Button>
+                  </Center>
+                )}
                 <CreateTableFormDrawer />
               </AppShell>
             </Trpc>
