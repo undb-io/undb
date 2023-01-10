db16daf699f2b18c8358cd29cd6e56404dfcefaa
nichenqin <nichenqin@hotmail.com>
2023-01-10 19:20:33 +0800

    :fire: (sqlite) remove not used table in memory repository

--- apps/backend/src/modules/table/adapters/in-memory/table-in-memory.repository.ts
+++
@@ -1,9 +0,0 @@
-import { TableInMemoryRepository } from '@egodb/in-memory-repository'
-import { Injectable } from '@nestjs/common'
-
-@Injectable()
-export class NestTableInMemoryRepository extends TableInMemoryRepository {
-  constructor() {
-    super()
-  }
-}
