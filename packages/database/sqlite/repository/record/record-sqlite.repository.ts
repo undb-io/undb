72c3dda85392842060b4431608ce00e51bcf917e
nichenqin <nichenqin@hotmail.com>
2023-01-11 09:16:44 +0800

    :recycle: (sqlite) better record sqlite data transform

--- packages/database/sqlite/repository/record/record-sqlite.repository.ts
+++ packages/database/sqlite/repository/record/record-sqlite.repository.ts
@@ -2,21 +2,25 @@ import type { IRecordRepository, IRecordSpec, Record as CoreRecord, TableSchemaM
 import { DateRangeFieldValue } from '@egodb/core'
 import type { EntityManager } from '@mikro-orm/better-sqlite'
 import type { Option } from 'oxide.ts'
+import type { Primitive } from 'type-fest'
 
 export class RecordSqliteRepository implements IRecordRepository {
   constructor(protected readonly em: EntityManager) {}
 
   async insert(record: CoreRecord): Promise<void> {
     const knex = this.em.getKnex()
-    const data = [...record.values.value.entries()].reduce<Record<string, any>>((prev, [fieldId, value]) => {
-      if (value instanceof DateRangeFieldValue) {
-        prev[fieldId + '_from'] = value.unpack()?.[0]
-        prev[fieldId + '_to'] = value.unpack()?.[1]
-      } else {
-        prev[fieldId] = value.unpack()
-      }
-      return prev
-    }, {} as Record<string, any>)
+    const data = [...record.values.value.entries()].reduce<Record<string, Primitive | Date>>(
+      (prev, [fieldId, value]) => {
+        if (value instanceof DateRangeFieldValue) {
+          prev[fieldId + '_from'] = value.from.into()
+          prev[fieldId + '_to'] = value.to.into()
+        } else {
+          prev[fieldId] = value.unpack()
+        }
+        return prev
+      },
+      {} as Record<string, Primitive | Date>,
+    )
     data['id'] = record.id.value
     await knex.insert(data).into(record.tableId.value)
   }
