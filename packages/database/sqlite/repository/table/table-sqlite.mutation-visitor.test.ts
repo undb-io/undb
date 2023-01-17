ac468dc0046c22e3c0319fa13d8c6275bb5e9543
nichenqin <nichenqin@hotmail.com>
2023-01-17 12:32:31 +0800

    :white_check_mark: (sqlite) add table repository visitor tests

---
+++ packages/database/sqlite/repository/table/table-sqlite.mutation-visitor.test.ts
@@ -0,0 +1,90 @@
+import {
+  createTestTable,
+  IUnderlyingTableManager,
+  Table as CoreTable,
+  View as CoreView,
+  Views,
+  WithTableName,
+  WithTableSchema,
+  WithTableViews,
+} from '@egodb/core'
+import { EntityManager } from '@mikro-orm/better-sqlite'
+import { mock } from 'vitest-mock-extended'
+import { Field, Table } from '../../entity'
+import { View } from '../../entity/view'
+import { TableSqliteMutationVisitor } from './table-sqlite.mutation-visitor'
+import { TableSqliteRepository } from './table-sqlite.repository'
+
+describe('TableSqliteMutationVisitor', () => {
+  let table: CoreTable
+  let em: EntityManager
+  let mv: TableSqliteMutationVisitor
+  let repo: TableSqliteRepository
+
+  beforeAll(() => {
+    // @ts-expect-error
+    em = global.em
+  })
+
+  beforeEach(async () => {
+    table = createTestTable()
+    em = em.fork()
+    mv = new TableSqliteMutationVisitor(table.id.value, em)
+
+    const tm = mock<IUnderlyingTableManager>()
+    repo = new TableSqliteRepository(em, tm)
+
+    await repo.insert(table)
+  })
+
+  afterEach(async () => {
+    await em.nativeDelete(Table, {})
+  })
+
+  test('nameEqual', async () => {
+    mv.nameEqual(WithTableName.fromString('newname'))
+    await em.flush()
+
+    const found = await em.findOne(Table, { id: table.id.value })
+
+    expect(found).not.to.be.null
+    expect(found!.name).to.be.eq('newname')
+  })
+
+  test('schemaEqual', async () => {
+    mv.schemaEqual(WithTableSchema.from([{ id: 'fld1', name: 'field1', key: 'field1', type: 'string' }]))
+
+    await em.flush()
+
+    const field = await em.findOne(Field, { id: 'fld1' })
+    expect(field).not.to.be.null
+    expect(field!.id).to.be.eq('fld1')
+    expect(field!.name).to.be.eq('field1')
+    expect(field!.type).to.be.eq('string')
+  })
+
+  test('viewsEqual', async () => {
+    mv.viewsEqual(
+      new WithTableViews(
+        new Views([CoreView.create({ id: 'viw1', name: 'view1', key: 'view1', displayType: 'grid' })]),
+      ),
+    )
+
+    await em.flush()
+
+    const view = await em.findOne(View, { id: 'viw1' })
+    expect(view).not.to.be.null
+    expect(view!.id).to.be.eq('viw1')
+    expect(view!.name).to.be.eq('view1')
+    expect(view!.key).to.be.eq('view1')
+    expect(view!.displayType).to.be.eq('grid')
+    expect(view!.kanban).to.be.undefined
+    expect(view!.calendar).to.be.undefined
+    expect(view!.filter).to.be.null
+    expect(view!.fieldOptions).to.be.null
+    expect(view!.fieldsOrder).to.be.undefined
+    expect(view!.createdAt).to.be.instanceOf(Date)
+    expect(view!.updatedAt).to.be.instanceOf(Date)
+    expect(view!.deletedAt).to.be.undefined
+  })
+})
---
+++ packages/database/sqlite/repository/table/table-sqlite.query-visitor.test.ts
@@ -0,0 +1,41 @@
+import { WithTableId, WithTableName } from '@egodb/core'
+import { EntityManager, QueryBuilder } from '@mikro-orm/better-sqlite'
+import { Table } from '../../entity'
+import { TableSqliteQueryVisitor } from './table-sqlite.query-visitor'
+
+describe('TableSqliteQueryVisitor', () => {
+  let em: EntityManager
+  let qb: QueryBuilder<Table>
+  let qv: TableSqliteQueryVisitor
+
+  beforeEach(() => {
+    // @ts-expect-error
+    em = global.em.fork()
+
+    qb = em.qb(Table)
+
+    qv = new TableSqliteQueryVisitor(qb)
+  })
+
+  test('idEqual', async () => {
+    qv.idEqual(WithTableId.fromString('someid'))
+
+    expect(qb.getQuery()).toMatchInlineSnapshot('"select `t0`.* from `table` as `t0` where `t0`.`id` = ?"')
+    expect(qb.getParams()).toMatchInlineSnapshot(`
+      [
+        "someid",
+      ]
+    `)
+  })
+
+  test('nameEqual', async () => {
+    qv.nameEqual(WithTableName.fromString('name'))
+
+    expect(qb.getQuery()).toMatchInlineSnapshot('"select `t0`.* from `table` as `t0` where `t0`.`name` = ?"')
+    expect(qb.getParams()).toMatchInlineSnapshot(`
+      [
+        "name",
+      ]
+    `)
+  })
+})
