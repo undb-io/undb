53d95ee2c770bba2692adf8522de71850fed4bd4
nichenqin <nichenqin@hotmail.com>
2023-01-21 19:12:01 +0800

    :bug: (core) view sort should be array

--- packages/core/view/sort/sort.schema.ts
+++ packages/core/view/sort/sort.schema.ts
@@ -5,6 +5,11 @@ export const sortDirection = z.enum(['asc', 'desc'])
 
 export type ISortDirection = z.infer<typeof sortDirection>
 
-export const sortsSchema = z.record(fieldIdSchema, sortDirection)
+export const sortSchema = z.object({
+  fieldId: fieldIdSchema,
+  direction: sortDirection,
+})
+
+export const sortsSchema = z.array(sortSchema)
 
 export type ISorts = z.infer<typeof sortsSchema>
