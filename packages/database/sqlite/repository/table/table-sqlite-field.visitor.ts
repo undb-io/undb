f40cdcbabc5d9ec637e7b9b3cc2218ffdd2679ee
nichenqin <nichenqin@hotmail.com>
2023-01-22 17:31:59 +0800

    :bug: (sqlite) initialize closure table data when create new tree field
    Closes #309

--- packages/database/sqlite/repository/table/table-sqlite-field.visitor.ts
+++ packages/database/sqlite/repository/table/table-sqlite-field.visitor.ts
@@ -9,6 +9,7 @@ import type {
   StringField as CoreStringField,
   TreeField as CoreTreeField,
 } from '@egodb/core'
+import { INTERNAL_COLUMN_ID_NAME } from '@egodb/core'
 import type { EntityManager } from '@mikro-orm/better-sqlite'
 import { wrap } from '@mikro-orm/core'
 import type { Table } from '../../entity'
@@ -27,10 +28,14 @@ import { UnderlyingAdjacencyListTable, UnderlyingClosureTable } from '../../unde
 
 export class TableSqliteFieldVisitor implements IFieldVisitor {
   constructor(private readonly table: Table, private readonly em: EntityManager) {}
-  private queries: string[] = []
+  #queries: string[] = []
+
+  public get queries() {
+    return this.#queries
+  }
 
   public async commit(): Promise<void> {
-    for (const query of this.queries) {
+    for (const query of this.#queries) {
       await this.em.execute(query)
     }
   }
@@ -79,16 +84,33 @@ export class TableSqliteFieldVisitor implements IFieldVisitor {
 
     const queries = adjacencyListTable.getSqls(this.em.getKnex())
 
-    this.queries.push(...queries)
+    this.#queries.push(...queries)
   }
 
   tree(value: CoreTreeField): void {
     const field = new TreeField(this.table, value)
     this.em.persist(field)
 
-    const closureTable = new UnderlyingClosureTable(this.table.id, value)
+    const tableId = this.table.id
+    const closureTable = new UnderlyingClosureTable(tableId, value)
 
-    const queries = closureTable.getSqls(this.em.getKnex())
-    this.queries.push(...queries)
+    const knex = this.em.getKnex()
+
+    const queries = closureTable.getSqls(knex)
+    this.#queries.push(...queries)
+
+    const insert = knex
+      .insert(
+        knex
+          .select([
+            `${INTERNAL_COLUMN_ID_NAME} as ${UnderlyingClosureTable.CLOSURE_TABLE_CHILD_ID_FIELD}`,
+            `${INTERNAL_COLUMN_ID_NAME} as ${UnderlyingClosureTable.CLOSURE_TABLE_PARENT_ID_FIELD}`,
+            knex.raw('? as ??', [0, UnderlyingClosureTable.CLOSURE_TABLE_DEPTH_FIELD]),
+          ])
+          .from(tableId),
+      )
+      .into(closureTable.name)
+      .toQuery()
+    this.#queries.push(insert)
   }
 }
