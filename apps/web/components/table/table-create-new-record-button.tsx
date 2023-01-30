d84867b1519bcb3204f339433cc8186a76222eae
nichenqin <nichenqin@hotmail.com>
2023-01-30 20:38:48 +0800

    :bug: (web) fix tree view item create record

--- apps/web/components/table/table-create-new-record-button.tsx
+++ apps/web/components/table/table-create-new-record-button.tsx
@@ -1,12 +1,15 @@
 import { Button, IconRowInsertBottom } from '@egodb/ui'
 import { useSetAtom } from 'jotai'
 import { unstable_batchedUpdates } from 'react-dom'
+import { createRecordInitialValueAtom } from '../create-record-form/create-record-initial-value.atom'
 import { createRecordFormDrawerOpened } from '../create-record-form/drawer-opened.atom'
 import { editRecordFormDrawerOpened } from '../edit-record-form/drawer-opened.atom'
 
 export const TableCreateNewRecordButton: React.FC = () => {
   const setOpened = useSetAtom(createRecordFormDrawerOpened)
   const setEditRecordOpened = useSetAtom(editRecordFormDrawerOpened)
+  const setCreateRecordInitialValue = useSetAtom(createRecordInitialValueAtom)
+
   return (
     <Button
       compact
@@ -14,6 +17,7 @@ export const TableCreateNewRecordButton: React.FC = () => {
       leftIcon={<IconRowInsertBottom size={14} />}
       onClick={() => {
         unstable_batchedUpdates(() => {
+          setCreateRecordInitialValue({})
           setEditRecordOpened(false)
           setOpened(true)
         })
