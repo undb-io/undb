e400c3402b6294c56e95f217134b0dedcd6fb68b
nichenqin <nichenqin@hotmail.com>
2023-02-16 20:05:47 +0800

    :sparkles: (web) add views order dnd

--- apps/web/components/views/views-list-item.tsx
+++ apps/web/components/views/views-list-item.tsx
@@ -1,3 +1,5 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import type { View } from '@egodb/core'
 import { useDeleteViewMutation, useDuplicateViewMutation, useUpdateViewNameMutation } from '@egodb/store'
 import {
@@ -10,6 +12,8 @@ import {
   FocusTrap,
   useHover,
   IconChevronDown,
+  IconGripVertical,
+  Box,
 } from '@egodb/ui'
 import { useSetAtom } from 'jotai'
 import { useRouter } from 'next/navigation'
@@ -19,7 +23,11 @@ import { useCurrentView } from '../../hooks/use-current-view'
 import { getDisplayTypeColor, DisplayTypeIcon } from '../view/display-type-icon'
 import { viewsOpenedAtom } from './views-opened.atom'
 
-export const ViewsListItem: React.FC<{ v: View }> = ({ v }) => {
+export interface IProps {
+  v: View
+}
+
+export const ViewsListItem: React.FC<IProps> = ({ v }) => {
   const table = useCurrentTable()
   const viewCount = table.views.count
   const view = useCurrentView()
@@ -43,105 +51,117 @@ export const ViewsListItem: React.FC<{ v: View }> = ({ v }) => {
     },
   })
 
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: v.id.value })
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
   return (
-    <Group
-      noWrap
-      position="apart"
-      p="xs"
-      ref={ref}
-      bg={isActive ? 'blue.0' : ''}
-      key={v.id.value}
-      onClick={() => {
-        router.push(`/t/${table.id.value}/${v.id.value}`)
-        setOpened(false)
-      }}
-      sx={(theme) => ({
-        cursor: 'pointer',
-        ':hover': {
-          backgroundColor: !isActive ? theme.colors.gray[0] : theme.colors.blue[1],
-        },
-      })}
-    >
-      <Group>
-        <ActionIcon variant="filled" color={getDisplayTypeColor(v.displayType)} size="sm">
-          <DisplayTypeIcon displayType={v.displayType} size={20} />
-        </ActionIcon>
-        {isEditing ? (
-          <FocusTrap>
-            <TextInput
-              onBlur={(e) => {
-                updateViewName({
-                  tableId: table.id.value,
-                  view: { id: v.id.value, name: e.target.value },
-                })
-                setOpened(false)
-              }}
-              placeholder={v.name.value}
-              size="xs"
-              lh={1}
-              onClick={(e) => e.stopPropagation()}
-              variant="unstyled"
-            />
-          </FocusTrap>
-        ) : (
-          <Text fz="sm" color="gray.9">
-            {v.name.value}
-          </Text>
+    <Box ref={setNodeRef} style={style}>
+      <Group
+        noWrap
+        position="apart"
+        p="xs"
+        ref={ref}
+        bg={isActive ? 'blue.0' : ''}
+        key={v.id.value}
+        onClick={() => {
+          router.push(`/t/${table.id.value}/${v.id.value}`)
+          setOpened(false)
+        }}
+        sx={(theme) => ({
+          cursor: 'pointer',
+          ':hover': {
+            backgroundColor: !isActive ? theme.colors.gray[0] : theme.colors.blue[1],
+          },
+        })}
+      >
+        <Group>
+          <ActionIcon variant="filled" color={getDisplayTypeColor(v.displayType)} size="sm">
+            <DisplayTypeIcon displayType={v.displayType} size={20} />
+          </ActionIcon>
+          {isEditing ? (
+            <FocusTrap>
+              <TextInput
+                onBlur={(e) => {
+                  updateViewName({
+                    tableId: table.id.value,
+                    view: { id: v.id.value, name: e.target.value },
+                  })
+                  setOpened(false)
+                }}
+                placeholder={v.name.value}
+                size="xs"
+                lh={1}
+                onClick={(e) => e.stopPropagation()}
+                variant="unstyled"
+              />
+            </FocusTrap>
+          ) : (
+            <Text fz="sm" color="gray.9">
+              {v.name.value}
+            </Text>
+          )}
+        </Group>
+        {hovered && (
+          <Group>
+            <Menu>
+              <Menu.Target>
+                <ActionIcon
+                  color="gray.4"
+                  size="xs"
+                  variant="filled"
+                  radius="xl"
+                  onClick={(e) => {
+                    e.stopPropagation()
+                  }}
+                >
+                  <IconChevronDown fontWeight={600} size={14} />
+                </ActionIcon>
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Item
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    handler.open()
+                  }}
+                >
+                  Update View Name
+                </Menu.Item>
+                <Menu.Item
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    duplicateView({ tableId: table.id.value, id: v.id.value })
+                  }}
+                >
+                  Duplicate View
+                </Menu.Item>
+
+                {viewCount > 1 && (
+                  <>
+                    <Menu.Divider />
+
+                    <Menu.Item
+                      color="red"
+                      onClick={(e) => {
+                        e.stopPropagation()
+                        confirm()
+                      }}
+                    >
+                      Delete View
+                    </Menu.Item>
+                  </>
+                )}
+              </Menu.Dropdown>
+            </Menu>
+
+            <ActionIcon {...attributes} {...listeners} size="xs" component="a">
+              <IconGripVertical size={12} />
+            </ActionIcon>
+          </Group>
         )}
       </Group>
-      {hovered && (
-        <Group>
-          <Menu>
-            <Menu.Target>
-              <ActionIcon
-                color="gray.4"
-                size="xs"
-                variant="filled"
-                radius="xl"
-                onClick={(e) => {
-                  e.stopPropagation()
-                }}
-              >
-                <IconChevronDown fontWeight={600} size={14} />
-              </ActionIcon>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item
-                onClick={(e) => {
-                  e.stopPropagation()
-                  handler.open()
-                }}
-              >
-                Update View Name
-              </Menu.Item>
-              <Menu.Item
-                onClick={(e) => {
-                  e.stopPropagation()
-                  duplicateView({ tableId: table.id.value, id: v.id.value })
-                }}
-              >
-                Duplicate View
-              </Menu.Item>
-
-              {viewCount > 1 && (
-                <>
-                  <Menu.Divider />
-
-                  <Menu.Item
-                    color="red"
-                    onClick={(e) => {
-                      e.stopPropagation()
-                      confirm()
-                    }}
-                  >
-                    Delete View
-                  </Menu.Item>
-                </>
-              )}
-            </Menu.Dropdown>
-          </Menu>
-        </Group>
-      )}
-    </Group>
+    </Box>
   )
 }
--- apps/web/components/views/views-list.tsx
+++ apps/web/components/views/views-list.tsx
@@ -1,4 +1,7 @@
-import { Button, openContextModal, Stack } from '@egodb/ui'
+import { closestCenter, DndContext } from '@dnd-kit/core'
+import { restrictToVerticalAxis } from '@dnd-kit/modifiers'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
+import { Button, openContextModal, Stack, useListState } from '@egodb/ui'
 import { useCurrentTable } from '../../hooks/use-current-table'
 import { CREATE_VIEW_MODAL_ID } from '../../modals'
 import { ViewsListItem } from './views-list-item'
@@ -7,12 +10,31 @@ export const ViewsList: React.FC = () => {
   const table = useCurrentTable()
   const views = table.views.views
 
+  const viewsOrder = table.viewsOrder.order
+  const [order, handlers] = useListState(viewsOrder)
+
   return (
     <Stack h="100%" justify="space-between">
       <Stack spacing={5}>
-        {views.map((v) => (
-          <ViewsListItem key={v.id.value} v={v} />
-        ))}
+        <DndContext
+          collisionDetection={closestCenter}
+          modifiers={[restrictToVerticalAxis]}
+          onDragEnd={(e) => {
+            const { over, active } = e
+            if (over) {
+              handlers.reorder({
+                from: active.data.current?.sortable?.index,
+                to: over?.data.current?.sortable?.index,
+              })
+            }
+          }}
+        >
+          <SortableContext items={order} strategy={verticalListSortingStrategy}>
+            {views.map((v) => (
+              <ViewsListItem key={v.id.value} v={v} />
+            ))}
+          </SortableContext>
+        </DndContext>
       </Stack>
       <Button
         variant="light"
