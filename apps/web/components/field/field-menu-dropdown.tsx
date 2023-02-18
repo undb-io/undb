7af3f82aa4db22a0b722780bca8ccba798486fe7
nichenqin <nichenqin@hotmail.com>
2023-02-18 14:32:42 +0800

    :lipstick: (fix) fix update field inside portal

--- apps/web/components/field/field-menu-dropdown.tsx
+++ apps/web/components/field/field-menu-dropdown.tsx
@@ -1,6 +1,7 @@
 import type { Field } from '@egodb/core'
 import { useDeleteFieldMutation } from '@egodb/store'
 import type { MenuItemProps } from '@egodb/ui'
+import { Portal } from '@egodb/ui'
 import { openContextModal } from '@egodb/ui'
 import { IconPencil, IconTrash, Menu } from '@egodb/ui'
 import { useConfirmModal } from '../../hooks'
@@ -27,26 +28,28 @@ export const FieldMenuDropdown: React.FC<{ field: Field }> = ({ field }) => {
   }
 
   return (
-    <Menu.Dropdown>
-      <Menu.Item
-        icon={<IconPencil size={14} />}
-        {...menuProps}
-        onClick={() =>
-          openContextModal({
-            title: 'Update Field',
-            modal: UPDATE_FIELD_MODAL_ID,
-            innerProps: { field },
-          })
-        }
-      >
-        Update Field
-      </Menu.Item>
+    <Portal>
+      <Menu.Dropdown>
+        <Menu.Item
+          icon={<IconPencil size={14} />}
+          {...menuProps}
+          onClick={() =>
+            openContextModal({
+              title: 'Update Field',
+              modal: UPDATE_FIELD_MODAL_ID,
+              innerProps: { field },
+            })
+          }
+        >
+          Update Field
+        </Menu.Item>
 
-      <Menu.Divider />
+        <Menu.Divider />
 
-      <Menu.Item icon={<IconTrash size={14} />} {...menuProps} color="red" onClick={confirm}>
-        Delete Field
-      </Menu.Item>
-    </Menu.Dropdown>
+        <Menu.Item icon={<IconTrash size={14} />} {...menuProps} color="red" onClick={confirm}>
+          Delete Field
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Portal>
   )
 }
