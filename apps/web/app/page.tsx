c8d37366e5b2497484d69beadb74f02940c5ece4
nichenqin <nichenqin@hotmail.com>
2023-02-22 09:41:29 +0800

    :sparkles: (web) redirect to last selected table

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,19 @@
+'use client'
+
+import { getCurrentTableId } from '@egodb/store'
+import { redirect } from 'next/navigation'
+import { useEffect } from 'react'
+import { useAppSelector } from '../hooks'
 import App from './App'
 
 export default function Page() {
+  const currentTableId = useAppSelector(getCurrentTableId)
+
+  useEffect(() => {
+    if (currentTableId) {
+      redirect(`/t/${currentTableId}`)
+    }
+  }, [currentTableId])
+
   return <App />
 }
