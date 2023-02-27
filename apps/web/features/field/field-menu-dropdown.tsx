cb76efd5970830ac615a101040c3561acc4c443f
nichenqin <nichenqin@hotmail.com>
2023-02-27 13:49:38 +0800

    :sparkles: (web) add hide field menu item

--- apps/web/features/field/field-menu-dropdown.tsx
+++ apps/web/features/field/field-menu-dropdown.tsx
@@ -1,16 +1,21 @@
 import type { Field } from '@egodb/core'
-import { useDeleteFieldMutation } from '@egodb/store'
+import { useDeleteFieldMutation, useSetVisibilityMutation } from '@egodb/store'
 import type { MenuItemProps } from '@egodb/ui'
+import { IconEye } from '@egodb/ui'
 import { Portal } from '@egodb/ui'
 import { openContextModal } from '@egodb/ui'
 import { IconPencil, IconTrash, Menu } from '@egodb/ui'
 import { useConfirmModal } from '../../hooks'
 import { useCurrentTable } from '../../hooks/use-current-table'
+import { useCurrentView } from '../../hooks/use-current-view'
 import { UPDATE_FIELD_MODAL_ID } from '../../modals'
 
 export const FieldMenuDropdown: React.FC<{ field: Field }> = ({ field }) => {
   const table = useCurrentTable()
+  const view = useCurrentView()
   const [deleteField] = useDeleteFieldMutation()
+
+  const [setVisibility] = useSetVisibilityMutation()
 
   const confirm = useConfirmModal({
     onConfirm() {
@@ -44,14 +49,22 @@ export const FieldMenuDropdown: React.FC<{ field: Field }> = ({ field }) => {
           Update Field
         </Menu.Item>
 
-        {!field.system && (
-          <>
-            <Menu.Divider />
+        <Menu.Divider />
 
-            <Menu.Item icon={<IconTrash size={14} />} {...menuProps} color="red" onClick={confirm}>
-              Delete Field
-            </Menu.Item>
-          </>
+        <Menu.Item
+          icon={<IconEye size={14} />}
+          {...menuProps}
+          onClick={() =>
+            setVisibility({ tableId: table.id.value, viewId: view.id.value, fieldId: field.id.value, hidden: true })
+          }
+        >
+          Hide Field
+        </Menu.Item>
+
+        {!field.system && (
+          <Menu.Item icon={<IconTrash size={14} />} {...menuProps} color="red" onClick={confirm}>
+            Delete Field
+          </Menu.Item>
         )}
       </Menu.Dropdown>
     </Portal>
