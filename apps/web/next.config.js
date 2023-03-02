2e2ba107cf017476324715f3eb504646b519eab3
nichenqin <nichenqin@hotmail.com>
2023-03-02 13:58:44 +0800

    :whale: optimize image size

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   transpilePackages: ['@egodb/ui', '@egodb/store', '@egodb/domain', '@egodb/core', '@egodb/cqrs'],
   experimental: {
     appDir: true,
+    outputFileTracingRoot: path.join(__dirname, '../../'),
   },
 }
 
