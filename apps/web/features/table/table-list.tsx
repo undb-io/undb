6e50f2df14dbcba5813ab70c042923d28c317abf
nichenqin <nichenqin@hotmail.com>
2023-03-07 12:55:07 +0800

    :art: (web) add update table form drawer

--- apps/web/features/table/table-list.tsx
+++ apps/web/features/table/table-list.tsx
@@ -1,9 +1,13 @@
+import { TableFactory } from '@egodb/core'
 import { getCurrentTableId, useGetTablesQuery } from '@egodb/store'
-import { ActionIcon, Center, Flex, IconPlus, Tabs } from '@egodb/ui'
+import { ActionIcon, Center, Flex, IconChevronDown, IconPlus, Menu, Tabs } from '@egodb/ui'
 import { useSetAtom } from 'jotai'
 import { useRouter } from 'next/navigation'
+import { CurrentTableContext } from '../../context/current-table'
 import { useAppSelector } from '../../hooks'
 import { createTableFormDrawerOpened } from '../create-table-form/drawer-opened.atom'
+import { UpdateTableFormDrawer } from '../update-table-form/update-table-form-drawer'
+import { TableMenuDropdown } from './table-menu-dropdown'
 
 export const TableList: React.FC = () => {
   const router = useRouter()
@@ -17,7 +21,7 @@ export const TableList: React.FC = () => {
     <Flex>
       <Center>
         <Tabs
-          variant="outline"
+          variant="default"
           display="flex"
           value={currentTableId}
           onTabChange={(value) => router.push(`/t/${value}`)}
@@ -25,7 +29,31 @@ export const TableList: React.FC = () => {
           {Object.values(tables.data?.entities ?? {})
             .filter(Boolean)
             .map((t) => (
-              <Tabs.Tab key={t.id} value={t.id}>
+              <Tabs.Tab
+                key={t.id}
+                value={t.id}
+                p="xs"
+                rightSection={
+                  t.id === currentTableId && (
+                    <>
+                      <Menu withinPortal width={200} shadow="xl">
+                        <Menu.Target>
+                          <ActionIcon size="xs">
+                            <IconChevronDown />
+                          </ActionIcon>
+                        </Menu.Target>
+
+                        <Menu.Dropdown>
+                          <TableMenuDropdown />
+                        </Menu.Dropdown>
+                      </Menu>
+                      <CurrentTableContext.Provider value={TableFactory.fromQuery(t)}>
+                        <UpdateTableFormDrawer />
+                      </CurrentTableContext.Provider>
+                    </>
+                  )
+                }
+              >
                 {t.name}
               </Tabs.Tab>
             ))}
--- apps/web/features/update-table-form/update-table-form.tsx
+++ apps/web/features/update-table-form/update-table-form.tsx
@@ -1,6 +1,6 @@
 import type { IUpdateTableSchema } from '@egodb/core'
 import { useUpdateTableMutation } from '@egodb/store'
-import { Alert, Button, Divider, Group, IconAlertCircle, Stack, Text, TextInput } from '@egodb/ui'
+import { Alert, Box, Button, Divider, Group, IconAlertCircle, Stack, Text, TextInput } from '@egodb/ui'
 import { useFormContext } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { useCurrentTable } from '../../hooks/use-current-table'
@@ -47,21 +47,29 @@ export const UpdateTableForm: React.FC<IProps> = ({ onCancel, onSuccess: success
         />
 
         <Divider />
-
-        <Group position="right">
-          <Button variant="subtle" onClick={() => onCancel()}>
-            {t('Cancel', { ns: 'common' })}
-          </Button>
-          <Button loading={isLoading} miw={200} disabled={disabled} type="submit">
-            {t('Update', { ns: 'common' })}
-          </Button>
-        </Group>
-
         {isError && (
           <Alert color="red" icon={<IconAlertCircle size={16} />} title="Oops! Create Table Error!" mt="lg">
             {(error as any).message}
           </Alert>
         )}
+        <Box
+          pos="fixed"
+          bottom={0}
+          right={0}
+          w="100%"
+          p="md"
+          bg="white"
+          sx={(theme) => ({ zIndex: 1000, borderTop: '1px solid ' + theme.colors.gray[2] })}
+        >
+          <Group position="right">
+            <Button variant="subtle" onClick={() => onCancel()}>
+              {t('Cancel', { ns: 'common' })}
+            </Button>
+            <Button loading={isLoading} miw={200} disabled={disabled} type="submit">
+              {t('Update', { ns: 'common' })}
+            </Button>
+          </Group>
+        </Box>
       </Stack>
     </form>
   )
