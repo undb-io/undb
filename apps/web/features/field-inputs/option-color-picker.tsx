83304aef0f1a84bbad9e17c6260a5df8b0861c1d
nichenqin <nichenqin@hotmail.com>
2023-03-07 13:15:13 +0800

    :bug: (web) render color picker dropdown in portal

--- apps/web/features/field-inputs/option-color-picker.tsx
+++ apps/web/features/field-inputs/option-color-picker.tsx
@@ -1,8 +1,7 @@
 import { optionColorOrder } from '@egodb/core'
-import { ActionIcon, IconCircleChevronDown, Popover, SimpleGrid, UnstyledButton } from '@egodb/ui'
+import { ActionIcon, IconCircleChevronDown, Popover, SimpleGrid, UnstyledButton, useDisclosure } from '@egodb/ui'
 import { OptionColor } from '@egodb/core'
 import { Option } from '../option/option'
-import { useState } from 'react'
 import type { OnColorChange } from './type'
 import type { IMutateOptionSchema } from '@egodb/core'
 
@@ -12,15 +11,15 @@ export interface IOptionColorPickerProps {
 }
 
 export const OptionColorPicker: React.FC<IOptionColorPickerProps> = ({ option, onChange }) => {
-  const [opened, setOpened] = useState(false)
+  const [opened, handler] = useDisclosure(false)
 
   return (
-    <Popover position="bottom-start" opened={opened} onChange={setOpened}>
+    <Popover withinPortal position="bottom-start" opened={opened} onChange={handler.toggle}>
       <Popover.Target>
         <ActionIcon
           size="sm"
           variant="filled"
-          onClick={() => setOpened(true)}
+          onClick={handler.toggle}
           color={`${option.color?.name}.${option.color?.shade}`}
         >
           <IconCircleChevronDown size={14} />
@@ -31,9 +30,10 @@ export const OptionColorPicker: React.FC<IOptionColorPickerProps> = ({ option, o
         <SimpleGrid cols={2}>
           {optionColorOrder.map((color) => (
             <UnstyledButton
+              key={color}
               onClick={() => {
                 onChange({ name: color, shade: option.color?.shade ?? OptionColor.defaultShade })
-                setOpened(false)
+                handler.close()
               }}
             >
               <Option
