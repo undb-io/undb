229f16ad2ef5d2bbf030b4925d9c20d099216bca
nichenqin <nichenqin@hotmail.com>
2023-03-08 13:27:50 +0800

    :children_crossing: (web) expanded button styles

--- apps/web/features/table/views-button.tsx
+++ apps/web/features/table/views-button.tsx
@@ -1,11 +1,11 @@
 import { Button, IconMenu2, Tooltip } from '@egodb/ui'
-import { useSetAtom } from 'jotai'
+import { useAtom } from 'jotai'
 import { useTranslation } from 'react-i18next'
 import { useCurrentTable } from '../../hooks/use-current-table'
 import { viewsOpenedAtom } from '../views/views-opened.atom'
 
 export const ViewsButton: React.FC = () => {
-  const setOpened = useSetAtom(viewsOpenedAtom)
+  const [opened, setOpened] = useAtom(viewsOpenedAtom)
   const table = useCurrentTable()
   const { t } = useTranslation()
 
@@ -13,7 +13,16 @@ export const ViewsButton: React.FC = () => {
 
   return (
     <Tooltip label={viewsCount > 1 ? `${viewsCount} views` : `${viewsCount} view`}>
-      <Button compact size="xs" leftIcon={<IconMenu2 size={14} />} variant="subtle" onClick={() => setOpened(true)}>
+      <Button
+        compact
+        size="xs"
+        leftIcon={<IconMenu2 size={14} />}
+        variant="subtle"
+        onClick={() => setOpened(true)}
+        sx={(theme) => ({
+          backgroundColor: opened ? theme.colors[theme.primaryColor][0] : 'initial',
+        })}
+      >
         {t('Views')}
       </Button>
     </Tooltip>
