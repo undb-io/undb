ebb66a00f8b6a0999922f3d5e86221359204f8f2
nichenqin <nichenqin@hotmail.com>
2023-03-08 10:28:32 +0800

    :sparkles: (sqlite) get total records count

--- packages/cqrs/src/queries/get-records/get-records.query.output.ts
+++ packages/cqrs/src/queries/get-records/get-records.query.output.ts
@@ -3,4 +3,5 @@ import * as z from 'zod'
 
 export const getRecordsQueryOutput = z.object({
   records: z.array(queryRecordSchema),
+  total: z.number().nonnegative().int(),
 })
--- packages/database/sqlite/src/repository/record/record.constants.ts
+++ packages/database/sqlite/src/repository/record/record.constants.ts
@@ -1,6 +1,8 @@
 export const TABLE_ALIAS = 't'
 export const FOREIGN_TABLE_ALIAS_PREFIX = 'ft'
 
+export const INTERNAL_COLUMN_NAME_TOTAL = 'total'
+
 export type ForeignTableAlias = `${typeof FOREIGN_TABLE_ALIAS_PREFIX}${number}`
 
 export const getFTAlias = (index: number): ForeignTableAlias => `${FOREIGN_TABLE_ALIAS_PREFIX}${index}`
