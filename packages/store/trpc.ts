4f748baa0a1aa4d1422b2f0d05914a00f3dc2dd5
nichenqin <nichenqin@hotmail.com>
2023-03-09 16:23:11 +0800

    :heavy_minus_sign: remove superjson

--- packages/store/trpc.ts
+++ packages/store/trpc.ts
@@ -1,9 +1,7 @@
 import type { AppRouter } from '@egodb/trpc'
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
-import superjson from 'superjson'
 
 export const trpc: ReturnType<typeof createTRPCProxyClient<AppRouter>> = createTRPCProxyClient<AppRouter>({
-  transformer: superjson,
   links: [
     httpBatchLink({
       url: 'http://localhost:4000/api/trpc',
--- packages/trpc/src/trpc.ts
+++ packages/trpc/src/trpc.ts
@@ -1,9 +1,6 @@
 import { initTRPC } from '@trpc/server'
-import superjson from 'superjson'
 
-const t = initTRPC.create({
-  transformer: superjson,
-})
+const t = initTRPC.create({})
 
 export const middleware = t.middleware
 export const router = t.router
