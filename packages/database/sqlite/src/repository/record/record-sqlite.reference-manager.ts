e5799511e84e4d6ff5e2e7463f5fd497aaad7c6f
nichenqin <nichenqin@hotmail.com>
2023-03-23 11:37:23 +0800

    :bug: (sqlite) reference query parent field

--- packages/database/sqlite/src/repository/record/record-sqlite.reference-manager.ts
+++ packages/database/sqlite/src/repository/record/record-sqlite.reference-manager.ts
@@ -6,7 +6,9 @@ import type {
   ColorField,
   Field as CoreField,
   LookupField as CoreLookupField,
+  ParentField as CoreParentField,
   ReferenceField as CoreReferenceField,
+  TreeField as CoreTreeField,
   CountField,
   CreatedAtField,
   DateField,
@@ -15,22 +17,24 @@ import type {
   IFieldVisitor,
   IdField,
   NumberField,
-  ParentField,
   RatingField,
   SelectField,
   StringField,
   Table,
-  TreeField,
   UpdatedAtField,
 } from '@egodb/core'
 import { INTERNAL_COLUMN_ID_NAME } from '@egodb/core'
 import type { EntityManager, Knex } from '@mikro-orm/better-sqlite'
 import { uniqBy } from 'lodash-es'
-import type { LookupField, ReferenceField } from '../../entity/field.js'
+import type { LookupField, ParentField, ReferenceField, TreeField } from '../../entity/field.js'
 import type { Table as TableEntity } from '../../entity/table.js'
 import type { IUnderlyingColumn } from '../../interfaces/underlying-column.js'
 import { UnderlyingColumnFactory } from '../../underlying-table/underlying-column.factory.js'
-import { AdjacencyListTable, getUnderlyingTableAlias } from '../../underlying-table/underlying-foreign-table.js'
+import {
+  AdjacencyListTable,
+  ClosureTable,
+  getUnderlyingTableAlias,
+} from '../../underlying-table/underlying-foreign-table.js'
 import { TABLE_ALIAS, getForeignTableAlias } from './record.constants.js'
 import { getExpandColumnName } from './record.util.js'
 
@@ -147,11 +151,108 @@ export class RecordSqliteReferenceManager implements IFieldVisitor {
       )
       .leftJoin(subQuery, `${uta}.${AdjacencyListTable.FROM_ID}`, `${TABLE_ALIAS}.${INTERNAL_COLUMN_ID_NAME}`)
   }
-  tree(field: TreeField): void {
-    throw new Error('Method not implemented.')
+  tree(field: CoreTreeField): void {
+    if (this.#visited.has(field.id.value)) {
+      return
+    }
+
+    const column = this.#mustGetColumn(field) as TreeField
+    const countFields = column.countFields.getItems().map((f) => f.toDomain())
+    const lookupFields = column.lookupFields.getItems()
+    const displayFields = column.displayFields
+      .getItems()
+      .concat(lookupFields.flatMap((c) => c.displayFields.getItems()))
+    const displayColumns = uniqBy(displayFields, (f) => f.id).map((field) => field.toDomain())
+
+    const foreignTableId = field.foreignTableId.unwrapOr(this.table.id.value)
+
+    const closure = new ClosureTable(foreignTableId, field)
+
+    const uta = getUnderlyingTableAlias(field)
+    const fta = getForeignTableAlias(field, this.table.schema.toIdMap())
+    const subQuery = this.knex
+      .queryBuilder()
+      .select(
+        ClosureTable.PARENT_ID,
+        ClosureTable.CHILD_ID,
+        this.knex.raw(`json_group_array(${ClosureTable.CHILD_ID}) as ${field.id.value}`),
+        ...displayColumns.map((f) => this.knex.raw(`json_group_array(${fta}.${f.id.value}) as ${f.id.value}`)),
+        ...countFields.map((f) => this.knex.raw(`count(*) as ${f.id.value}`)),
+      )
+      .from(closure.name)
+      .groupBy(ClosureTable.PARENT_ID)
+      .as(uta)
+
+    const nestSubQuery = this.knex
+      .queryBuilder()
+      .select(
+        INTERNAL_COLUMN_ID_NAME,
+        ...displayColumns.map((f) =>
+          f.isSystem()
+            ? (UnderlyingColumnFactory.create(f, foreignTableId) as IUnderlyingColumn).name + ` as ${f.id.value}`
+            : f.id.value,
+        ),
+      )
+      .from(foreignTableId)
+      .groupBy(INTERNAL_COLUMN_ID_NAME)
+      .as(fta)
+
+    subQuery.leftJoin(nestSubQuery, `${closure.name}.${ClosureTable.CHILD_ID}`, `${fta}.${INTERNAL_COLUMN_ID_NAME}`)
+    this.#visited.add(field.id.value)
+
+    const getFieldExpand = (column: LookupField | TreeField) =>
+      this.knex.raw(
+        `json_object('${column.id}', json_object(${column.displayFields
+          .getItems()
+          .flatMap((c) => [`'${c.id}'`, `${uta}.${c.id}`])
+          .join(',')})) as ${getExpandColumnName(column.id)}`,
+      )
+
+    this.qb
+      .select(
+        `${uta}.${field.id.value} as ${field.id.value}`,
+        getFieldExpand(column),
+        ...lookupFields.map((c) => getFieldExpand(c)),
+        ...countFields.map((c) => `${uta}.${c.id.value} as ${c.id.value}`),
+      )
+      .leftJoin(subQuery, `${uta}.${ClosureTable.PARENT_ID}`, `${TABLE_ALIAS}.${INTERNAL_COLUMN_ID_NAME}`)
   }
-  parent(field: ParentField): void {
-    throw new Error('Method not implemented.')
+  parent(field: CoreParentField): void {
+    if (this.#visited.has(field.id.value)) {
+      return
+    }
+    const column = this.#mustGetColumn(field) as ParentField
+    const displayFields = column.displayFields.getItems()
+    const displayColumns = uniqBy(displayFields, (f) => f.id).map((field) => field.toDomain())
+    const foreignTableId = field.foreignTableId.unwrapOr(this.table.id.value)
+    const closure = new ClosureTable(foreignTableId, field)
+    const uta = getUnderlyingTableAlias(field)
+    const fta = getForeignTableAlias(field, this.table.schema.toIdMap())
+    const subQuery = this.knex
+      .queryBuilder()
+      .select(
+        ClosureTable.PARENT_ID,
+        ClosureTable.CHILD_ID,
+        `${ClosureTable.PARENT_ID} as ${field.id.value}`,
+        // ...displayColumns.map((f) => `${fta}.${f.id.value} as ${f.id.value}`),
+      )
+      .from(closure.name)
+      .groupBy(ClosureTable.CHILD_ID)
+      .as(uta)
+
+    const getFieldExpand = (column: LookupField | ParentField) =>
+      this.knex.raw(
+        `json_object('${column.id}', json_object(${column.displayFields
+          .getItems()
+          .flatMap((c) => [`'${c.id}'`, `${uta}.${c.id}`])
+          .join(',')})) as ${getExpandColumnName(column.id)}`,
+      )
+
+    this.qb
+      .select(`${uta}.${field.id.value} as ${field.id.value}`)
+      .leftJoin(subQuery, `${uta}.${ClosureTable.CHILD_ID}`, `${TABLE_ALIAS}.${INTERNAL_COLUMN_ID_NAME}`)
+
+    this.#visited.add(field.id.value)
   }
   rating(field: RatingField): void {}
   count(field: CountField): void {
