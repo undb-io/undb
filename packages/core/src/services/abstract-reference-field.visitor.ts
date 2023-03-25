1a6b205b7f84ff1ba8af1e427452e2800537d6ff
nichenqin <nichenqin@hotmail.com>
2023-03-25 11:36:07 +0800

    :recycle: (core) add abstract reference field visitor

--- packages/core/src/services/abstract-reference-field.visitor.ts
+++ packages/core/src/services/abstract-reference-field.visitor.ts
@@ -21,46 +21,9 @@ import type {
   SumField,
   TreeField,
   UpdatedAtField,
-  WithDisplayFields,
-  WithFieldDescription,
-  WithFieldName,
-  WithFieldRequirement,
-  WithFormat,
-  WithNewOption,
-  WithOptions,
-  WithSymmetricReferenceField,
-  WithoutField,
-  WithoutOption,
-} from '../field/index.js'
-import type {
-  ITableSpecVisitor,
-  WithFilter,
-  WithNewField,
-  WithTableId,
-  WithTableName,
-  WithTableSchema,
-} from '../specifications/index.js'
-import type {
-  WithCalendarField,
-  WithDisplayType,
-  WithFieldOption,
-  WithFieldVisibility,
-  WithFieldWidth,
-  WithKanbanField,
-  WithNewView,
-  WithShowSystemFieldsSpec,
-  WithSorts,
-  WithTableView,
-  WithTableViews,
-  WithTreeViewField,
-  WithViewFieldsOrder,
-  WithViewName,
-  WithViewPinnedFields,
-  WithViewsOrder,
-  WithoutView,
-} from '../view/index.js'
+} from '../field'
 
-export abstract class AbstractReferenceFieldVisitor implements ITableSpecVisitor, IFieldVisitor {
+export abstract class AbstractReferenceFieldVisitor implements IFieldVisitor {
   id(field: IdField): void {}
   createdAt(field: CreatedAtField): void {}
   updatedAt(field: UpdatedAtField): void {}
@@ -80,46 +43,4 @@ export abstract class AbstractReferenceFieldVisitor implements ITableSpecVisitor
   count(field: CountField): void {}
   sum(field: SumField): void {}
   lookup(field: LookupField): void {}
-  idEqual(s: WithTableId): void {}
-  nameEqual(s: WithTableName): void {}
-  schemaEqual(s: WithTableSchema): void {
-    for (const field of s.schema.fields) {
-      field.accept(this)
-    }
-  }
-  viewsEqual(s: WithTableViews): void {}
-  viewEqual(s: WithTableView): void {}
-  viewNameEqual(s: WithViewName): void {}
-  newView(s: WithNewView): void {}
-  withoutView(s: WithoutView): void {}
-  viewsOrderEqual(s: WithViewsOrder): void {}
-  sortsEqual(s: WithSorts): void {}
-  filterEqual(s: WithFilter): void {}
-  fieldsOrder(s: WithViewFieldsOrder): void {}
-  fieldOptionsEqual(s: WithFieldOption): void {}
-  fieldWidthEqual(s: WithFieldWidth): void {}
-  fieldVisibility(s: WithFieldVisibility): void {}
-  pinnedFields(s: WithViewPinnedFields): void {}
-  displayTypeEqual(s: WithDisplayType): void {}
-  kanbanFieldEqual(s: WithKanbanField): void {}
-  calendarFieldEqual(s: WithCalendarField): void {}
-  treeViewFieldEqual(s: WithTreeViewField): void {}
-  newField(s: WithNewField): void {
-    s.field.accept(this)
-  }
-  withoutField(s: WithoutField): void {}
-  optionsEqual(s: WithOptions): void {}
-  optionEqual(s: WithNewOption): void {}
-  newOption(s: WithNewOption): void {}
-  witoutOption(s: WithoutOption): void {}
-  withFieldName(s: WithFieldName): void {}
-  withFieldDescription(s: WithFieldDescription): void {}
-  displayFieldsEqual(s: WithDisplayFields): void {}
-  withFormat(s: WithFormat): void {}
-  withShowSystemFields(s: WithShowSystemFieldsSpec): void {}
-  withFieldRequirement(s: WithFieldRequirement): void {}
-  symmetricReferenceFieldEqual(s: WithSymmetricReferenceField): void {}
-  not(): this {
-    return this
-  }
 }
--- packages/database/sqlite/src/repository/record/record-sqlite.reference-delete-visitor.ts
+++ packages/database/sqlite/src/repository/record/record-sqlite.reference-delete-visitor.ts
@@ -1,74 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type {
-  AutoIncrementField,
-  BoolField,
-  ColorField,
-  CountField,
-  CreatedAtField,
-  DateField,
-  DateRangeField,
-  EmailField,
-  IFieldVisitor,
-  IdField,
-  LookupField,
-  NumberField,
-  ParentField,
-  RatingField,
-  ReferenceField,
-  SelectField,
-  StringField,
-  SumField,
-  TreeField,
-  UpdatedAtField,
-} from '@egodb/core'
+import type { IFieldVisitor, ParentField, ReferenceField, TreeField } from '@egodb/core'
+import { AbstractReferenceFieldVisitor } from '@egodb/core'
 import type { EntityManager, Knex } from '@mikro-orm/better-sqlite'
+import { Mixin } from 'ts-mixer'
 import type { Table as TableEntity } from '../../entity/table.js'
 import { AdjacencyListTable, ClosureTable } from '../../underlying-table/underlying-foreign-table.js'
 import { BaseEntityManager } from '../base-entity-manager.js'
 
-export class RecordSqliteReferenceDleteVisitor extends BaseEntityManager implements IFieldVisitor {
+export class RecordSqliteReferenceDeleteVisitor
+  extends Mixin(BaseEntityManager, AbstractReferenceFieldVisitor)
+  implements IFieldVisitor
+{
   private readonly knex: Knex
   constructor(em: EntityManager, private readonly tableEntity: TableEntity, private readonly recordId: string) {
     super(em)
     this.knex = em.getKnex()
   }
 
-  id(field: IdField): void {
-    throw new Error('Method not implemented.')
-  }
-  createdAt(field: CreatedAtField): void {
-    throw new Error('Method not implemented.')
-  }
-  updatedAt(field: UpdatedAtField): void {
-    throw new Error('Method not implemented.')
-  }
-  autoIncrement(field: AutoIncrementField): void {
-    throw new Error('Method not implemented.')
-  }
-  string(field: StringField): void {
-    throw new Error('Method not implemented.')
-  }
-  email(field: EmailField): void {
-    throw new Error('Method not implemented.')
-  }
-  color(field: ColorField): void {
-    throw new Error('Method not implemented.')
-  }
-  number(field: NumberField): void {
-    throw new Error('Method not implemented.')
-  }
-  bool(field: BoolField): void {
-    throw new Error('Method not implemented.')
-  }
-  date(field: DateField): void {
-    throw new Error('Method not implemented.')
-  }
-  dateRange(field: DateRangeField): void {
-    throw new Error('Method not implemented.')
-  }
-  select(field: SelectField): void {
-    throw new Error('Method not implemented.')
-  }
   reference(field: ReferenceField): void {
     const foreignTable = new AdjacencyListTable(this.tableEntity.id, field)
     const query = this.knex.delete().from(foreignTable.name).where(AdjacencyListTable.TO_ID, this.recordId).toQuery()
@@ -83,17 +31,5 @@ export class RecordSqliteReferenceDleteVisitor extends BaseEntityManager impleme
     const foreignTable = new ClosureTable(this.tableEntity.id, field)
     const query = this.knex.delete().from(foreignTable.name).where(ClosureTable.PARENT_ID, this.recordId).toQuery()
     this.addQueries(query)
-  }
-  rating(field: RatingField): void {
-    throw new Error('Method not implemented.')
-  }
-  count(field: CountField): void {
-    throw new Error('Method not implemented.')
-  }
-  sum(field: SumField): void {
-    throw new Error('Method not implemented.')
-  }
-  lookup(field: LookupField): void {
-    throw new Error('Method not implemented.')
   }
 }
