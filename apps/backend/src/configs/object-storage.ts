29753c8a4f4c4ac92c04774b3f677f304b0fdcb3
nichenqin <nichenqin@hotmail.com>
2023-03-30 11:21:53 +0800

    :sparkles: (backend) static serve attachments

--- apps/backend/src/configs/object-storage.ts
+++ apps/backend/src/configs/object-storage.ts
@@ -4,7 +4,7 @@ import { registerAs } from '@nestjs/config'
 export const InjectObjectStorageConfig = () => Inject(objectStorageConfig.KEY)
 
 export const objectStorageConfig = registerAs('object-storage', () => ({
-  provider: process.env.EGODB_OBJECT_STORAGE_PROVIDER,
+  provider: process.env.EGODB_OBJECT_STORAGE_PROVIDER! as 'local',
   local: {
     path: process.env.EGODB_OBJECT_STORAGE_LOCAL_PATH!,
   },
