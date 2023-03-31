c7bc5f3957c64cd744cd2b97e7d6c513e7c6eee5
nichenqin <nichenqin@hotmail.com>
2023-03-31 11:54:38 +0800

    :sparkles: (web) support default value

--- apps/web/features/field-inputs/attachment-input.tsx
+++ apps/web/features/field-inputs/attachment-input.tsx
@@ -1,53 +1,73 @@
 import type { IAttachmentItem } from '@egodb/core'
 import { useUploadMutation } from '@egodb/store'
-import { Text, Dropzone, Group, IconUpload, IconX, IconPhoto, useListState } from '@egodb/ui'
-import useDeepCompareEffect from 'use-deep-compare-effect'
+import { Text, Dropzone, Group, IconUpload, IconX, IconPhoto, useListState, AspectRatio, Box } from '@egodb/ui'
+import { useDeepCompareEffectNoCheck } from 'use-deep-compare-effect'
+import { AttachmentValue } from '../field-value/attachment-value'
 
 interface IProps {
   value: IAttachmentItem[]
   onChange(items: IAttachmentItem[]): void
 }
 
-export const AttachmentInput: React.FC<IProps> = ({ onChange }) => {
-  const [attachments, handler] = useListState<IAttachmentItem>([])
+export const AttachmentInput: React.FC<IProps> = ({ onChange, value = [] }) => {
+  const [attachments, handler] = useListState<IAttachmentItem>(value ?? [])
   const [upload, { isLoading }] = useUploadMutation()
 
-  useDeepCompareEffect(() => {
+  useDeepCompareEffectNoCheck(() => {
     onChange(attachments)
   }, [attachments])
 
-  return (
-    <Dropzone
-      onDrop={async (files) => {
-        const formData = new FormData()
-        formData.set('file', files[0])
-        const data = await upload(formData)
-        if (!('error' in data)) {
-          handler.append(data.data)
-        }
-      }}
-      loading={isLoading}
-    >
-      <Group position="center" spacing="xs" style={{ pointerEvents: 'none' }}>
-        <Dropzone.Accept>
-          <IconUpload size="3.2rem" stroke={1.5} />
-        </Dropzone.Accept>
-        <Dropzone.Reject>
-          <IconX size="3.2rem" stroke={1.5} />
-        </Dropzone.Reject>
-        <Dropzone.Idle>
-          <IconPhoto size="3.2rem" stroke={1.5} />
-        </Dropzone.Idle>
+  useDeepCompareEffectNoCheck(() => {
+    if (value) {
+      handler.setState(value)
+    }
+  }, [value])
 
-        <div>
-          <Text size="xl" inline>
-            Drag images here or click to select files
-          </Text>
-          <Text size="sm" color="dimmed" inline mt={7}>
-            Attach as many files as you like, each file should not exceed 5mb
-          </Text>
-        </div>
-      </Group>
-    </Dropzone>
+  return (
+    <>
+      <Dropzone
+        onDrop={async (files) => {
+          const formData = new FormData()
+          formData.set('file', files[0])
+          const data = await upload(formData)
+          if (!('error' in data)) {
+            handler.append(data.data)
+          }
+        }}
+        loading={isLoading}
+      >
+        <Group position="center" spacing="xs" style={{ pointerEvents: 'none' }}>
+          <Dropzone.Accept>
+            <IconUpload size="3.2rem" stroke={1.5} />
+          </Dropzone.Accept>
+          <Dropzone.Reject>
+            <IconX size="3.2rem" stroke={1.5} />
+          </Dropzone.Reject>
+          <Dropzone.Idle>
+            <IconPhoto size="3.2rem" stroke={1.5} />
+          </Dropzone.Idle>
+
+          <div>
+            <Text size="xl" inline>
+              Drag images here or click to select files
+            </Text>
+            <Text size="sm" color="dimmed" inline mt={7}>
+              Attach as many files as you like, each file should not exceed 5mb
+            </Text>
+          </div>
+        </Group>
+      </Dropzone>
+      {!!attachments.length && (
+        <Group h={60}>
+          {attachments.map((file) => (
+            <Box key={file.id} pos="relative">
+              <AspectRatio ratio={1} h="100%" w={60}>
+                <AttachmentValue attachment={file} />
+              </AspectRatio>
+            </Box>
+          ))}
+        </Group>
+      )}
+    </>
   )
 }
