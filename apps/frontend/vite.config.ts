51858990940c303ad26677abf3a021a3883dee44
nichenqin <nichenqin@hotmail.com>
2023-04-05 19:30:24 +0800

    :wrench: (frontend) config vite proxy

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,4 +4,18 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:4000',
+        changeOrigin: true,
+      },
+      '/public': {
+        target: 'http://0.0.0.0:4000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
