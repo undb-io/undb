386e552476136a3522a00e55aff003372adbbf4b
nichenqin <nichenqin@hotmail.com>
2023-04-08 09:22:06 +0800

    :card_file_box: (sqlite) implement user repository

--- packages/core/src/user/user.repository.ts
+++ packages/core/src/user/user.repository.ts
@@ -1,6 +1,8 @@
 import type { Option } from 'oxide.ts'
+import type { UserSpecification } from './specifications/interface.js'
 import type { User } from './user.js'
 
 export interface IUserRepository {
   findOneById(id: string): Promise<Option<User>>
+  exists(spec: UserSpecification): Promise<boolean>
 }
