a7393cb5402edc6c2ebdbec6d074b6acf4fb1a52
nichenqin <nichenqin@hotmail.com>
2023-04-13 16:09:35 +0800

    :globe_with_meridians: (i18n) add i18n package

--- apps/backend/src/i18n/i18n.module.ts
+++ apps/backend/src/i18n/i18n.module.ts
@@ -1,11 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule, OnModuleInit } from '@nestjs/common'
+import { config } from '@undb/i18n'
 import i18next from 'i18next'
 import middleware from 'i18next-http-middleware'
 
 @Module({})
 export class I18nModule implements NestModule, OnModuleInit {
   onModuleInit() {
-    i18next.use(middleware.LanguageDetector).init()
+    i18next.use(middleware.LanguageDetector).init(config)
   }
 
   configure(consumer: MiddlewareConsumer) {
