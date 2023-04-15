458a1bc3b429caca033963dd82e85d91f364137b
nichenqin <nichenqin@hotmail.com>
2023-04-15 20:16:41 +0800

    style(frontend): get members loading

--- apps/frontend/src/features/members/member-list.tsx
+++ apps/frontend/src/features/members/member-list.tsx
@@ -1,11 +1,13 @@
 import { useGetUsersQuery } from '@undb/store'
-import { Space, Stack, Title } from '@undb/ui'
+import { Skeleton, Space, Stack, Title, useDebouncedValue } from '@undb/ui'
 import { MemberListItem } from './member-list-item'
 import { useTranslation } from 'react-i18next'
 
 export const MemberList: React.FC = () => {
-  const { data } = useGetUsersQuery({})
+  const { data, isLoading } = useGetUsersQuery({})
   const { t } = useTranslation()
+
+  const [debouncedIsLoading] = useDebouncedValue(isLoading, 200)
 
   return (
     <>
@@ -14,9 +16,23 @@ export const MemberList: React.FC = () => {
       <Space my="xl" />
 
       <Stack>
-        {data?.ids.map((id) => (
-          <MemberListItem key={id} member={data.entities[id]!} />
-        ))}
+        {debouncedIsLoading ? (
+          <>
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+            <Skeleton h={40} />
+          </>
+        ) : (
+          data?.ids.map((id) => <MemberListItem key={id} member={data.entities[id]!} />)
+        )}
       </Stack>
     </>
   )
