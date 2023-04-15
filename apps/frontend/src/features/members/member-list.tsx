fc7e691215a64a3bee790253193c8624b4910190
nichenqin <nichenqin@hotmail.com>
2023-04-15 20:13:26 +0800

    chore(frontend): member header i18n

--- apps/frontend/src/features/members/member-list.tsx
+++ apps/frontend/src/features/members/member-list.tsx
@@ -1,13 +1,15 @@
 import { useGetUsersQuery } from '@undb/store'
 import { Space, Stack, Title } from '@undb/ui'
 import { MemberListItem } from './member-list-item'
+import { useTranslation } from 'react-i18next'
 
 export const MemberList: React.FC = () => {
   const { data } = useGetUsersQuery({})
+  const { t } = useTranslation()
 
   return (
     <>
-      <Title order={3}>Members</Title>
+      <Title order={3}>{t('Members', { ns: 'common' })}</Title>
 
       <Space my="xl" />
 
