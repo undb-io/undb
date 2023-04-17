168b8af9d7eb953266429e11cf9297c9a836dee7
nichenqin <nichenqin@hotmail.com>
2023-04-17 20:34:19 +0800

    fix(frontend): current page style

--- apps/frontend-next/src/routes/+layout.svelte
+++ apps/frontend-next/src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	import { page } from '$app/stores'
 
 	const navigation = [
-		{ name: 'Tables', href: '/', icon: TableCells, current: false },
+		{ name: 'Tables', href: '/', icon: TableCells, current: $page.url.pathname === '/' },
 		{ name: 'Members', href: '/members', icon: Users, current: false },
 	]
 
@@ -181,13 +181,19 @@
 									<a
 										href={`/t/${table.id}`}
 										class={cx(
-											'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
+											table.id === $page.params.tableId
+												? 'bg-gray-50 text-indigo-600'
+												: 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
 											'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
 										)}
 									>
 										<span
 											class={cx(
-												'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600',
+												table.id === $page.params.tableId
+													? 'bg-gray-50 text-indigo-600'
+													: 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
+
+												' border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600',
 												'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white',
 											)}
 										>
