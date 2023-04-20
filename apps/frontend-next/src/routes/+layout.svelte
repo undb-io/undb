6d19dfab608af0573ef1e0af539cbf9844f123fc
nichenqin <nichenqin@hotmail.com>
2023-04-20 13:34:20 +0800

    feat(frontend): add nprgress

--- apps/frontend-next/src/routes/+layout.svelte
+++ apps/frontend-next/src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
 	import '../app.postcss'
+	import NProgress from 'nprogress'
+	import { navigating } from '$app/stores'
+
+	import 'nprogress/nprogress.css'
+
+	NProgress.configure({
+		minimum: 0.16,
+	})
+
+	$: {
+		if ($navigating) {
+			NProgress.start()
+		}
+		if (!$navigating) {
+			NProgress.done()
+		}
+	}
 </script>
 
 <slot />
