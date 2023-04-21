bcd3f61de900d0db8756df27e1b25f1844e0444e
nichenqin <nichenqin@hotmail.com>
2023-04-21 11:52:53 +0800

    feat(frontend): create field some fields

--- apps/frontend-next/src/lib/field/CreateField.svelte
+++ apps/frontend-next/src/lib/field/CreateField.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { getTable } from '$lib/context'
 	import { createFieldOpen } from '$lib/store'
-	import { Button, Input, Label, Modal, Select, Spinner } from 'flowbite-svelte'
+	import { Button, Input, Label, Modal, Select, Spinner, Toggle, A, Popover, Badge } from 'flowbite-svelte'
 	import FieldIcon from './FieldIcon.svelte'
 	import { FIELD_SELECT_ITEMS } from './types'
 	import { page } from '$app/stores'
@@ -10,6 +10,7 @@
 	import { invalidateAll } from '$app/navigation'
 	import CreateFieldComponent from './CreateFieldComponent/CreateFieldComponent.svelte'
 	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte'
+	import { IconPlus } from '@tabler/icons-svelte'
 
 	const table = getTable()
 
@@ -25,9 +26,19 @@
 			await invalidateAll()
 			createFieldOpen.set(false)
 		},
+		onResult(event) {
+			reset()
+		},
 	})
 
-	const { form, enhance, delayed, submitting } = superFrm
+	const { form, enhance, delayed, reset, submitting } = superFrm
+
+	$: showDescription = false
+	$: if (!showDescription) {
+		$form.description = ''
+	}
+
+	$: displayFields = $table.schema.displayFields.map((f) => f.name.value).concat($form.display ? $form.name : undefined)
 </script>
 
 <form method="POST" use:enhance>
@@ -35,7 +46,7 @@
 		title="Create New Field"
 		placement="top-center"
 		class="w-full rounded-sm"
-		size="md"
+		size="lg"
 		bind:open={$createFieldOpen}
 	>
 		<div class="grid grid-cols-2 gap-x-3 gap-y-4">
@@ -59,19 +70,52 @@
 			</Label>
 		</div>
 
+		{#if showDescription}
+			<Label class="flex flex-col gap-2">
+				<div class="flex gap-2 items-center">
+					<span>description</span>
+				</div>
+
+				<Input class="rounded-sm" name="description" required bind:value={$form.description} />
+			</Label>
+		{/if}
+
 		<CreateFieldComponent type={$form.type} form={superFrm} />
 
 		<SuperDebug data={$form} />
 
 		<svelte:fragment slot="footer">
-			<div class="w-full flex justify-end gap-2">
-				<Button color="alternative" on:click={() => createFieldOpen.set(false)}>Discard</Button>
-				<Button class="gap-4" type="submit" disabled={$submitting}>
-					{#if $delayed}
-						<Spinner size="5" />
-					{/if}
-					Create New Field</Button
-				>
+			<div class="w-full flex items-center justify-between">
+				<div class="flex-1">
+					<Button size="xs" color="alternative" class="space-x-1" on:click={() => (showDescription = !showDescription)}>
+						<IconPlus size={16} />
+						<span>{showDescription ? 'hide' : 'show'} description </span>
+					</Button>
+				</div>
+				<div class="flex justify-end items-center gap-4">
+					<div class="flex gap-2 items-center">
+						<Toggle bind:checked={$form.required}>required</Toggle>
+						<Toggle bind:checked={$form.display}>display</Toggle>
+						{#if $form.display}
+							<Popover class="w-64 text-sm font-light " title="display fields">
+								<div class="flex gap-2">
+									{#each displayFields as field}
+										<Badge>{field}</Badge>
+									{/each}
+								</div>
+							</Popover>
+						{/if}
+					</div>
+					<div class="space-x-2">
+						<Button color="alternative" on:click={() => createFieldOpen.set(false)}>Discard</Button>
+						<Button class="gap-4" type="submit" disabled={$submitting}>
+							{#if $delayed}
+								<Spinner size="5" />
+							{/if}
+							Create New Field</Button
+						>
+					</div>
+				</div>
 			</div>
 		</svelte:fragment>
 	</Modal>
