c4b3a24fe4aa1fc7942dcc679a86555b6b35df91
nichenqin <nichenqin@hotmail.com>
2023-04-22 21:51:58 +0800

    style(frontend): use breadcrumb in table toolbar

--- apps/frontend-next/src/lib/table/TableNavigator.svelte
+++ apps/frontend-next/src/lib/table/TableNavigator.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
-	import { goto } from '$app/navigation'
 	import { getTable } from '$lib/context'
-	import { IconChevronLeft } from '@tabler/icons-svelte'
-	import { Button, P } from 'flowbite-svelte'
-
-	const back = () => goto('/')
+	import { Breadcrumb, BreadcrumbItem, Button, P } from 'flowbite-svelte'
 
 	const table = getTable()
 </script>
 
-<div class="flex items-center gap-2">
-	<Button outline pill color="light" size="xs" class="h-[26px] w-[26px] rounded-full gap-2 !px-1" on:click={back}>
-		<IconChevronLeft size={16} />
-	</Button>
-
-	<P size="sm" color="text-gray-700">{$table.name.value}</P>
-</div>
+<Breadcrumb aria-label="undb navigator">
+	<BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+	<BreadcrumbItem>{$table.name.value}</BreadcrumbItem>
+</Breadcrumb>
