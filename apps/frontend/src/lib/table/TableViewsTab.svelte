45d6eea8cc82d872465cadaec4ea801dff92d798
nichenqin <nichenqin@hotmail.com>
2023-04-27 16:06:59 +0800

    feat: sync table view with view store

--- apps/frontend/src/lib/table/TableViewsTab.svelte
+++ apps/frontend/src/lib/table/TableViewsTab.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { getTable } from '$lib/store/table'
+	import { getTable, getView } from '$lib/store/table'
 	import { Tooltip } from 'flowbite-svelte'
 	import TableViewTabItem from './TableViewTabItem.svelte'
 	import { createViewOpen } from '$lib/store/modal'
 
 	const table = getTable()
+	const currentView = getView()
 
 	$: views = $table.views.views ?? []
 </script>
@@ -12,7 +13,7 @@
 <section class="w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 border-b flex space-x-2 items-center">
 	<ul class="flex flex-wrap space-x-2">
 		{#each views as view}
-			<TableViewTabItem {view} />
+			<TableViewTabItem view={view.id.value === $currentView.id.value ? $currentView : view} />
 		{/each}
 	</ul>
 	<button class="w-7 h-7 hover:bg-gray-100 transition" on:click={() => ($createViewOpen = true)}>
