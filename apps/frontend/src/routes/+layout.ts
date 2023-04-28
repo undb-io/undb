a96b6169324631d638e03e7722a8e54b4c11685c
nichenqin <nichenqin@hotmail.com>
2023-04-28 21:03:33 +0800

    fix: handle get me

--- apps/frontend/src/routes/+layout.ts
+++ apps/frontend/src/routes/+layout.ts
@@ -1,9 +1,14 @@
 import type { ICollaboratorProfile } from '@undb/core'
+import Cookies from 'js-cookie'
 import type { LayoutLoad } from './$types'
 
 export const ssr = false
 
 export const load: LayoutLoad = (event) => {
+	const auth = Cookies.get('undb_auth')
+	if (!auth) {
+		return { me: { me: null } }
+	}
 	const me = event
 		.fetch('/api/auth/me')
 		.then((r) => r.json())
