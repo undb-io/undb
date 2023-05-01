68f610f6530e17795add383b3cbdfdd79a4c0f98
nichenqin <nichenqin@hotmail.com>
2023-05-01 22:04:30 +0800

    style: fix rating a11y

--- apps/frontend/src/lib/cell/CellInput/Rating.svelte
+++ apps/frontend/src/lib/cell/CellInput/Rating.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import cx from 'classnames'
 	import type { RatingField } from '@undb/core'
+	import { Radio } from 'flowbite-svelte'
 	export let value: number = 0
 	export let field: RatingField
 
@@ -15,27 +16,25 @@
 	const onMouseOver = (index: number) => {
 		overIndex = index
 	}
-	const onClick = (index: number) => {
-		value = index + 1
-	}
 </script>
 
 <div class={cx('h-full flex items-center', $$restProps.class)} on:mouseleave={onMouseLeave}>
 	<!-- svelte-ignore a11y-interactive-supports-focus -->
 	{#each Array(max) as _, i}
-		<!-- svelte-ignore a11y-mouse-events-have-key-events -->
-		<!-- svelte-ignore a11y-interactive-supports-focus -->
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<span
-			class="inline-flex items-center justify-center w-5 h-5 transition hover:scale-125"
-			role="button"
-			on:mouseover={() => onMouseOver(i)}
-			on:click={() => onClick(i)}
-		>
-			<i
-				class={cx('ti ti-star-filled inline-block text-gray-300 transition')}
-				class:text-yellow-400={(overIndex === undefined && value > i) || (overIndex !== undefined && overIndex >= i)}
-			/>
-		</span>
+		<Radio value={i + 1} bind:group={value} custom class="group">
+			<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+			<!-- svelte-ignore a11y-interactive-supports-focus -->
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<span
+				class="inline-flex items-center justify-center w-5 h-5 transition hover:scale-125"
+				role="button"
+				on:mouseover={() => onMouseOver(i)}
+			>
+				<i
+					class={cx('ti ti-star-filled inline-block text-gray-300 transition group-focus:!text-red-500')}
+					class:text-yellow-400={(overIndex === undefined && value > i) || (overIndex !== undefined && overIndex >= i)}
+				/>
+			</span>
+		</Radio>
 	{/each}
 </div>
