8bb8c59eb597c4456ccf7cad7807f8d95a621569
nichenqin <nichenqin@hotmail.com>
2023-05-02 13:52:31 +0800

    fix: fix initial record value not displayed

--- apps/frontend/src/lib/cell/CellInput/UsersPicker.svelte
+++ apps/frontend/src/lib/cell/CellInput/UsersPicker.svelte
@@ -3,10 +3,12 @@
 	import CollaboratorComponent from '../CellComponents/CollaboratorComponent.svelte'
 	import { trpc } from '$lib/trpc/client'
 	import { t } from '$lib/i18n'
+	import type { ICollaboratorProfile } from '@undb/core'
 
 	export let value: string[] | undefined
+	export let initialMembers: Map<string, ICollaboratorProfile & { userId: string }> = new Map()
 
-	let group: string[]
+	$: membersMap = new Map(initialMembers)
 
 	let open = false
 	let opened = false
@@ -18,10 +20,13 @@
 	$: query = trpc().user.users.query({}, { enabled: opened })
 	$: members = $query.data?.users ?? []
 
-	$: value = group
+	$: {
+		for (const member of members) {
+			membersMap.set(member.userId, { userId: member.userId, avatar: member.avatar ?? null, username: member.username })
+		}
+	}
 
-	$: membersMap = new Map(members.map((m) => [m.userId, m]))
-	$: selected = group?.map((userId) => membersMap.get(userId)!).filter(Boolean) ?? []
+	$: selected = value?.map((userId) => membersMap.get(userId)!).filter(Boolean) ?? []
 </script>
 
 <Button color="alternative" class="inline-flex gap-3 max-h-10 max-w-max">
@@ -36,7 +41,7 @@
 <Dropdown bind:open>
 	{#each members as member}
 		{@const isSelected = selected.some((s) => s.userId === member.userId)}
-		<Checkbox bind:group value={member.userId} custom>
+		<Checkbox bind:group={value} value={member.userId} custom>
 			<span class="inline-flex items-center justify-between px-4 py-2 cursor-pointer w-full hover:bg-gray-100">
 				<CollaboratorComponent username={member.username} avatar={member.avatar} />
 				{#if isSelected}
