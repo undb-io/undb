5f381b62af99daf475670c9848044525a3836101
nichenqin <nichenqin@hotmail.com>
2023-05-20 15:44:13 +0800

    feat(frontend): implement move view

--- apps/frontend/src/lib/table/TableViewsTab.svelte
+++ apps/frontend/src/lib/table/TableViewsTab.svelte
@@ -4,17 +4,57 @@
 	import TableViewTabItem from './TableViewTabItem.svelte'
 	import { t } from '$lib/i18n'
 	import { createViewModal } from '$lib/store/modal'
+	import Sortable from 'sortablejs'
+	import { onMount } from 'svelte'
+	import type { SortableEvent } from 'sortablejs'
+	import { trpc } from '$lib/trpc/client'
+	import { invalidate } from '$app/navigation'
 
 	const table = getTable()
 	const currentView = getView()
 
-	$: views = $table.views.views ?? []
+	$: views = $table.orderedViews
+
+	let el: HTMLUListElement
+
+	const moveView = trpc().table.view.move.mutation({
+		async onSuccess() {
+			// await invalidate(`table:${$table.id.value}`)
+		},
+	})
+
+	const onEnd = (event: SortableEvent) => {
+		const { oldIndex, newIndex } = event
+		if (oldIndex !== undefined && newIndex !== undefined) {
+			const from = views[oldIndex]?.id.value
+			const to = views[newIndex]?.id.value
+			if (from && to && from !== to) {
+				$moveView.mutate({
+					tableId: $table.id.value,
+					from,
+					to,
+				})
+			}
+		}
+	}
+
+	onMount(async () => {
+		Sortable.create(el, {
+			animation: 200,
+			direction: 'horizontal',
+			onEnd,
+		})
+	})
+
+	$: console.log(el)
 </script>
 
 <section class="w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 border-b flex space-x-2 items-center">
-	<ul class="flex flex-wrap space-x-2">
+	<ul bind:this={el} class="flex flex-wrap space-x-2">
 		{#each views as view}
-			<TableViewTabItem view={view.id.value === $currentView.id.value ? $currentView : view} />
+			<li>
+				<TableViewTabItem view={view.id.value === $currentView.id.value ? $currentView : view} />
+			</li>
 		{/each}
 	</ul>
 	<button class="w-7 h-7 hover:bg-gray-100 transition" on:click={() => createViewModal.open()}>
