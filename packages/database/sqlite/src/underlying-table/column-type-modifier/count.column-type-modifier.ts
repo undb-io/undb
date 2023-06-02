4a84d3cc5b00aa145e95217ec71117be4f18be8b
nichenqin <nichenqin@hotmail.com>
2023-06-02 22:26:08 +0800

    feat(sqlite): count field modifier

--- packages/database/sqlite/src/underlying-table/column-type-modifier/count.column-type-modifier.ts
+++ packages/database/sqlite/src/underlying-table/column-type-modifier/count.column-type-modifier.ts
@@ -2,6 +2,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { type CountField } from '@undb/core'
 import { ReferenceField } from '../../entity/field.js'
+import type { IUnderlyingColumn } from '../../interfaces/underlying-column.js'
+import { UnderlyingForeignTableFactory } from '../undelying-foreign-table.factory.js'
 import {
   UnderlyingBoolColumn,
   UnderlyingColorColumn,
@@ -10,52 +12,52 @@ import {
   UnderlyingDateColumn,
   UnderlyingEmailColumn,
   UnderlyingNumberColumn,
+  UnderlyingRatingColumn,
   UnderlyingSelectColumn,
   UnderlyingStringColumn,
 } from '../underlying-column.js'
-import { AdjacencyListTable } from '../underlying-foreign-table.js'
 import { BaseColumnTypeModifier } from './base.column-type-modifier.js'
 
 export class CountColumnTypeModifier extends BaseColumnTypeModifier<CountField> {
   private readonly column = new UnderlyingCountColumn(this.field.id.value, this.tableId)
 
-  string(): void {
-    const newColumn = new UnderlyingStringColumn(this.field.id.value, this.tableId)
-    this.castTo('text', newColumn, this.column)
-  }
-  number(): void {
-    const newColumn = new UnderlyingNumberColumn(this.field.id.value, this.tableId)
-
+  private castCountColumn(column: IUnderlyingColumn) {
     this.addJobs(async () => {
       const referenceFieldId = this.field.referenceFieldId
       const referenceField = await this.em.findOne(ReferenceField, referenceFieldId.value)
       if (!referenceField) return
 
-      // TODO: generic field type
-      if (referenceField.type === 'reference') {
-        const field = referenceField.toDomain()
+      const field = referenceField.toDomain()
 
-        const ft = new AdjacencyListTable(this.tableId, field)
+      const ft = UnderlyingForeignTableFactory.create(this.tableId, field)
 
-        const addColumn = this.knex.schema.alterTable(this.tableId, (tb) => newColumn.build(tb)).toQuery()
-        await this.em.execute(addColumn)
+      const addColumn = this.knex.schema.alterTable(this.tableId, (tb) => column.build(tb, this.knex, false)).toQuery()
+      await this.em.execute(addColumn)
 
-        const subQuery = this.knex
-          .queryBuilder()
-          .select(this.knex.raw(`count(*) as value`))
-          .from(ft.name)
-          .groupBy(`${ft.name}.${AdjacencyListTable.FROM_ID}`)
-          .toQuery()
+      const subQuery = this.knex
+        .queryBuilder()
+        .select(this.knex.raw(`count(*) as value`))
+        .from(ft.name)
+        .groupBy(ft.fromId)
+        .toQuery()
 
-        const query = `
+      const query = `
       UPDATE ${this.tableId}
-      SET ${newColumn.name} = tt.value
+      SET ${column.name} = tt.value
       FROM (${subQuery}) as tt
       `
 
-        await this.em.execute(query)
-      }
+      await this.em.execute(query)
     })
+  }
+
+  string(): void {
+    const newColumn = new UnderlyingStringColumn(this.field.id.value, this.tableId)
+    this.castCountColumn(newColumn)
+  }
+  number(): void {
+    const newColumn = new UnderlyingNumberColumn(this.field.id.value, this.tableId)
+    this.castCountColumn(newColumn)
   }
   color(): void {
     const newColumn = new UnderlyingColorColumn(this.field.id.value, this.tableId)
@@ -66,7 +68,8 @@ export class CountColumnTypeModifier extends BaseColumnTypeModifier<CountField> 
     this.alterColumn(newColumn, this.column)
   }
   date(): void {
-    this.alterColumn(new UnderlyingDateColumn(this.field.id.value, this.tableId), this.column)
+    const newColumn = new UnderlyingDateColumn(this.field.id.value, this.tableId)
+    this.alterColumn(newColumn, this.column)
   }
   select(): void {
     const newColumn = new UnderlyingSelectColumn(this.field.id.value, this.tableId)
@@ -74,7 +77,7 @@ export class CountColumnTypeModifier extends BaseColumnTypeModifier<CountField> 
   }
   bool(): void {
     const newColumn = new UnderlyingBoolColumn(this.field.id.value, this.tableId)
-    this.castTo('bool', newColumn, this.column)
+    this.castCountColumn(newColumn)
   }
   reference(): void {
     throw new Error('Method not implemented.')
@@ -83,18 +86,17 @@ export class CountColumnTypeModifier extends BaseColumnTypeModifier<CountField> 
     throw new Error('Method not implemented.')
   }
   rating(): void {
-    throw new Error('Method not implemented.')
+    const newColumn = new UnderlyingRatingColumn(this.field.id.value, this.tableId)
+    this.castCountColumn(newColumn)
   }
   currency(): void {
     const newColumn = new UnderlyingCurrencyColumn(this.field.id.value, this.tableId)
-    this.castTo('real', newColumn, this.column)
+    this.castCountColumn(newColumn)
   }
   attachment(): void {
     throw new Error('Method not implemented.')
   }
-  collaborator(): void {
-    this.dropColumn(this.column)
-  }
+  collaborator(): void {}
   count(): void {
     throw new Error('Method not implemented.')
   }
