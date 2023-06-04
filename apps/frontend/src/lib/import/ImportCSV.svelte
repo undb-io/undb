79d60aa2f83dada822d718d4c58eee599d78ce2c
nichenqin <nichenqin@hotmail.com>
2023-06-04 12:46:38 +0800

    feat(core): import csv file

--- apps/frontend/src/lib/import/ImportCSV.svelte
+++ apps/frontend/src/lib/import/ImportCSV.svelte
@@ -1,13 +1,40 @@
 <script lang="ts">
+	import { goto, invalidate } from '$app/navigation'
+	import { t } from '$lib/i18n'
 	import { importCSVModal } from '$lib/store/modal'
-	import { Modal } from 'flowbite-svelte'
+	import { trpc } from '$lib/trpc/client'
+	import { getFieldNames, type ICreateTableSchemaInput } from '@undb/core'
+	import { Button, Modal } from 'flowbite-svelte'
 	import { Dropzone } from 'flowbite-svelte'
+	import Papa from 'papaparse'
+
+	let data: string[][] | undefined
+	let fileName: string | undefined
+
+	$: header = data?.[0]
+
+	let schema: ICreateTableSchemaInput | undefined
+
+	$: if (header) {
+		schema = getFieldNames(header, $t).map((name) => ({ name, type: 'string' }))
+	}
+
+	const parse = (file: File) => {
+		fileName = file.name
+		Papa.parse<string[]>(file, {
+			complete(results, file) {
+				if (results.data.length > 1) {
+					data = results.data
+				}
+			},
+		})
+	}
+
 	const dropHandle = (event: DragEvent) => {
 		event.preventDefault()
 		const files = event.dataTransfer?.files
 		if (!!files?.length) {
-			const fileName = files[0].name
-			alert('You dropped ' + fileName)
+			parse(files[0])
 		}
 	}
 
@@ -15,14 +42,31 @@
 		const target = event.target as HTMLInputElement
 		const files = target.files
 		if (!!files?.length) {
-			const fileName = files[0].name
-			alert('You selected ' + fileName)
+			parse(files[0])
+		}
+	}
+
+	const createTable = trpc().table.create.mutation({
+		async onSuccess(data, variables, context) {
+			importCSVModal.close()
+			await goto(`/t/${data.id}`)
+			await invalidate('tables')
+		},
+	})
+
+	const create = () => {
+		if (fileName && schema?.length) {
+			$createTable.mutate({
+				name: fileName.substring(0, fileName.lastIndexOf('.')) || fileName,
+				schema,
+			})
 		}
 	}
 </script>
 
-<Modal class="w-full" bind:open={$importCSVModal.open}
-	><Dropzone
+<Modal class="w-full" bind:open={$importCSVModal.open}>
+	<Dropzone
+		accept=".csv"
 		id="dropzone"
 		on:drop={dropHandle}
 		on:dragover={(event) => {
@@ -37,16 +81,35 @@
 			stroke="currentColor"
 			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
-			><path
+		>
+			<path
 				stroke-linecap="round"
 				stroke-linejoin="round"
 				stroke-width="2"
 				d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-			/></svg
-		>
+			/>
+		</svg>
 		<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
 			<span class="font-semibold">Click to upload</span> or drag and drop
 		</p>
 		<p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-	</Dropzone></Modal
->
+	</Dropzone>
+	{#if fileName}
+		<div class="flex items-center gap-2">
+			<i class="ti ti-file-code" />
+			<span>
+				{fileName}
+			</span>
+		</div>
+	{/if}
+	<div class="flex justify-end">
+		<div class="space-y-2">
+			<Button size="xs" outline color="alternative" on:click={() => importCSVModal.close()}>
+				{$t('Cancel', { ns: 'common' })}
+			</Button>
+			<Button size="xs" disabled={!data} on:click={create}>
+				{$t('Confirm', { ns: 'common' })}
+			</Button>
+		</div>
+	</div>
+</Modal>
