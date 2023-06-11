44e9bc7b3afbea461e4a8a7832ab13fa0eeee5c6
nichenqin <nichenqin@hotmail.com>
2023-06-11 14:57:37 +0800

    feat(backend): support cache provider mongo

--- apps/backend/src/configs/env.validate.ts
+++ apps/backend/src/configs/env.validate.ts
@@ -36,7 +36,7 @@ export const configSchema = Joi.object({
   UNDB_ADMIN_EMAIL: Joi.string().email().optional(),
   UNDB_ADMIN_PASSWORD: Joi.string().optional(),
 
-  UNDB_CACHE_STORAGE_PROVIDER: Joi.string().valid('memory', 'fs', 'redis').default('memory'),
+  UNDB_CACHE_STORAGE_PROVIDER: Joi.string().valid('memory', 'fs', 'redis', 'mongo').default('memory'),
   UNDB_CACHE_STORAGE_REDIS_HOST: Joi.when('UNDB_CACHE_STORAGE_PROVIDER', {
     is: 'redis',
     then: Joi.string(),
@@ -56,5 +56,18 @@ export const configSchema = Joi.object({
   UNDB_CACHE_STORAGE_REDIS_TTL: Joi.when('UNDB_CACHE_STORAGE_PROVIDER', {
     is: 'redis',
     then: Joi.number().positive().optional(),
+  }),
+
+  UNDB_CACHE_STORAGE_MONGO_CONNECTION_STRING: Joi.when('UNDB_CACHE_STORAGE_PROVIDER', {
+    is: 'mongo',
+    then: Joi.string(),
+  }),
+  UNDB_CACHE_STORAGE_MONGO_DATABASE_NAME: Joi.when('UNDB_CACHE_STORAGE_PROVIDER', {
+    is: 'mongo',
+    then: Joi.string().optional().default('undb_cache'),
+  }),
+  UNDB_CACHE_STORAGE_MONGO_COLLECTION_NAME: Joi.when('UNDB_CACHE_STORAGE_PROVIDER', {
+    is: 'mongo',
+    then: Joi.string().optional().default('undb_cache'),
   }),
 })
--- apps/backend/src/health/health.controller.ts
+++ apps/backend/src/health/health.controller.ts
@@ -1,28 +1,33 @@
 import { RedisHealthIndicator } from '@liaoliaots/nestjs-redis-health'
 import { Controller, Get } from '@nestjs/common'
 import type { ConfigType } from '@nestjs/config'
-import { HealthCheck, HealthCheckService, HealthIndicatorFunction } from '@nestjs/terminus'
+import { HealthCheck, HealthCheckService, HealthIndicatorFunction, MongooseHealthIndicator } from '@nestjs/terminus'
 import Redis from 'ioredis'
+import { createConnection, type Connection } from 'mongoose'
 import { InjectCacheStorageConfig, cacheStorageConfig } from '../configs/cache-storage.config.js'
 import { StorageHealthIndicator } from '../storage/storage.health.js'
 
 @Controller('health')
 export class HealthController {
   redis: Redis | undefined
+  mongo: Connection | undefined
   constructor(
     private readonly health: HealthCheckService,
     private readonly storageHealth: StorageHealthIndicator,
     private readonly redisIndicator: RedisHealthIndicator,
+    private readonly mongooseIndicator: MongooseHealthIndicator,
     @InjectCacheStorageConfig() private readonly cacheConfig: ConfigType<typeof cacheStorageConfig>,
   ) {
     if (cacheConfig.provider === 'redis') {
-      console.log(cacheConfig.redis)
       this.redis = new Redis({
         host: cacheConfig.redis.host,
         password: cacheConfig.redis.password,
         port: cacheConfig.redis.port,
         connectTimeout: 10000,
       })
+    }
+    if (cacheConfig.provider === 'mongo') {
+      this.mongo = createConnection(cacheConfig.mongo.connectionString)
     }
   }
 
@@ -31,6 +36,14 @@ export class HealthController {
     if (this.cacheConfig.provider === 'redis') {
       indicators.push(() =>
         this.redisIndicator.checkHealth('cache-storage-redis', { client: this.redis!, type: 'redis' }),
+      )
+    }
+
+    if (this.cacheConfig.provider === 'mongo') {
+      indicators.push(() =>
+        this.mongooseIndicator.pingCheck('cache-storage-mongo', {
+          connection: this.mongo,
+        }),
       )
     }
 
