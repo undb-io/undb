9532cb6e0ef1c9a66be7edb90eb850afea0dc12d
nichenqin <nichenqin@hotmail.com>
2023-06-11 11:49:57 +0800

    style(frontend): update table navigator styles

--- apps/frontend/src/lib/table/TableNavigator.svelte
+++ apps/frontend/src/lib/table/TableNavigator.svelte
@@ -1,24 +1,12 @@
 <script lang="ts">
-	import { t } from '$lib/i18n'
 	import { getTable, getView } from '$lib/store/table'
-	import ViewIcon from '$lib/view/ViewIcon.svelte'
-	import { Breadcrumb, BreadcrumbItem, Button, P } from 'flowbite-svelte'
+	import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte'
 
 	const table = getTable()
 	const view = getView()
 </script>
 
-<Breadcrumb aria-label="undb navigator" class="hidden xl:block">
-	<BreadcrumbItem class="whitespace-nowrap flex items-center" href="/" home
-		>{$t('Home', { ns: 'common' })}</BreadcrumbItem
-	>
+<Breadcrumb aria-label="undb navigator" class="hidden xl:block" olClass="md:space-x-0 flex items-center">
+	<BreadcrumbItem href="/" home />
 	<BreadcrumbItem class="whitespace-nowrap flex items-center">{$table.name.value}</BreadcrumbItem>
-	<BreadcrumbItem class="whitespace-nowrap flex items-center">
-		<span class="inline-flex items-center gap-2">
-			<ViewIcon type={$view.displayType} class="!text-gray-500" />
-			<span>
-				{$view.name.value}
-			</span>
-		</span>
-	</BreadcrumbItem>
 </Breadcrumb>
