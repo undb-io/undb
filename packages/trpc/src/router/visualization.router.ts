502f9e96a9f72d7ec0ae6c6213d9e965cff6de31
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-14 19:31:47 +0800

    fix(trpc): virsualization typo

--- packages/trpc/src/router/visualization.router.ts
+++ packages/trpc/src/router/visualization.router.ts
@@ -1,16 +1,16 @@
-import { UpdateVirsualizationCommand, updateVirsualizationCommandInput } from '@undb/cqrs'
+import { UpdateVisualizationCommand, updateVisualizationCommandInput } from '@undb/cqrs'
 import type { ICommandBus } from '@undb/domain'
 import { z } from 'zod'
 import type { publicProcedure } from '../trpc.js'
 import { router } from '../trpc.js'
 
-export const createVirsualizationRouter = (procedure: typeof publicProcedure) => (commandBus: ICommandBus) =>
+export const createVisualizationRouter = (procedure: typeof publicProcedure) => (commandBus: ICommandBus) =>
   router({
     update: procedure
-      .input(updateVirsualizationCommandInput)
+      .input(updateVisualizationCommandInput)
       .output(z.void())
       .mutation(({ input }) => {
-        const cmd = new UpdateVirsualizationCommand(input)
+        const cmd = new UpdateVisualizationCommand(input)
         return commandBus.execute<void>(cmd)
       }),
   })
