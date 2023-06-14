242f24a689fcc53934030f6ba785f50f4f78ddcf
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-14 13:26:39 +0800

    feat(backend): add csvExportor & excelExportor to ExportGridCommandHandler

--- apps/backend/src/core/table/commands/export-grid.command.handler.ts
+++ apps/backend/src/core/table/commands/export-grid.command.handler.ts
@@ -2,7 +2,12 @@ import type { ICommandHandler } from '@nestjs/cqrs'
 import { CommandHandler } from '@nestjs/cqrs'
 import { type IRecordRepository, type ITableRepository, type IRecordExportor } from '@undb/core'
 import { ExportGridCommandHandler as DomainHandler, ExportGridCommand } from '@undb/cqrs'
-import { InjectRecordReposiory, InjectTableReposiory, InjectRecordExportor } from '../adapters/index.js'
+import {
+  InjectRecordReposiory,
+  InjectTableReposiory,
+  InjectRecordCSVExportor,
+  InjectRecordExcelExportor,
+} from '../adapters/index.js'
 
 @CommandHandler(ExportGridCommand)
 export class ExportGridCommandHandler extends DomainHandler implements ICommandHandler<ExportGridCommand> {
@@ -11,9 +16,11 @@ export class ExportGridCommandHandler extends DomainHandler implements ICommandH
     protected readonly tableRepo: ITableRepository,
     @InjectRecordReposiory()
     protected readonly recordRepo: IRecordRepository,
-    @InjectRecordExportor()
-    protected readonly exportor: IRecordExportor,
+    @InjectRecordCSVExportor()
+    protected readonly csvExportor: IRecordExportor,
+    @InjectRecordExcelExportor()
+    protected readonly excelExportor: IRecordExportor,
   ) {
-    super(tableRepo, recordRepo, exportor)
+    super(tableRepo, recordRepo, csvExportor, excelExportor)
   }
 }
