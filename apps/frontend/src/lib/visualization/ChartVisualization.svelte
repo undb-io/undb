d2ae975502ab4e716743069486b9230a9efc020c
nichenqin <nichenqin@hotmail.com>
2023-06-22 08:47:13 +0800

    fix(chart): fix empty chart query error

--- apps/frontend/src/lib/visualization/ChartVisualization.svelte
+++ apps/frontend/src/lib/visualization/ChartVisualization.svelte
@@ -16,7 +16,7 @@
 	$: fieldId = visualization.fieldId?.value
 	$: field = fieldId ? $table.schema.getFieldById(fieldId).into() : undefined
 
-	const getChartData = trpc().table.aggregate.chart.query(
+	$: getChartData = trpc().table.aggregate.chart.query(
 		{
 			tableId: $table.id.value,
 			viewId: $view.id.value,
@@ -24,6 +24,7 @@
 		},
 		{
 			queryHash: visualization.id.value,
+			enabled: !!field,
 		},
 	)
 
