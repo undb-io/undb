03576b07ecf0ee36405a470f38e8ccd0e0346807
Sophie <wanshufen0414@gmail.com>
2023-07-02 23:18:57 +0800

    feat: modify dark mode color

--- apps/frontend/src/routes/(authed)/+layout.svelte
+++ apps/frontend/src/routes/(authed)/+layout.svelte
@@ -251,18 +251,18 @@
 								href={`/t/${table.id}`}
 								class={cx(
 									table.id === $page.params.tableId
-										? 'bg-blue-50 text-indigo-600 dark:text-blue-600 dark:bg-gray-700'
-										: 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50  dark:text-white dark:hover:bg-gray-700',
+										? 'bg-blue-50 text-indigo-600 dark:text-gray-50 dark:bg-gray-700'
+										: 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50  dark:text-gray-200 dark:hover:bg-gray-700',
 									'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
 								)}
 							>
 								<span
 									class={cx(
 										table.id === $page.params.tableId
-											? 'bg-blue-50 text-indigo-600 dark:text-blue-600 dark:bg-gray-700 '
-											: 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50 dark:text-white dark:hover:text-blue-600 dark:bg-gray-700',
+											? 'bg-blue-50 text-indigo-600 dark:text-gray-50 dark:bg-gray-700 '
+											: 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50 dark:text-gray-200 dark:hover:text-gray-200 dark:bg-gray-700',
 
-										' border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 dark:border-white dark:group-hover:border-blue-500 dark:group-hover:text-blue-600',
+										' border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 dark:border-white dark:group-hover:border-gray-50 dark:group-hover:text-gray-50',
 										'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white ',
 									)}
 								>
