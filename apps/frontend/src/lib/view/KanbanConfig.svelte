cedc6ab653b90209b423315de093458ef20ce3f8
nichenqin <nichenqin@hotmail.com>
2023-07-06 09:21:29 +0800

    fix: fix view config set field

--- apps/frontend/src/lib/view/KanbanConfig.svelte
+++ apps/frontend/src/lib/view/KanbanConfig.svelte
@@ -7,6 +7,7 @@
 	import { configViewModal, createFieldInitial, createFieldModal } from '$lib/store/modal'
 	import { t } from '$lib/i18n'
 	import { invalidate } from '$app/navigation'
+	import { FieldId } from '@undb/core'
 
 	const table = getTable()
 	const view = getView()
@@ -41,9 +42,11 @@
 </div>
 
 {#if kanbanFields.length}
-	<Hr class="my-6">
-		<span class="text-gray-400 text-sm font-normal">{$t('or', { ns: 'common' })}</span></Hr
-	>
+	<div class="my-4">
+		<Hr>
+			<span class="text-gray-400 text-sm font-normal">{$t('or', { ns: 'common' })}</span>
+		</Hr>
+	</div>
 {/if}
 
 <div class="flex flex-col justify-center gap-2">
@@ -52,10 +55,18 @@
 		color="light"
 		class="flex gap-2"
 		on:click={() => {
+			const id = FieldId.createId()
 			$createFieldInitial = {
+				id,
 				type: 'select',
 			}
-			createFieldModal.open()
+			createFieldModal.open(async () => {
+				$setField.mutate({
+					tableId: $table.id.value,
+					viewId: $view.id.value,
+					field: id,
+				})
+			})
 		}}
 	>
 		<i class="ti ti-plus" />
@@ -68,10 +79,18 @@
 		color="light"
 		class="flex gap-2"
 		on:click={() => {
+			const id = FieldId.createId()
 			$createFieldInitial = {
+				id,
 				type: 'date',
 			}
-			createFieldModal.open()
+			createFieldModal.open(async () => {
+				$setField.mutate({
+					tableId: $table.id.value,
+					viewId: $view.id.value,
+					field: id,
+				})
+			})
 		}}
 	>
 		<i class="ti ti-plus" />
