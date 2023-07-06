f41fb082ec177b3ebd690d7cd86315ad0f06489b
nichenqin <nichenqin@hotmail.com>
2023-07-06 10:10:19 +0800

    fix: use svelte gantt

--- apps/frontend/src/lib/gantt/GanttView.svelte
+++ apps/frontend/src/lib/gantt/GanttView.svelte
@@ -1,47 +1,97 @@
 <script lang="ts">
-	import { getTable, listRecordFn } from '$lib/store/table'
-	import { RecordFactory, type DateRangeField } from '@undb/core'
-	import { endOfMonth, startOfMonth } from 'date-fns'
-	import Gantt from 'frappe-gantt'
+	import { SvelteGantt, SvelteGanttDependencies, SvelteGanttTable } from 'svelte-gantt'
+	import { onMount } from 'svelte'
+	import type { SvelteGanttComponent, SvelteGanttOptions } from 'svelte-gantt/types/gantt'
+	import { endOfDay, endOfMonth, endOfWeek, startOfDay, startOfMonth, startOfWeek } from 'date-fns'
 
-	const table = getTable()
+	let currentStart = startOfWeek(new Date())
+	let currentEnd = endOfWeek(new Date())
 
-	export let field: DateRangeField
-	export let ele: SVGSVGElement | undefined
-
-	let start = startOfMonth(new Date())
-	let end = endOfMonth(new Date())
-
-	const listRecords = $listRecordFn([
-		{
-			path: field.id.value,
-			type: field.type,
-			operator: '$between',
-			value: [start.toISOString(), end.toISOString()],
-		},
-	])
-
-	$: records = RecordFactory.fromQueryRecords($listRecords?.data?.records ?? [], $table.schema.toIdMap()) ?? []
-
-	$: tasks = records.map((r) => ({
-		id: r.id.value,
-		name: r.getDisplayFieldsValue($table),
-		start: r.valuesJSON[field.id.value]?.[0],
-		end: r.valuesJSON[field.id.value]?.[1],
-		progress: 100,
-		dependencies: '',
-	}))
-
-	let gantt: Gantt | undefined
-	$: if (ele) {
-		gantt = new Gantt(ele, tasks, {
-			view_mode: 'Day',
-		})
+	export const data = {
+		rows: [
+			{
+				id: 1,
+				label: 'Accounting',
+				height: 52,
+			},
+			{
+				id: 2,
+				label: 'Business Development',
+				height: 52,
+			},
+			{
+				id: 3,
+				label: 'Ida Flewan',
+				height: 52,
+			},
+			{
+				id: 4,
+				label: 'Lauréna Shrigley',
+				height: 52,
+			},
+			{
+				id: 5,
+				label: 'Ange Kembry',
+				height: 52,
+			},
+		],
+		tasks: [
+			{
+				id: 3,
+				resourceId: 1,
+				label: 'PET-CT',
+				from: startOfDay(new Date()).getTime(),
+				to: endOfDay(new Date()).getTime(),
+				classes: 'orange',
+			},
+		],
+		dependencies: [],
 	}
+
+	let options: SvelteGanttOptions = {
+		rows: data.rows,
+		tasks: data.tasks,
+		dependencies: data.dependencies,
+		timeRanges: [],
+		columnOffset: 15,
+		magnetOffset: 15,
+		rowHeight: 52,
+		rowPadding: 6,
+		headers: [{ unit: 'day', format: 'MMMM Do' }],
+		fitWidth: true,
+		minWidth: 800,
+		from: currentStart.getTime(),
+		to: currentEnd.getTime(),
+		tableHeaders: [{ title: 'Label', property: 'label', width: 140, type: 'tree' }],
+		tableWidth: 240,
+		ganttTableModules: [SvelteGanttTable],
+		ganttBodyModules: [SvelteGanttDependencies],
+	}
+
+	let ele: HTMLElement | undefined
+	let gantt: SvelteGanttComponent
+	onMount(() => {
+		if (ele) {
+			gantt = new SvelteGantt({ target: ele, props: options })
+		}
+	})
 </script>
 
-{#if $listRecords.isLoading}
-	<span>loading</span>
-{:else}
-	<svg class="h-full" bind:this={ele} />
-{/if}
+<div class="w-full">
+	<div bind:this={ele} id="undb-gantt" />
+</div>
+
+<style>
+	#undb-gantt {
+		flex-grow: 1;
+		overflow: auto;
+	}
+
+	#undb-gantt :global(.sg-hover) {
+		background-color: #00000008;
+	}
+
+	#undb-gantt :global(.sg-hover .sg-table-body-cell) {
+		background-color: #00000008;
+	}
+</style>
