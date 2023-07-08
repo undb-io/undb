cd7aed6375f30f319fdcb004f790b7d72dd645f9
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-07-08 19:43:43 +0800

    feat(database): add min to field vistor

--- packages/database/sqlite/src/repository/table/table-sqlite-field.visitor.ts
+++ packages/database/sqlite/src/repository/table/table-sqlite-field.visitor.ts
@@ -31,6 +31,7 @@ import type {
   TreeField as CoreTreeField,
   UpdatedByField as CoreUpdatedByField,
   UrlField as CoreUrlField,
+  MinField as CoreMinMield,
   IFieldVisitor,
 } from '@undb/core'
 import { INTERNAL_COLUMN_ID_NAME } from '@undb/core'
@@ -66,6 +67,7 @@ import {
   UpdatedAtField,
   UpdatedByField,
   UrlField,
+  MinField,
 } from '../../entity/index.js'
 import {
   AdjacencyListTable,
@@ -302,5 +304,12 @@ export class TableSqliteFieldVisitor extends BaseEntityManager implements IField
     const queries = ft.getCreateTableSqls(this.em.getKnex())
 
     this.addQueries(...queries)
+  }
+
+  min(value: CoreMinMield): void {
+    const field = new MinField(this.table, value)
+    field.minReferenceField = this.em.getReference(Field, value.referenceFieldId.value) as ReferenceField | TreeField
+
+    this.em.persist(field)
   }
 }
