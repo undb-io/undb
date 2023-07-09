eb94613ef6d23935d91552b589e741d83256d918
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-07-09 15:40:58 +0800

    feat(frontend): add Min to mutate field component

---
+++ apps/frontend/src/lib/field/MutateFieldComponent/Min.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import cx from 'classnames'
+	import { fieldProxy, type SuperForm } from 'sveltekit-superforms/client'
+	import type { UnwrapEffects } from 'sveltekit-superforms/index'
+	import ReferenceFieldPicker from '../FieldInputs/ReferenceFieldPicker.svelte'
+	import type { Writable } from 'svelte/store'
+
+	export let form: SuperForm<UnwrapEffects<string>, unknown>
+	export let path: any[] = []
+
+	const referenceFieldId = fieldProxy(form.form, [...path, 'referenceFieldId'] as any) as Writable<string>
+</script>
+
+<div class="grid grid-cols-2 gap-x-3">
+	<ReferenceFieldPicker
+		bind:value={$referenceFieldId}
+		required
+		{...$$restProps}
+		class={cx('w-full !justify-start', $$restProps.class)}
+	/>
+</div>
