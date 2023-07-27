557ff9291ad782670b78107c279ee9b02fb784bb
nichenqin <nichenqin@hotmail.com>
2023-07-27 20:37:21 +0800

    style: fix some dark mode styl;e

--- apps/frontend/src/lib/cell/CellComponents/Id.svelte
+++ apps/frontend/src/lib/cell/CellComponents/Id.svelte
@@ -7,6 +7,6 @@
 </script>
 
 <span
-	class="bg-gray-200 text-gray-600 border border-gray-300 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-600 dark:text-gray-200"
+	class="bg-gray-200 text-gray-600 border border-gray-300 dark:border-gray-950 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-600 dark:text-gray-200"
 	>{value}</span
 >
