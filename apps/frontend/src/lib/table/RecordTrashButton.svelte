3416d240924f5c71cc7cb2828feedc52700c9a64
nichenqin <nichenqin@hotmail.com>
2023-07-29 11:55:44 +0800

    style: trash style

--- apps/frontend/src/lib/table/RecordTrashButton.svelte
+++ apps/frontend/src/lib/table/RecordTrashButton.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { recordTrashModal } from '$lib/store/modal'
 	import { Button } from 'flowbite-svelte'
-	import { i18n, t } from '$lib/i18n'
+	import { t } from '$lib/i18n'
 </script>
 
-<Button class="gap-1 md:min-w-[110px]" color="alternative" size="xs" on:click={() => recordTrashModal.open()}>
+<Button class="gap-1" color="alternative" size="xs" on:click={() => recordTrashModal.open()}>
 	<i class="ti ti-recycle" />
-	<span class="hidden md:block">{$t('recycle bin', { ns: 'table' })}</span>
+	<span class="hidden lg:block">{$t('recycle bin', { ns: 'table' })}</span>
 </Button>
