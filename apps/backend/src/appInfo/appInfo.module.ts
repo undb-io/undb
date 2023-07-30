95ff782c3288a66e2f65c1eaa7d62dd699de0c61
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-07-30 20:40:06 +0800

    feat(backend): add HttpModule

--- apps/backend/src/appInfo/appInfo.module.ts
+++ apps/backend/src/appInfo/appInfo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
+import { HttpModule } from '@nestjs/axios'
 import { AppInfoController } from './appInfo.controller.js'
 import { AppInfoService } from './appInfo.service.js'
 
 @Module({
+  imports: [HttpModule],
   providers: [AppInfoService],
   controllers: [AppInfoController],
 })
