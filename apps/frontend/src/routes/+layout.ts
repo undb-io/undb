b91cda69ecee7b588d196a762e4991040e11f47a
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-07-31 13:35:29 +0800

    feat(frontend): load appInfo version

--- apps/frontend/src/routes/+layout.ts
+++ apps/frontend/src/routes/+layout.ts
@@ -1,18 +1,31 @@
-import type { ICollaboratorProfile } from '@undb/core'
+import type { ICollaboratorProfile, IAppInfo } from '@undb/core'
 import Cookies from 'js-cookie'
 import type { LayoutLoad } from './$types'
+import { env } from '$env/dynamic/public'
 
 export const ssr = false
 
 export const load: LayoutLoad = (event) => {
 	const auth = Cookies.get('undb_auth')
+
+	const appInfo =
+		env.PUBLIC_UNDB_VERSION ??
+		(event
+			.fetch('/api/appInfo')
+			.then((r) => r.json())
+			.catch(() => ({
+				appInfo: {
+					version: null,
+				},
+			})) as Promise<{ appInfo: IAppInfo }>)
+
 	if (!auth) {
-		return { me: { me: null } }
+		return { me: { me: null }, appInfo }
 	}
 	const me = event
 		.fetch('/api/auth/me')
 		.then((r) => r.json())
 		.catch(() => ({ me: null })) as Promise<{ me: ICollaboratorProfile }>
 
-	return { me }
+	return { me, appInfo }
 }
