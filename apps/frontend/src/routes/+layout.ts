84c9988fca54ab2738f85e682c7d8b35fca38670
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-07-31 17:15:51 +0800

    fix(frontend): revise appInfo logic

--- apps/frontend/src/routes/+layout.ts
+++ apps/frontend/src/routes/+layout.ts
@@ -1,23 +1,20 @@
 import type { ICollaboratorProfile, IAppInfo } from '@undb/core'
 import Cookies from 'js-cookie'
 import type { LayoutLoad } from './$types'
-import { env } from '$env/dynamic/public'
 
 export const ssr = false
 
 export const load: LayoutLoad = (event) => {
 	const auth = Cookies.get('undb_auth')
 
-	const appInfo =
-		env.PUBLIC_UNDB_VERSION ??
-		(event
-			.fetch('/api/appInfo')
-			.then((r) => r.json())
-			.catch(() => ({
-				appInfo: {
-					version: null,
-				},
-			})) as Promise<{ appInfo: IAppInfo }>)
+	const appInfo = event
+		.fetch('/api/appInfo')
+		.then((r) => r.json())
+		.catch(() => ({
+			appInfo: {
+				version: null,
+			},
+		})) as Promise<{ appInfo: IAppInfo }>
 
 	if (!auth) {
 		return { me: { me: null }, appInfo }
