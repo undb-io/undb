4f001a7afa4ef2cd5b3c78e12c342dc4d599493f
nichenqin <nichenqin@hotmail.com>
2023-08-03 10:34:04 +0800

    fix: fix nest rls repository

--- apps/backend/src/authz/rls/adapters/rls-sqlite.repository.ts
+++ apps/backend/src/authz/rls/adapters/rls-sqlite.repository.ts
@@ -2,6 +2,7 @@ import { MikroORM, UseRequestContext } from '@mikro-orm/core'
 import { Inject, Injectable } from '@nestjs/common'
 import { RLS, type RLSSpecification } from '@undb/authz'
 import { EntityManager, RLSSqliteRepository } from '@undb/sqlite'
+import type { Option } from 'oxide.ts'
 
 export const RLS_REPOSITORY = Symbol('RLS_REPOSITORY')
 
@@ -22,7 +23,22 @@ export class NestRLSSqliteRepository extends RLSSqliteRepository {
   }
 
   @UseRequestContext()
+  findOneById(id: string): Promise<Option<RLS>> {
+    return super.findOneById(id)
+  }
+
+  @UseRequestContext()
   insert(rls: RLS): Promise<void> {
     return super.insert(rls)
+  }
+
+  @UseRequestContext()
+  updateOneById(id: string, spec: RLSSpecification): Promise<void> {
+    return super.updateOneById(id, spec)
+  }
+
+  @UseRequestContext()
+  deleteOneById(id: string): Promise<void> {
+    return super.deleteOneById(id)
   }
 }
