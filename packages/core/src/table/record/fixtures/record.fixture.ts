36ef336c320cfdb7623aa1e348e422215e166867
nichenqin <nichenqin@hotmail.com>
2023-08-07 19:35:40 +0800

    test: fix test record fixture

--- packages/core/src/table/record/fixtures/record.fixture.ts
+++ packages/core/src/table/record/fixtures/record.fixture.ts
@@ -1,9 +1,15 @@
 import { RecordFactory } from '../record.factory.js'
-import { WithRecordId, WithRecordTableId } from '../specifications/index.js'
+import { WithRecordCreatedAt, WithRecordId, WithRecordTableId, WithRecordUpdatedAt } from '../specifications/index.js'
 import type { RecordCompositeSpecification } from '../specifications/interface.js'
 
+const date = new Date(2022, 1, 1)
+
 export const createTestRecord = (...specs: RecordCompositeSpecification[]) => {
-  let spec = WithRecordTableId.fromString('tableId').unwrap().and(WithRecordId.fromString('record'))
+  let spec = WithRecordTableId.fromString('tableId')
+    .unwrap()
+    .and(WithRecordId.fromString('record'))
+    .and(WithRecordCreatedAt.fromDate(date))
+    .and(WithRecordUpdatedAt.fromDate(date))
   for (const s of specs) {
     spec = spec.and(s)
   }
