f5248dff795cb078ef0943712420c849e26c93e7
nichenqin <nichenqin@hotmail.com>
2023-08-09 13:09:26 +0800

    chore(authz): data.sql grant member role

--- data/data.sql
+++ data/data.sql
@@ -1904,5 +1904,7 @@ create index `undb_rls_table_id_index` on `undb_rls` (`table_id`);
 create table `undb_member` (`id` text not null, `created_at` datetime not null, `updated_at` datetime not null, `deleted_at` datetime null, `role` text not null, `user_id` text not null, constraint `undb_member_user_id_foreign` foreign key(`user_id`) references `undb_user`(`id`) on update cascade, primary key (`id`));
 create index `undb_member_deleted_at_index` on `undb_member` (`deleted_at`);
 create index `undb_member_user_id_index` on `undb_member` (`user_id`);
+INSERT INTO undb_member VALUES('mem12345678',1684577267430,1685212920300,NULL,'owner','usrbuajh55q');
+INSERT INTO undb_member VALUES('mem12345679',1684577267430,1685212920300,NULL,'admin','usrzvhv8srb');
 
 COMMIT;
