11619f2476226cca7494d892841bae5bc9b6561e
nichenqin <nichenqin@hotmail.com>
2023-08-10 19:53:07 +0800

    fix: fix type issue

--- packages/cqrs/src/commands/update-role/update-role.command.ts
+++ packages/cqrs/src/commands/update-role/update-role.command.ts
@@ -1,11 +1,11 @@
-import type { IRoles } from '@undb/authz'
+import type { IUpdateMemberRole } from '@undb/authz'
 import type { CommandProps } from '@undb/domain'
 import { Command } from '@undb/domain'
 import type { IUpdateRoleCommandInput } from './update-role.command.input.js'
 
 export class UpdateRoleCommand extends Command implements IUpdateRoleCommandInput {
   readonly memberId: string
-  readonly role: IRoles
+  readonly role: IUpdateMemberRole
 
   constructor(props: CommandProps<IUpdateRoleCommandInput>) {
     super(props)
