e7447dfc6931ac766bc51b1e80be4a6d4e1d5c49
nichenqin <nichenqin@hotmail.com>
2023-08-24 20:40:15 +0800

    fix: fix insert button

--- apps/frontend/src/lib/table/InsertFieldButton.svelte
+++ apps/frontend/src/lib/table/InsertFieldButton.svelte
@@ -7,12 +7,7 @@
 </script>
 
 {#if $hasPermission('table:create_field')}
-	<Button
-		on:click={() => createFieldModal.open()}
-		size="sm"
-		color="alternative"
-		class=" w-8 px-0 inline-flex items-center whitespace-nowrap transition  dark:hover:bg-gray-800 border-gray-200 text-gray-900 dark:text-gray-50 dark:border-gray-400  border bg-[unset] hover:text-primary hover:bg-gray-100"
-	>
+	<Button on:click={() => createFieldModal.open()} size="sm" variant="secondary">
 		<i class="ti ti-column-insert-right text-sm dark:text-gray-200" />
 	</Button>
 	<Tooltip class="z-50" placement="bottom">{$t('Insert Field Right')}</Tooltip>
