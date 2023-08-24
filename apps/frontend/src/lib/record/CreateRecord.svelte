4c1fb4866a8241cefce0370004b67c6c54a979c2
nichenqin <nichenqin@hotmail.com>
2023-08-24 20:17:17 +0800

    feat: add dialog component

--- apps/frontend/src/lib/record/CreateRecord.svelte
+++ apps/frontend/src/lib/record/CreateRecord.svelte
@@ -3,7 +3,7 @@
 	import { getTable, getView } from '$lib/store/table'
 	import { createRecordInitial, createRecordModal } from '$lib/store/modal'
 	import { trpc } from '$lib/trpc/client'
-	import { Button, Label, Modal, Spinner, Toast } from 'flowbite-svelte'
+	import { Label, Modal, Spinner, Toast } from 'flowbite-svelte'
 	import { superForm } from 'sveltekit-superforms/client'
 	import type { Validation } from 'sveltekit-superforms'
 	import FieldIcon from '$lib/field/FieldIcon.svelte'
@@ -11,6 +11,8 @@
 	import { t } from '$lib/i18n'
 	import { keys } from 'lodash-es'
 	import { pick } from 'lodash-es'
+	import * as Dialog from '$lib/components/ui/dialog'
+	import { Button } from '$lib/components/ui/button'
 
 	const table = getTable()
 	const view = getView()
@@ -51,45 +53,53 @@
 	$: $table, reset()
 </script>
 
-<Modal title={$t('Create New Record') ?? undefined} class="w-full" size="lg" bind:open={$createRecordModal.open}>
-	<form id="createRecord" class="space-y-5" method="POST" use:enhance>
-		<div class="grid grid-cols-5 gap-x-3 gap-y-4 items-center">
-			{#each fields as field}
-				<div class="h-full items-start gap-1 pt-2">
-					<Label class="leading-5" for={field.id.value} data-field-id={field.id.value}>
-						<div class="inline-flex items-center gap-2">
-							<FieldIcon type={field.type} size={16} />
-							<span>
-								{field.name.value}
-							</span>
-						</div>
-						{#if field.required}
-							<span class="text-red-500">*</span>
-						{/if}
-					</Label>
-				</div>
-				<div class="col-span-4">
-					<CellInput class="w-full" {field} bind:value={$form[field.id.value]} {...$constraints[field.id.value]} />
-				</div>
-			{/each}
-		</div>
-	</form>
-	<!-- <SuperDebug data={$form} /> -->
+<Dialog.Root bind:open={$createRecordModal.open}>
+	<Dialog.Content class="!w-1/2 max-w-none">
+		<Dialog.Header>
+			<Dialog.Title>{$t('Create New Record')}</Dialog.Title>
+		</Dialog.Header>
 
-	<svelte:fragment slot="footer">
-		<div class="w-full flex justify-end gap-2">
-			<Button color="alternative" on:click={() => createRecordModal.close()}>{$t('Cancel', { ns: 'common' })}</Button>
-			<Button class="gap-2" type="submit" form="createRecord" disabled={$submitting}>
-				{#if $delayed}
-					<Spinner size="5" />
-				{:else}
-					<i class="ti ti-row-insert-bottom" />
-				{/if}
-				{$t('Create New Record')}</Button
-			>
-		</div>
-	</svelte:fragment>
-</Modal>
+		<form id="createRecord" class="space-y-5" method="POST" use:enhance>
+			<div class="grid grid-cols-5 gap-x-3 gap-y-4 items-center">
+				{#each fields as field}
+					<div class="h-full items-start gap-1 pt-2">
+						<Label class="leading-5" for={field.id.value} data-field-id={field.id.value}>
+							<div class="inline-flex items-center gap-2">
+								<FieldIcon type={field.type} size={16} />
+								<span>
+									{field.name.value}
+								</span>
+							</div>
+							{#if field.required}
+								<span class="text-red-500">*</span>
+							{/if}
+						</Label>
+					</div>
+					<div class="col-span-4">
+						<CellInput class="w-full" {field} bind:value={$form[field.id.value]} {...$constraints[field.id.value]} />
+					</div>
+				{/each}
+			</div>
+		</form>
+		<!-- <SuperDebug data={$form} /> -->
+
+		<Dialog.Footer>
+			<div class="w-full flex justify-end gap-2">
+				<Button size="sm" variant="secondary" on:click={() => createRecordModal.close()}>
+					{$t('Cancel', { ns: 'common' })}
+				</Button>
+				<Button size="sm" class="gap-2" type="submit" form="createRecord" disabled={$submitting}>
+					{#if $delayed}
+						<Spinner size="5" />
+					{:else}
+						<i class="ti ti-row-insert-bottom" />
+					{/if}
+					{$t('Create New Record')}</Button
+				>
+			</div>
+		</Dialog.Footer>
+	</Dialog.Content>
+</Dialog.Root>
 
 {#if $createRecord.error}
 	<Toast transition={slide} position="bottom-right" class="z-[99999] !bg-red-500 border-0 text-white font-semibold">
