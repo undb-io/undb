1b722768de844152fa298d926bcdbd8ecb8593c1
Sophie <wanshufen0414@gmail.com>
2023-08-27 21:43:32 +0800

    refactor: refactoring RLS

---
+++ apps/frontend/src/lib/authz/rls/RLSTabTrigger.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { IRLSAction } from '@undb/authz/dist'
+	import { currentRLSS } from '$lib/store/table'
+	import { Badge } from '$components/ui/badge'
+
+	export let action: IRLSAction
+	$: rlss = $currentRLSS.filter((rls) => rls.policy.action === action)
+	$: count = rlss.length
+</script>
+
+<div class="flex gap-1">
+	{action}
+	{#if count}
+		<Badge class="rounded-full">
+			{count}
+		</Badge>
+	{/if}
+</div>
