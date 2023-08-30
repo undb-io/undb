0cf23646d824cf72e904866ff9306c24678d8a2a
Sophie <wanshufen0414@gmail.com>
2023-08-30 17:53:07 +0800

    test: setRowHeight handler test

--- packages/cqrs/src/commands/set-row-height/set-row-height.command.handler.test.ts
+++ packages/cqrs/src/commands/set-row-height/set-row-height.command.handler.test.ts
@@ -1,20 +1,32 @@
-import { ITableRepository, createTestTable } from '@undb/core'
-import { None, Some } from 'oxide.ts'
-import { mock } from 'vitest-mock-extended'
+import { ITableRepository, Table, createTestTable } from '@undb/core'
+import { Err, None, Some } from 'oxide.ts'
+import { MockProxy, mock } from 'vitest-mock-extended'
 import { SetRowHeightCommandHandler } from './set-row-height.command.handler.js'
 import { SetRowHeightCommand } from './set-row-height.command.js'
+import { SpyInstance } from 'vitest'
 
 describe('SetRowHeightCommandHandler', () => {
+  let table: Table
+  let repo: MockProxy<ITableRepository>
+  let spy: SpyInstance
+  let command: SetRowHeightCommand
+  let handler: SetRowHeightCommandHandler
+
+  beforeEach(() => {
+    table = createTestTable()
+
+    repo = mock<ITableRepository>()
+
+    spy = vi.spyOn(table, 'setRowHeight')
+
+    command = new SetRowHeightCommand({ tableId: table.id.value, rowHeight: 'short' })
+
+    handler = new SetRowHeightCommandHandler(repo)
+  })
+
   test('execute success', async () => {
-    const table = createTestTable()
-    const repo = mock<ITableRepository>()
     repo.findOneById.mockResolvedValue(Some(table))
 
-    const spy = vi.spyOn(table, 'setRowHeight')
-
-    const command = new SetRowHeightCommand({ tableId: table.id.value, rowHeight: 'short' })
-
-    const handler = new SetRowHeightCommandHandler(repo)
     await handler.execute(command)
 
     expect(repo.findOneById).toHaveBeenCalledWith(table.id.value)
@@ -26,14 +38,7 @@ describe('SetRowHeightCommandHandler', () => {
   })
 
   test('execute failed', async () => {
-    const table = createTestTable()
-    const repo = mock<ITableRepository>()
     repo.findOneById.mockResolvedValue(None)
-    const spy = vi.spyOn(table, 'setRowHeight')
-
-    const command = new SetRowHeightCommand({ tableId: table.id.value, rowHeight: 'short' })
-
-    const handler = new SetRowHeightCommandHandler(repo)
 
     await expect(handler.execute(command)).rejects.toThrowErrorMatchingInlineSnapshot(
       '"Failed to unwrap Option (found None)"',
@@ -41,5 +46,23 @@ describe('SetRowHeightCommandHandler', () => {
     expect(repo.findOneById).toHaveBeenCalledWith(table.id.value)
     expect(spy).not.toHaveBeenCalled()
     expect(repo.updateOneById).not.toHaveBeenCalled()
+  })
+
+  test('findOneById throw error', async () => {
+    repo.findOneById.mockRejectedValue(new Error('findOneById failed'))
+
+    await expect(handler.execute(command)).rejects.toThrowErrorMatchingInlineSnapshot('"findOneById failed"')
+    expect(spy).not.toHaveBeenCalled()
+    expect(repo.updateOneById).not.toHaveBeenCalled()
+  })
+
+  test('updateOneById throw error', async () => {
+    repo.findOneById.mockResolvedValue(Some(table))
+    repo.updateOneById.mockRejectedValue(new Error('updateOneById failed'))
+
+    await expect(handler.execute(command)).rejects.toThrowErrorMatchingInlineSnapshot('"updateOneById failed"')
+
+    expect(repo.findOneById).toHaveBeenCalledWith(table.id.value)
+    expect(spy).toHaveBeenCalledWith(command)
   })
 })
