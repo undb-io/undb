8b91987eb432a8a0b8547bac5b7885128789a19e
nichenqin <nichenqin@hotmail.com>
2023-09-04 10:15:30 +0800

    fix: tailwindcss bakcgroupnd color

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -17,7 +17,7 @@ const config = {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
+				background: '#fff',
 				foreground: 'hsl(var(--foreground))',
 				primary: {
 					DEFAULT: 'hsl(var(--primary))',
