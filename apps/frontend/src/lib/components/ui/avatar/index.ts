ed69b0018c6d39685b76fff5fad6310b37228ce1
nichenqin <nichenqin@hotmail.com>
2023-09-06 08:19:52 +0800

    style(ui): adding avatar variant

--- apps/frontend/src/lib/components/ui/avatar/index.ts
+++ apps/frontend/src/lib/components/ui/avatar/index.ts
@@ -1,13 +1,30 @@
-import Root from './avatar.svelte'
-import Image from './avatar-image.svelte'
+import { tv, type VariantProps } from 'tailwind-variants'
 import Fallback from './avatar-fallback.svelte'
+import Image from './avatar-image.svelte'
+import Root from './avatar.svelte'
+
+export const avatarVariants = tv({
+	base: 'relative flex shrink-0 overflow-hidden rounded-full',
+	variants: {
+		size: {
+			lg: 'h-10 w-10',
+			default: 'h-7 w-7',
+			sm: 'h-5 w-5',
+		},
+	},
+	defaultVariants: {
+		size: 'default',
+	},
+})
+
+export type Size = VariantProps<typeof avatarVariants>['size']
 
 export {
-	Root,
-	Image,
-	Fallback,
 	//
 	Root as Avatar,
-	Image as AvatarImage,
 	Fallback as AvatarFallback,
+	Image as AvatarImage,
+	Fallback,
+	Image,
+	Root,
 }
--- apps/frontend/src/lib/components/ui/button/index.ts
+++ apps/frontend/src/lib/components/ui/button/index.ts
@@ -1,5 +1,5 @@
-import Root from './button.svelte'
 import { tv, type VariantProps } from 'tailwind-variants'
+import Root from './button.svelte'
 
 export const buttonVariants = tv({
 	base: 'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -29,7 +29,7 @@ export type Variant = VariantProps<typeof buttonVariants>['variant']
 export type Size = VariantProps<typeof buttonVariants>['size']
 
 export {
-	Root,
 	//
 	Root as Button,
+	Root,
 }
