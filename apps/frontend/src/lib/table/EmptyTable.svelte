035563bd9e8305d88e78efe66b7af760f4f858ad
nichenqin <nichenqin@hotmail.com>
2023-09-07 11:10:46 +0800

    fix: fix color

--- apps/frontend/src/lib/table/EmptyTable.svelte
+++ apps/frontend/src/lib/table/EmptyTable.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex-1 w-full flex flex-col gap-4 items-center justify-center content-center">
-	<p class="font-bold">{$t('Create New Record')}</p>
+	<p class="font-bold dark:text-white">{$t('Create New Record')}</p>
 
 	<p class="inline-flex items-center dark:text-gray-100">{@html $t('shortcut R', { shortcut: 'R' })}</p>
 
