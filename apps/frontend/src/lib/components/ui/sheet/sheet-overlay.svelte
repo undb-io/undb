482841eab5c2a45c3b80f0181bf1d1643b204eef
nichenqin <nichenqin@hotmail.com>
2023-09-07 11:08:46 +0800

    fix: fix bacground color

--- apps/frontend/src/lib/components/ui/sheet/sheet-overlay.svelte
+++ apps/frontend/src/lib/components/ui/sheet/sheet-overlay.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
-	import { Dialog as SheetPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils";
-	import { fade } from "svelte/transition";
+	import { Dialog as SheetPrimitive } from 'bits-ui'
+	import { cn } from '$lib/utils'
+	import { fade } from 'svelte/transition'
 
-	type $$Props = SheetPrimitive.OverlayProps;
+	type $$Props = SheetPrimitive.OverlayProps
 
-	let className: $$Props["class"] = undefined;
-	export let transition: $$Props["transition"] = fade;
-	export let transitionConfig: $$Props["transitionConfig"] = {
-		duration: 150
-	};
-	export { className as class };
+	let className: $$Props['class'] = undefined
+	export let transition: $$Props['transition'] = fade
+	export let transitionConfig: $$Props['transitionConfig'] = {
+		duration: 150,
+	}
+	export { className as class }
 </script>
 
 <SheetPrimitive.Overlay
 	{transition}
 	{transitionConfig}
-	class={cn(
-		"fixed inset-0 z-50 bg-background/80 backdrop-blur-sm ",
-		className
-	)}
+	class={cn('fixed inset-0 z-50 bg-white dark:bg-gray-900/80 backdrop-blur-sm ', className)}
 	{...$$restProps}
 />
