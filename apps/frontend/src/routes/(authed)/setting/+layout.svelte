9216027fe2cdbdae08d2a34d05115e431ee94207
nichenqin <nichenqin@hotmail.com>
2023-09-09 07:54:54 +0800

    style: empty api token page

--- apps/frontend/src/routes/(authed)/setting/+layout.svelte
+++ apps/frontend/src/routes/(authed)/setting/+layout.svelte
@@ -1,5 +1,5 @@
 <div class="w-full h-full bg-gray-50 overflow-scroll dark:bg-gray-700">
-	<main class="container mx-auto py-20">
+	<main class="container mx-auto py-20 h-full">
 		<slot />
 	</main>
 </div>
--- apps/frontend/src/routes/(authed)/setting/+page.svelte
+++ apps/frontend/src/routes/(authed)/setting/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import Button from '$components/ui/button/button.svelte'
 	import { t } from '$lib/i18n'
+	import EmptyApiToken from '$lib/openapi/api-token/EmptyApiToken.svelte'
 	import { trpc } from '$lib/trpc/client'
 
 	const createToken = trpc().openapi.apiToken.create.mutation({})
 </script>
 
-<div>
+<div class="w-full h-full">
+	<EmptyApiToken />
 	<div class="flex justify-end">
 		<Button class="w-40 gap-2" on:click={() => $createToken.mutate()}>
 			<i class="ti ti-plus"></i>
