e27403b2d7f27e809f23a0afd7179bd11c8b889f
Sophie <wanshufen0414@gmail.com>
2023-09-10 20:54:57 +0800

    test: test create api token command handler

---
+++ packages/cqrs/src/commands/create-api-token/create-api-token.command.handler.test.ts
@@ -0,0 +1,34 @@
+import { IClsService, ClsStore, UserId } from '@undb/core'
+import { ApiToken, ApiTokenFactory, IApiTokenRepository } from '@undb/openapi'
+import { MockProxy, mock } from 'vitest-mock-extended'
+import { CreateApiTokenCommand } from './create-api-token.command'
+import { CreateApiTokenCommandHandler } from './create-api-token.command.handler'
+import { SpyInstance } from 'vitest'
+
+describe('test create api token command handler', () => {
+  let repo: MockProxy<IApiTokenRepository>
+  let cls: MockProxy<IClsService<ClsStore>>
+  let command: MockProxy<CreateApiTokenCommand>
+  let handler: CreateApiTokenCommandHandler
+  let userId: UserId
+  let token: ApiToken
+  let spy: SpyInstance
+
+  beforeEach(() => {
+    repo = mock<IApiTokenRepository>()
+    cls = mock<IClsService<ClsStore>>()
+    spy = vi.spyOn(ApiTokenFactory, 'new')
+    command = new CreateApiTokenCommand({})
+    handler = new CreateApiTokenCommandHandler(cls, repo)
+    userId = new UserId('user123')
+    token = new ApiToken()
+  })
+
+  test('create api token success', async () => {
+    cls.get.calledWith('user.userId').mockReturnValue('user123')
+    await handler.execute(command)
+    const tokenObj = spy.mock.results[0]
+    expect(spy.mock.calls[0][0]).toBe(userId.value)
+    expect(repo.insert).toHaveBeenCalledWith(tokenObj.value)
+  })
+})
--- packages/cqrs/src/commands/delete-api-token/delete-api-token.command.handler.test.ts
+++ packages/cqrs/src/commands/delete-api-token/delete-api-token.command.handler.test.ts
@@ -5,25 +5,24 @@ import { DeleteApiTokenCommand } from './delete-api-token.command'
 import { DeleteApiTokenCommandHandler } from './delete-api-token.command.handler'
 import { None, Some } from 'oxide.ts'
 
-describe('APITokenStrategy', () => {
+describe('test DeleteApiTokenCommandHandler', () => {
   let repo: MockProxy<IApiTokenRepository>
   let cls: MockProxy<IClsService<ClsStore>>
   let command: DeleteApiTokenCommand
   let handler: DeleteApiTokenCommandHandler
-  let apiToken: ApiToken
+  const apiToken: ApiToken = new ApiToken()
   let userId: UserId
 
   beforeEach(() => {
     repo = mock<IApiTokenRepository>()
     cls = mock<IClsService<ClsStore>>()
-    apiToken = new ApiToken()
     command = new DeleteApiTokenCommand({ apiTokenId: 'apiTokenIdString' })
     handler = new DeleteApiTokenCommandHandler(repo, cls)
     userId = new UserId('user123')
-  })
-  test('Delete api token success', async () => {
     cls.get.calledWith('user.userId').mockReturnValue('user123')
     apiToken.userId = userId
+  })
+  test('Delete api token success', async () => {
     repo.findOneById.mockResolvedValue(Some(apiToken))
     await handler.execute(command)
     expect(repo.findOneById).toHaveBeenCalledWith(command.apiTokenId)
@@ -31,16 +30,12 @@ describe('APITokenStrategy', () => {
   })
 
   test('Delete api token faild', async () => {
-    cls.get.calledWith('user.userId').mockReturnValue('user123')
-    apiToken.userId = userId
     repo.findOneById.mockResolvedValue(None)
     await handler.execute(command)
     expect(repo.deleteOneById).not.toHaveBeenCalled()
   })
 
   test('Delete api token error', async () => {
-    cls.get.calledWith('user.userId').mockReturnValue('user321')
-    apiToken.userId = userId
     repo.findOneById.mockResolvedValue(Some(apiToken))
     await expect(handler.execute(command)).rejects.toThrowErrorMatchingInlineSnapshot(
       '"cannot delete api token belongs to other user"',
