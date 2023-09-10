51640dbb677366db5a4d06a8e2383ea61df76839
nichenqin <nichenqin@hotmail.com>
2023-09-10 11:04:07 +0800

    fix: fix style

--- apps/frontend/src/routes/(authed)/members/+layout.svelte
+++ apps/frontend/src/routes/(authed)/members/+layout.svelte
@@ -1,3 +1,5 @@
 <div class="w-full h-full bg-gray-50 overflow-scroll dark:bg-gray-700">
-	<main class="container mx-auto"><slot /></main>
+	<main class="container mx-auto h-full">
+		<slot />
+	</main>
 </div>
