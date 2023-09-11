d6a26ff92608ae0635a60dae8dcdd104a89c5c55
nichenqin <nichenqin@hotmail.com>
2023-09-11 09:13:37 +0800

    test(backend): setup backend vitest

---
+++ apps/backend/vitest.config.e2e.ts
@@ -0,0 +1,11 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  plugins: [swc.vite()],
+})
