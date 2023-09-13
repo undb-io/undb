fff9b5f1b5925c8f00ae488308db03fd6e812e5a
nichenqin <nichenqin@hotmail.com>
2023-09-13 08:22:05 +0800

    feat: template export from table

--- packages/template/src/value-objects/template-exports.vo.ts
+++ packages/template/src/value-objects/template-exports.vo.ts
@@ -1,4 +1,4 @@
-import type { ClsStore, Table } from '@undb/core'
+import type { ClsStore, ICreateFieldSchema, Table } from '@undb/core'
 import { TableFactory, createFieldSchema, tableIdSchema, tableNameSchema } from '@undb/core'
 import { ValueObject } from '@undb/domain'
 import { z } from 'zod'
@@ -18,6 +18,17 @@ export type IExportSchema = z.infer<typeof exportSchema>
 export class TemplateExport extends ValueObject<IExportSchema> {
   public get tables() {
     return this.props.tables
+  }
+
+  static fromTables(tables: Table[]): TemplateExport {
+    const exp: IExportSchema = {
+      tables: tables.map((table) => ({
+        id: table.id.value,
+        name: table.name.value,
+        schema: table.schema.fields.map((f) => f.json as ICreateFieldSchema),
+      })),
+    }
+    return new this(exp)
   }
 
   toTables(ctx: ClsStore): Table[] {
