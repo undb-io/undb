bcbbd4eb3a4f6064a140b16c84a75b6067560e40
nichenqin <nichenqin@hotmail.com>
2023-09-15 17:33:26 +0800

    fix: fix bug

--- apps/backend/src/template/template.service.ts
+++ apps/backend/src/template/template.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
-import { type ITableRepository } from '@undb/core'
+import { type IRecordQueryModel, type ITableRepository } from '@undb/core'
 import { TemplateService } from '@undb/template'
+import { InjectRecordQueryModel } from '../core/table/adapters/sqlite/record-sqlite.query-model.js'
 import { InjectTableRepository } from '../core/table/adapters/sqlite/table-sqlite.repository.js'
 
 @Injectable()
@@ -8,7 +9,9 @@ export class NestTemplateService extends TemplateService {
   constructor(
     @InjectTableRepository()
     repo: ITableRepository,
+    @InjectRecordQueryModel()
+    qm: IRecordQueryModel,
   ) {
-    super(repo)
+    super(repo, qm)
   }
 }
