03ad8290f06a896f7ac4f6ea9042f572b2a74033
nichenqin <nichenqin@hotmail.com>
2023-09-21 09:47:27 +0800

    style: search input style

--- apps/frontend/src/lib/table/SearchTable.svelte
+++ apps/frontend/src/lib/table/SearchTable.svelte
@@ -3,6 +3,9 @@
 	import { getTable, q, tableQ } from '$lib/store/table'
 	import { Button } from '$components/ui/button'
 	import { Input } from '$components/ui/input'
+	import { fly } from 'svelte/transition'
+	import { quintOut } from 'svelte/easing'
+	import { clickOutside } from '$lib/utils'
 
 	let value: string | undefined = $q
 
@@ -15,22 +18,24 @@
 	$: if (!value) tableQ.resetTableQ($table.id.value)
 
 	const placeholder = $t('Search')
+
+	let show = false
 </script>
 
 <form
+	use:clickOutside
+	on:click_outside={() => (show = false)}
 	on:submit={() => {
 		tableQ.setTableQ($table.id.value, value || undefined)
 	}}
 >
-	<div class="flex items-center">
-		<Input bind:value {placeholder} class="min-w-[150px] rounded-r-none border-r-0 h-8" />
-		<!-- <Search
-			size="sm"
-			bind:value
-			{placeholder}
-			class="dark:hover:bg-gray-800 dark:border-gray-400 dark:hover:border-gray-800 dark:text-gray-200 min-w-[100px]"
-		/> -->
-		<Button type="submit" size="sm" class="rounded-l-none border-l-0">
+	<div class="flex items-center gap-2">
+		{#if show}
+			<div transition:fly={{ duration: 300, x: 100, easing: quintOut }}>
+				<Input bind:value {placeholder} class="min-w-[150px]	 h-8" />
+			</div>
+		{/if}
+		<Button type="submit" variant="ghost" size="icon" on:click={() => (show = true)}>
 			<svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 				<path
 					stroke-linecap="round"
