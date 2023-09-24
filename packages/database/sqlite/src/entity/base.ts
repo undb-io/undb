dd5a0ecd12646173b50b52c9f6816add3ec9256a
nichenqin <nichenqin@hotmail.com>
2023-09-24 10:07:31 +0800

    feat(sqlite): add tables to base entity

--- packages/database/sqlite/src/entity/base.ts
+++ packages/database/sqlite/src/entity/base.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+import { Cascade, Collection, Entity, OneToMany, PrimaryKey, Property } from '@mikro-orm/core'
 import { type Base as CoreBase } from '@undb/core'
+import { Table } from './table.js'
 
 @Entity({ tableName: 'undb_base' })
 export class Base {
@@ -13,4 +14,7 @@ export class Base {
 
   @Property()
   name: string
+
+  @OneToMany(() => Table, (table) => table.base, { orphanRemoval: true, cascade: [Cascade.ALL] })
+  tables = new Collection<Table>(this)
 }
