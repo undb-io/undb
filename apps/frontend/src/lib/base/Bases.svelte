628f0007e319421a660edc7dc0d3264e0d9640be
nichenqin <nichenqin@hotmail.com>
2023-09-26 11:43:30 +0800

    style: base list style

--- apps/frontend/src/lib/base/Bases.svelte
+++ apps/frontend/src/lib/base/Bases.svelte
@@ -10,9 +10,9 @@
 	{#each bases as base}
 		<a
 			href={`/bases/${base.id}`}
-			class="cursor-pointer disabled:cursor-not-allowed disabled:pointer-events-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 [&:focus:not(:focus-visible)]:outline-none focus-visible:ring-indigo-600 rounded-md block p-4 bg-white border border-solid border-slate-200 hover:border-slate-100 hover:shadow-md transition"
+			class="cursor-pointer disabled:cursor-not-allowed disabled:pointer-events-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 [&:focus:not(:focus-visible)]:outline-none focus-visible:ring-indigo-600 rounded-md block p-4 bg-white border border-solid border-slate-200 hover:border-slate-100 hover:shadow-md transition h-20 flex items-center"
 		>
-			<h6 class="antialiased font-bold text-slate-900 leading-6 text-[1rem] flex items-center gap-1">
+			<h6 class="antialiased font-bold text-slate-900 text-[1rem] flex items-center gap-1">
 				<i class="ti ti-category"></i>
 				<span>
 					{base.name}
@@ -22,7 +22,7 @@
 	{/each}
 
 	<button
-		class="cursor-pointer disabled:cursor-not-allowed disabled:pointer-events-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 [&:focus:not(:focus-visible)]:outline-none focus-visible:ring-indigo-600 rounded-md border border-dashed border-slate-200 hover:border-slate-100 py-10 px-4 antialiased font-sans font-medium text-sm text-slate-600 leading-6 hover:text-slate-700 flex items-center justify-center gap-2"
+		class="cursor-pointer disabled:cursor-not-allowed disabled:pointer-events-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 [&:focus:not(:focus-visible)]:outline-none focus-visible:ring-indigo-600 rounded-md border border-dashed border-slate-200 hover:border-slate-100 py-10 px-4 antialiased font-sans font-medium text-sm text-slate-600 leading-6 hover:text-slate-700 flex items-center justify-center gap-2 h-20"
 		on:click={() => createBaseModal.open()}
 	>
 		<i class="ti ti-plus"></i>
