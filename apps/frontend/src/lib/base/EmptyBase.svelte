eddab4eb150c35c06006377ab0d76f7d7eea8d84
nichenqin <nichenqin@hotmail.com>
2023-09-26 12:37:34 +0800

    style: empty base ui

--- apps/frontend/src/lib/base/EmptyBase.svelte
+++ apps/frontend/src/lib/base/EmptyBase.svelte
@@ -1,19 +1,36 @@
 <script lang="ts">
 	import * as Card from '$components/ui/card'
+	import Separator from '$components/ui/separator/separator.svelte'
 	import { t } from '$lib/i18n'
-	import { selectTableMoveToBaseModal } from '$lib/store/modal'
+	import { createTableModal, selectTableMoveToBaseModal } from '$lib/store/modal'
 </script>
 
-<div class="flex h-full items-center justify-center">
-	<Card.Root
-		class="!max-w-none cursor-pointer hover:bg-primary-500/90  hover:shadow-sm transition border border-dashed border-slate-200 hover:border-slate-100 w-52"
-		on:click={() => selectTableMoveToBaseModal.open()}
-	>
-		<Card.Header class="py-4">
-			<div class="flex items-center justify-center gap-2 h-full font-normal text-sm text-gray-500">
-				<i class="ti ti-arrow-bar-to-right" />
-				<p>{$t('move to base', { ns: 'base' })}</p>
-			</div>
-		</Card.Header>
-	</Card.Root>
+<div class="h-full">
+	<div class="flex items-center justify-center h-full gap-6">
+		<Card.Root
+			class="!max-w-none cursor-pointer hover:bg-primary-500/90  hover:shadow-sm transition border border-dashed border-slate-200 hover:border-slate-100 w-52"
+			on:click={() => createTableModal.open()}
+		>
+			<Card.Header class="py-4">
+				<div class="flex items-center justify-center gap-2 h-full font-normal text-sm text-gray-500">
+					<i class="ti ti-plus" />
+					<p>{$t('Create New Table')}</p>
+				</div>
+			</Card.Header>
+		</Card.Root>
+
+		<Separator orientation="vertical" class="h-10" />
+
+		<Card.Root
+			class="!max-w-none cursor-pointer hover:bg-primary-500/90  hover:shadow-sm transition border border-dashed border-slate-200 hover:border-slate-100 w-52"
+			on:click={() => selectTableMoveToBaseModal.open()}
+		>
+			<Card.Header class="py-4">
+				<div class="flex items-center justify-center gap-2 h-full font-normal text-sm text-gray-500">
+					<i class="ti ti-arrow-bar-to-right" />
+					<p>{$t('move to base', { ns: 'base' })}</p>
+				</div>
+			</Card.Header>
+		</Card.Root>
+	</div>
 </div>
