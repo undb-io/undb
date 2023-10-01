b335413a1214195353682bfbe112d1b737880faa
nichenqin <nichenqin@hotmail.com>
2023-10-01 18:40:38 +0800

    feat: allow to create base from template

--- packages/cqrs/src/commands/create-base/create-base.command.ts
+++ packages/cqrs/src/commands/create-base/create-base.command.ts
@@ -1,16 +1,19 @@
 import type { CommandProps } from '@undb/domain'
 import { Command } from '@undb/domain'
+import type { ITemplateSchema } from '@undb/template'
 import type { ICreateBaseCommandInput } from './create-base.command.interface.js'
 
 export class CreateBaseCommand extends Command implements ICreateBaseCommandInput {
   public readonly id?: string
   public readonly name: string
   public readonly tableIds?: [string, ...string[]]
+  public readonly template?: ITemplateSchema
 
   constructor(props: CommandProps<ICreateBaseCommandInput>) {
     super(props)
     this.id = props.id
     this.name = props.name
     this.tableIds = props.tableIds
+    this.template = props.template
   }
 }
