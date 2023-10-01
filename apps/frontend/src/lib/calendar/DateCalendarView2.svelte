98a1ed4cc1d7eaf246ffb94dd3b17efc1f3b8d01
nichenqin <nichenqin@hotmail.com>
2023-10-01 09:51:43 +0800

    fix: fix render calendar init screen

--- apps/frontend/src/lib/calendar/DateCalendarView2.svelte
+++ apps/frontend/src/lib/calendar/DateCalendarView2.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Button from '$components/ui/button/button.svelte'
+	import { t } from '$lib/i18n'
 	import { createRecordInitial, createRecordModal } from '$lib/store/modal'
 	import { currentRecordId, getTable, listRecordFn, recordsStore } from '$lib/store/table'
 	import Calendar, { type EventObject } from '@toast-ui/calendar'
 	import '@toast-ui/calendar/dist/toastui-calendar.min.css'
 	import { RecordFactory, type DateField } from '@undb/core'
 	import { format } from 'date-fns'
+	import { writable } from 'svelte/store'
 
 	export let field: DateField
 	const table = getTable()
@@ -14,6 +16,55 @@
 	let end: Date | undefined = undefined
 
 	let el: HTMLDivElement | undefined
+
+	let range = ''
+	let calendar: Calendar | undefined = undefined
+	$: if (el) {
+		calendar = new Calendar(el, {
+			defaultView: 'month',
+			usageStatistics: false,
+			template: {
+				allday(event) {
+					return `<span class="text-white">${event.title}</span>`
+				},
+			},
+		})
+	}
+
+	const setDateRange = () => {
+		if (!calendar) return
+		start = calendar.getDateRangeStart().toDate()
+		end = calendar.getDateRangeEnd().toDate()
+	}
+
+	$: if (start && end) {
+		range = getNavbarRange()
+	}
+
+	$: if (calendar) {
+		setDateRange()
+	}
+
+	const bindEvents = (calendar: Calendar) => {
+		calendar.on('clickEvent', (event) => {
+			$currentRecordId = event.event.id
+		})
+
+		calendar.on('selectDateTime', (event) => {
+			createRecordInitial.set({
+				[field.id.value]: event.start.toISOString(),
+			})
+			calendar.clearGridSelections()
+			createRecordModal.open()
+		})
+	}
+
+	function getNavbarRange() {
+		if (!start || !end) return ''
+		const middle = new Date(start.getTime() + (end.getTime() - start.getTime()) / 2)
+
+		return format(middle, 'yyyy-MM')
+	}
 
 	$: data = $listRecordFn(
 		[
@@ -25,7 +76,7 @@
 			},
 		],
 		{
-			enabled: !!start && !!end,
+			enabled: !!start?.toISOString() && !!end?.toISOString(),
 		},
 	)
 
@@ -63,85 +114,50 @@
 		}
 	}) satisfies EventObject[]
 
-	let range = ''
-	let calendar: Calendar | undefined = undefined
-	$: if (el) {
-		calendar = new Calendar(el, {
-			defaultView: 'month',
-			usageStatistics: false,
-			template: {
-				allday(event) {
-					return `<span class="text-white">${event.title}</span>`
-				},
-			},
-		})
-
-		setDateRange()
-	}
-
-	const setDateRange = () => {
-		if (!calendar) return
-		start = calendar.getDateRangeStart().toDate()
-		end = calendar.getDateRangeEnd().toDate()
-	}
-
-	$: if (start && end) {
-		range = getNavbarRange()
-	}
-
 	$: if (calendar) {
 		calendar.clear()
 		calendar.createEvents(events)
 		bindEvents(calendar)
 	}
-
-	const bindEvents = (calendar: Calendar) => {
-		calendar.on('clickEvent', (event) => {
-			$currentRecordId = event.event.id
-		})
-
-		calendar.on('selectDateTime', (event) => {
-			createRecordInitial.set({
-				[field.id.value]: event.start.toISOString(),
-			})
-			calendar.clearGridSelections()
-			createRecordModal.open()
-		})
-	}
-
-	function getNavbarRange() {
-		if (!start || !end) return ''
-		const middle = new Date(start.getTime() + (end.getTime() - start.getTime()) / 2)
-
-		return format(middle, 'yyyy-MM')
-	}
 </script>
 
 <div class="w-full h-full flex flex-col">
-	<div class="px-3 py-1.5 border-b flex items-center justify-center gap-4">
-		<Button
-			size="icon"
-			variant="outline"
-			on:click={() => {
-				calendar?.prev()
-				setDateRange()
-			}}
-		>
-			<i class="ti ti-chevron-left"></i>
-		</Button>
-		<span class="text-gray-700 text-sm">
-			{range}
-		</span>
-		<Button
-			size="icon"
-			variant="outline"
-			on:click={() => {
-				calendar?.next()
-				setDateRange()
-			}}
-		>
-			<i class="ti ti-chevron-right"></i>
-		</Button>
+	<div class="px-3 py-1.5 border-b flex items-center">
+		<div class="flex-1"></div>
+		<div class="flex gap-4 items-center">
+			<Button
+				size="icon"
+				variant="outline"
+				on:click={() => {
+					calendar?.prev()
+				}}
+			>
+				<i class="ti ti-chevron-left"></i>
+			</Button>
+			<span class="text-gray-700 text-sm">
+				{range}
+			</span>
+			<Button
+				size="icon"
+				variant="outline"
+				on:click={() => {
+					calendar?.next()
+				}}
+			>
+				<i class="ti ti-chevron-right"></i>
+			</Button>
+		</div>
+		<div class="flex-1 flex justify-end">
+			<Button
+				size="sm"
+				variant="outline"
+				on:click={() => {
+					calendar?.today()
+				}}
+			>
+				{$t('TODAY', { ns: 'common' })}
+			</Button>
+		</div>
 	</div>
 	<div bind:this={el} class="flex-1 w-full"></div>
 </div>
