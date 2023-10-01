6f4115b499ce974db2d2a5706c0749fbb5b9892c
nichenqin <nichenqin@hotmail.com>
2023-10-01 16:46:37 +0800

    refactor: template from tables handle foreign table

---
+++ apps/frontend/src/lib/base/BaseMenu.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Button } from '$components/ui/button'
+	import * as DropdownMenu from '$components/ui/dropdown-menu'
+	import { t } from '$lib/i18n'
+	import { confirmDeleteBase } from '$lib/store/modal'
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button variant="ghost" size="icon" builders={[builder]}>
+			<i class="ti ti-dots"></i>
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content class="w-40">
+		<DropdownMenu.Item class="text-red-500 text-xs gap-2" on:click={() => ($confirmDeleteBase = true)}>
+			<i class="ti ti-trash"></i>
+			<span>
+				{$t('Delete Base', { ns: 'base' })}
+			</span>
+		</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
