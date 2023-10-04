b49f5185ff5f1b3095d297366ff61a8b185e0acd
Sophie <wanshufen0414@gmail.com>
2023-10-04 16:18:45 +0800

    test: test create base command handler

--- packages/cqrs/src/commands/create-base/create-base.command.handler.test.ts
+++ packages/cqrs/src/commands/create-base/create-base.command.handler.test.ts
@@ -1,6 +1,6 @@
 import {
-  Base,
   BaseFactory,
+  BaseId,
   BaseRepository,
   ClsStore,
   IClsService,
@@ -14,7 +14,7 @@ import { MockProxy, mock } from 'vitest-mock-extended'
 import { CreateBaseCommand } from './create-base.command'
 import { CreateBaseCommandHandler } from './create-base.command.handler'
 import { SpyInstance } from 'vitest'
-import { Some } from 'oxide.ts'
+import { Option, Some } from 'oxide.ts'
 
 describe('create base command handler test', () => {
   let cls: MockProxy<IClsService<ClsStore>>
@@ -22,43 +22,58 @@ describe('create base command handler test', () => {
   let tableRepo: MockProxy<ITableRepository>
   let recordRepo: MockProxy<IRecordRepository>
   let command: CreateBaseCommand
-  // let base: Base
   let handler: CreateBaseCommandHandler
   let spy: SpyInstance
+  let tables: Table[]
+  const table1 = createTestTable()
+  table1.id = TableId.fromOrCreate('tbl1')
+  const table2 = createTestTable()
+  table2.id = TableId.fromOrCreate('tbl2')
+  const tbl2Spy = vi.spyOn(table2, 'moveToBase')
+  const tbl1Spy = vi.spyOn(table1, 'moveToBase')
 
   beforeEach(() => {
     cls = mock<IClsService<ClsStore>>()
     repo = mock<BaseRepository>()
     tableRepo = mock<ITableRepository>()
     recordRepo = mock<IRecordRepository>()
-    command = new CreateBaseCommand({ name: 'baseName1', tableIds: ['tbl1', 'tbl2'] })
-    // base = new Base()
     spy = vi.spyOn(BaseFactory, 'new')
     handler = new CreateBaseCommandHandler(cls, repo, tableRepo, recordRepo)
   })
 
-  test('create base success', async () => {
-    const base = spy.mock.results[0]
-    console.log(222, base)
-    const table1 = createTestTable()
-    table1.id = TableId.fromOrCreate('tbl1')
-    const tbl1Spy = vi.spyOn(table1, 'moveToBase')
-    const table2 = createTestTable()
-    table2.id = TableId.fromOrCreate('tbl2')
-    const tbl2Spy = vi.spyOn(table2, 'moveToBase')
-    for (const table of [table1, table2]) {
-      tableRepo.findOneById.calledWith(table.id.value).mockResolvedValue(Some(table))
-    }
+  test('Successfully create base and updated the table', async () => {
+    tables = [table1]
+    command = new CreateBaseCommand({ name: 'baseName1', tableIds: ['tbl1'] })
+    tableRepo.findOneById.mockResolvedValue(Some(table1))
     await handler.execute(command)
-    // expect(repo.insert).toHaveBeenCalledWith(base.value)
-    // console.log(1111, tbl1Spy.mock.calls[0], tbl1Spy.mock.results[0].value)
-    // console.log(1111, tbl2Spy.mock.calls[0])
-    for (const table of [table1, table2]) {
-      if (table1.id.value === 'tbl1') {
-        expect(tableRepo.updateOneById).toHaveBeenCalledWith(table.id.value, tbl1Spy.mock.results[0].value)
-      } else {
-        expect(tableRepo.updateOneById).toHaveBeenCalledWith(table.id.value, tbl2Spy.mock.results[0].value)
-      }
-    }
+    const base = spy.mock.results[0].value
+    expect(repo.insert).toHaveBeenCalledWith(base)
+    const spec = tbl1Spy.mock.results[0].value
+    expect(tableRepo.updateOneById).toHaveBeenCalledWith(table1.id.value, spec.unwrap())
+  })
+
+  test('Successfully create base without the need to update the table', async () => {
+    tables = [table1]
+    table1.baseId = Option(BaseId.fromOrCreate('basId1'))
+    command = new CreateBaseCommand({ name: 'baseName1', id: 'basId1', tableIds: ['tbl1'] })
+    tableRepo.findOneById.mockResolvedValue(Some(table1))
+    await handler.execute(command)
+    const base = spy.mock.results[0].value
+    expect(repo.insert).toHaveBeenCalledWith(base)
+    expect(tableRepo.updateOneById).not.toHaveBeenCalled()
+  })
+
+  test('Successfully create base and updated the table once time', async () => {
+    tables = [table1, table2]
+    table1.baseId = Option(BaseId.fromOrCreate('basId1'))
+    command = new CreateBaseCommand({ name: 'baseName1', id: 'basId1', tableIds: ['tbl1', 'tbl2'] })
+    tableRepo.findOneById.mockResolvedValueOnce(Some(table1)).mockResolvedValueOnce(Some(table2))
+    await handler.execute(command)
+    const base = spy.mock.results[0].value
+    expect(repo.insert).toHaveBeenCalledWith(base)
+    expect(tableRepo.findOneById).toHaveBeenLastCalledWith('tbl2')
+    const spec = tbl2Spy.mock.results[0].value
+    expect(tableRepo.updateOneById).toHaveBeenCalledOnce()
+    expect(tableRepo.updateOneById).toHaveBeenCalledWith(table2.id.value, spec.unwrap())
   })
 })
