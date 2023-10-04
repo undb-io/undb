5ae4b734dae4e69158798d4218d32b9bd11f80f8
Sophie <wanshufen0414@gmail.com>
2023-10-04 13:31:17 +0800

    test: test delete base command handler

--- packages/cqrs/src/commands/delete-base/delete-base.command.handler.test.ts
+++ packages/cqrs/src/commands/delete-base/delete-base.command.handler.test.ts
@@ -30,13 +30,13 @@ describe('delete base command handler test', () => {
     tableRepo = mock<ITableRepository>()
     base = new Base()
     base.id = BaseId.fromOrCreate('basId1')
+    table1.baseId = Option(base.id)
     command = new DeleteBaseCommand({ id: 'basId1' })
     handler = new DeleteBaseCommandHandler(repo, tableRepo)
   })
 
   test('Successfully deleted the base and updated the table', async () => {
     tables = [table1]
-    table1.baseId = Option(base.id)
     repo.findOneById.calledWith('basId1')
     repo.findOneById.mockResolvedValue(Some(base))
     const spy1 = vi.spyOn(table1, 'withoutBase')
@@ -58,5 +58,17 @@ describe('delete base command handler test', () => {
     expect(repo.deleteOneById).toHaveBeenCalledWith(base.id.value)
     await expect(tableRepo.find(new WithTableBaseId(Some(base.id)))).resolves.toEqual([table2])
     expect(tableRepo.updateOneById).not.toHaveBeenCalled()
+  })
+
+  test('Successfully deleted the base without the need to update the table', async () => {
+    tables = [table1, table2]
+    repo.findOneById.calledWith('basId1')
+    repo.findOneById.mockResolvedValue(Some(base))
+    tableRepo.find.mockResolvedValue(tables)
+    await handler.execute(command)
+    expect(repo.findOneById).toHaveBeenCalledWith(command.id)
+    expect(repo.deleteOneById).toHaveBeenCalledWith(base.id.value)
+    await expect(tableRepo.find(new WithTableBaseId(Some(base.id)))).resolves.toEqual([table1, table2])
+    expect(tableRepo.updateOneById).toHaveBeenCalledOnce
   })
 })
