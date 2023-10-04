56c8b5cdd5b86a48c47823b6edb299f39f677cab
Sophie <wanshufen0414@gmail.com>
2023-10-05 01:30:00 +0800

    test: test delete base command handler

--- packages/cqrs/src/commands/delete-base/delete-base.command.handler.test.ts
+++ packages/cqrs/src/commands/delete-base/delete-base.command.handler.test.ts
@@ -4,14 +4,15 @@ import {
   BaseRepository,
   ITableRepository,
   Table,
-  TableId,
   WithTableBaseId,
+  WithTableId,
   createTestTable,
 } from '@undb/core'
 import { MockProxy, mock } from 'vitest-mock-extended'
 import { DeleteBaseCommand } from './delete-base.command'
 import { DeleteBaseCommandHandler } from './delete-base.command.handler'
-import { Option, Some } from 'oxide.ts'
+import { Some } from 'oxide.ts'
+import { SpyInstance } from 'vitest'
 
 describe('delete base command handler test', () => {
   let repo: MockProxy<BaseRepository>
@@ -20,28 +21,29 @@ describe('delete base command handler test', () => {
   let handler: DeleteBaseCommandHandler
   let base: Base
   let tables: Table[]
-  const table1 = createTestTable()
-  table1.id = TableId.fromOrCreate('tbl1')
-  const table2 = createTestTable()
-  table2.id = TableId.fromOrCreate('tbl2')
+  let spy1: SpyInstance
+  let spy2: SpyInstance
+  const table1 = createTestTable(WithTableId.fromExistingString('tbl1').unwrap(), WithTableBaseId.fromString('basId1'))
+  const table2 = createTestTable(WithTableId.fromExistingString('tbl2').unwrap())
 
   beforeEach(() => {
     repo = mock<BaseRepository>()
     tableRepo = mock<ITableRepository>()
     base = new Base()
     base.id = BaseId.fromOrCreate('basId1')
-    table1.baseId = Option(base.id)
+    spy1 = vi.spyOn(table1, 'withoutBase')
+    spy2 = vi.spyOn(table2, 'withoutBase')
     command = new DeleteBaseCommand({ id: 'basId1' })
     handler = new DeleteBaseCommandHandler(repo, tableRepo)
   })
 
   test('Successfully deleted the base and updated the table', async () => {
     tables = [table1]
-    repo.findOneById.calledWith('basId1')
-    repo.findOneById.mockResolvedValue(Some(base))
-    const spy1 = vi.spyOn(table1, 'withoutBase')
+    repo.findOneById.calledWith('basId1').mockResolvedValue(Some(base))
     tableRepo.find.mockResolvedValue(tables)
+
     await handler.execute(command)
+
     expect(repo.findOneById).toHaveBeenCalledWith(command.id)
     expect(repo.deleteOneById).toHaveBeenCalledWith(base.id.value)
     await expect(tableRepo.find(new WithTableBaseId(Some(base.id)))).resolves.toEqual([table1])
@@ -50,25 +52,29 @@ describe('delete base command handler test', () => {
 
   test('Successfully deleted the base without the need to update the table', async () => {
     tables = [table2]
-    repo.findOneById.calledWith('basId1')
-    repo.findOneById.mockResolvedValue(Some(base))
+    repo.findOneById.calledWith('basId1').mockResolvedValue(Some(base))
     tableRepo.find.mockResolvedValue(tables)
+
     await handler.execute(command)
+
     expect(repo.findOneById).toHaveBeenCalledWith(command.id)
     expect(repo.deleteOneById).toHaveBeenCalledWith(base.id.value)
     await expect(tableRepo.find(new WithTableBaseId(Some(base.id)))).resolves.toEqual([table2])
+    expect(spy2.mock.results[0].value.isNone()).toBe(true)
     expect(tableRepo.updateOneById).not.toHaveBeenCalled()
   })
 
-  test('Successfully deleted the base without the need to update the table', async () => {
+  test('Successfully deleted the base and updated the table only once', async () => {
     tables = [table1, table2]
-    repo.findOneById.calledWith('basId1')
-    repo.findOneById.mockResolvedValue(Some(base))
+    repo.findOneById.calledWith('basId1').mockResolvedValue(Some(base))
     tableRepo.find.mockResolvedValue(tables)
+
     await handler.execute(command)
+
     expect(repo.findOneById).toHaveBeenCalledWith(command.id)
     expect(repo.deleteOneById).toHaveBeenCalledWith(base.id.value)
     await expect(tableRepo.find(new WithTableBaseId(Some(base.id)))).resolves.toEqual([table1, table2])
-    expect(tableRepo.updateOneById).toHaveBeenCalledOnce
+    expect(spy2.mock.results[0].value.isNone()).toBe(true)
+    expect(tableRepo.updateOneById).toHaveBeenCalledOnce()
   })
 })
