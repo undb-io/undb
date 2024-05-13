2b514bc422cb6c3d32f03ec001a0bb7dd2fb96cb
nichenqin <nichenqin@hotmail.com>
2024-05-13 22:29:27 +0800

    feat: resizable grid

--- apps/frontend/src/routes/(authed)/+layout.svelte
+++ apps/frontend/src/routes/(authed)/+layout.svelte
@@ -11,6 +11,7 @@
 	import Users from 'lucide-svelte/icons/users';
 
 	import { Badge } from '$lib/components/ui/badge/index.js';
+	import * as Resizable from '$lib/components/ui/resizable';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
@@ -36,8 +37,13 @@
 	}
 </script>
 
-<div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-	<div class="bg-muted/40 hidden border-r md:block">
+<Resizable.PaneGroup direction="horizontal">
+	<Resizable.Pane
+		class="bg-muted/40 hidden border-r md:block"
+		defaultSize={20}
+		minSize={15}
+		maxSize={30}
+	>
 		<div class="flex h-full max-h-screen flex-col gap-2">
 			<div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
 				<a href="/" class="flex items-center gap-2 font-semibold">
@@ -53,101 +59,106 @@
 				<TablesNav tables={data.tables} />
 			</div>
 			<div class={cn('p-4', data.tables.length ? 'mt-auto' : '')}>
-				<CreateTableButton variant={data.tables.length ? "outline": "default"} />
+				<CreateTableButton variant={data.tables.length ? 'outline' : 'default'} />
 			</div>
 		</div>
-	</div>
-	<div class="flex flex-col">
-		<header class="bg-muted/40 flex h-14 items-center gap-4 border-b px-4 lg:h-[60px] lg:px-6">
-			<Sheet.Root>
-				<Sheet.Trigger asChild let:builder>
-					<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
-						<Menu class="h-5 w-5" />
-						<span class="sr-only">Toggle navigation menu</span>
-					</Button>
-				</Sheet.Trigger>
-				<Sheet.Content side="left" class="flex flex-col">
-					<nav class="grid gap-2 text-lg font-medium">
-						<a href="##" class="flex items-center gap-2 text-lg font-semibold">
-							<Package2 class="h-6 w-6" />
-							<span class="sr-only">Acme Inc</span>
-						</a>
-						<a
-							href="##"
-							class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
-						>
-							<Home class="h-5 w-5" />
-							Dashboard
-						</a>
-						<a
-							href="##"
-							class="bg-muted text-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
-						>
-							<ShoppingCart class="h-5 w-5" />
-							Orders
-							<Badge class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-								6
-							</Badge>
-						</a>
-						<a
-							href="##"
-							class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
-						>
-							<Package class="h-5 w-5" />
-							Products
-						</a>
-						<a
-							href="##"
-							class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
-						>
-							<Users class="h-5 w-5" />
-							Customers
-						</a>
-						<a
-							href="##"
-							class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
-						>
-							<LineChart class="h-5 w-5" />
-							Analytics
-						</a>
-					</nav>
-					<div class="mt-auto">
-						<CreateTableButton />
-					</div>
-				</Sheet.Content>
-			</Sheet.Root>
-			<div class="w-full flex-1">
-				<form>
-					<div class="relative">
-						<Search class="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
-						<Input
-							type="search"
-							placeholder="Search products..."
-							class="bg-background w-full appearance-none pl-8 shadow-none md:w-2/3 lg:w-1/3"
-						/>
-					</div>
-				</form>
-			</div>
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger asChild let:builder>
-					<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
-						<CircleUser class="h-5 w-5" />
-						<span class="sr-only">Toggle user menu</span>
-					</Button>
-				</DropdownMenu.Trigger>
-				<DropdownMenu.Content align="end">
-					<DropdownMenu.Label>My Account</DropdownMenu.Label>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item>Settings</DropdownMenu.Item>
-					<DropdownMenu.Item>Support</DropdownMenu.Item>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item>Logout</DropdownMenu.Item>
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
-		</header>
-		<slot />
-	</div>
-</div>
+	</Resizable.Pane>
+	<Resizable.Handle />
+	<Resizable.Pane class="grid min-h-screen" defaultSize={80}>
+		<div class="flex w-full flex-col">
+			<header class="bg-muted/40 flex h-14 items-center gap-4 border-b px-4 lg:h-[60px] lg:px-6">
+				<Sheet.Root>
+					<Sheet.Trigger asChild let:builder>
+						<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
+							<Menu class="h-5 w-5" />
+							<span class="sr-only">Toggle navigation menu</span>
+						</Button>
+					</Sheet.Trigger>
+					<Sheet.Content side="left" class="flex flex-col">
+						<nav class="grid gap-2 text-lg font-medium">
+							<a href="##" class="flex items-center gap-2 text-lg font-semibold">
+								<Package2 class="h-6 w-6" />
+								<span class="sr-only">Acme Inc</span>
+							</a>
+							<a
+								href="##"
+								class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
+							>
+								<Home class="h-5 w-5" />
+								Dashboard
+							</a>
+							<a
+								href="##"
+								class="bg-muted text-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
+							>
+								<ShoppingCart class="h-5 w-5" />
+								Orders
+								<Badge
+									class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full"
+								>
+									6
+								</Badge>
+							</a>
+							<a
+								href="##"
+								class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
+							>
+								<Package class="h-5 w-5" />
+								Products
+							</a>
+							<a
+								href="##"
+								class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
+							>
+								<Users class="h-5 w-5" />
+								Customers
+							</a>
+							<a
+								href="##"
+								class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
+							>
+								<LineChart class="h-5 w-5" />
+								Analytics
+							</a>
+						</nav>
+						<div class="mt-auto">
+							<CreateTableButton />
+						</div>
+					</Sheet.Content>
+				</Sheet.Root>
+				<div class="w-full flex-1">
+					<form>
+						<div class="relative">
+							<Search class="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
+							<Input
+								type="search"
+								placeholder="Search products..."
+								class="bg-background w-full appearance-none pl-8 shadow-none md:w-2/3 lg:w-1/3"
+							/>
+						</div>
+					</form>
+				</div>
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger asChild let:builder>
+						<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
+							<CircleUser class="h-5 w-5" />
+							<span class="sr-only">Toggle user menu</span>
+						</Button>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content align="end">
+						<DropdownMenu.Label>My Account</DropdownMenu.Label>
+						<DropdownMenu.Separator />
+						<DropdownMenu.Item>Settings</DropdownMenu.Item>
+						<DropdownMenu.Item>Support</DropdownMenu.Item>
+						<DropdownMenu.Separator />
+						<DropdownMenu.Item>Logout</DropdownMenu.Item>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</header>
+			<slot />
+		</div>
+	</Resizable.Pane>
+</Resizable.PaneGroup>
 
 <CreateTableSheet data={data.createTableForm} />
 
