0d878fd4f1d9c0767efbc074e8a4360686791742
nichenqin <nichenqin@hotmail.com>
2024-05-14 14:49:38 +0800

    fix: preload data

--- apps/frontend/src/routes/(authed)/t/[tableId]/+layout.svelte
+++ apps/frontend/src/routes/(authed)/t/[tableId]/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { page } from "$app/stores"
   import { tableCreator } from "@undb/table"
   import { setTable } from "$lib/store/table.store"
   import type { LayoutData } from "./$types"
@@ -7,9 +8,12 @@
   export let data: LayoutData
 
   const table = writable(tableCreator.fromJSON(data.table))
-  setTable(table)
-
-  $: table.set(tableCreator.fromJSON(data.table))
+  $: {
+    if ($page.params.tableId === $table.id.value) {
+      table.set(tableCreator.fromJSON(data.table))
+      setTable(table)
+    }
+  }
 </script>
 
 <main class="flex flex-1 flex-col gap-4 px-4 py-2 lg:gap-6 lg:px-6 lg:py-4">
