1a3e46b1503e2f70b833ff810cbc753d579e98c1
nichenqin <nichenqin@hotmail.com>
2024-06-02 10:40:21 +0800

    feat: Add Create Webhook dialog with table name in header

--- apps/frontend/src/lib/components/blocks/webhook/create-webhook-dialog.svelte
+++ apps/frontend/src/lib/components/blocks/webhook/create-webhook-dialog.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import * as Dialog from "$lib/components/ui/dialog"
   import { CREATE_WEBHOOK_MODAL, closeModal, isModalOpen } from "$lib/store/modal.store"
+  import { getTable } from "$lib/store/table.store"
   import CreateWebhook from "./create-webhook.svelte"
+
+  const table = getTable()
 </script>
 
 <Dialog.Root
@@ -13,6 +16,13 @@
   }}
 >
   <Dialog.Content>
+    <Dialog.Header>
+      <span class="text-md inline-flex">
+        Create Webhook for <strong class="ml-2">
+          {$table.name.value}
+        </strong>
+      </span>
+    </Dialog.Header>
     <CreateWebhook />
   </Dialog.Content>
 </Dialog.Root>
