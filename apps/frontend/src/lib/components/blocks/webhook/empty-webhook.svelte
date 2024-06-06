29e5d539cf786ddc29a34d5d0d827bd00be314af
nichenqin <nichenqin@hotmail.com>
2024-06-06 09:15:53 +0800

    fix: ui

--- apps/frontend/src/lib/components/blocks/webhook/empty-webhook.svelte
+++ apps/frontend/src/lib/components/blocks/webhook/empty-webhook.svelte
@@ -8,7 +8,7 @@
     data-x-chunk-name="dashboard-02-chunk-1"
     data-x-chunk-description="An empty state showing no products with a heading, description and a call to action to add a product."
   >
-    <div class="flex flex-col items-center gap-1 text-center">
+    <div class="flex flex-col items-center gap-3 text-center">
       <h3 class="text-2xl font-bold tracking-tight">You have no webhooks</h3>
       <p class="text-muted-foreground text-sm">Click button to create your first webhook</p>
       <CreateWebhookButton />
