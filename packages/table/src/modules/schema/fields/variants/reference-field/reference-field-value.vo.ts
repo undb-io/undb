1cc9c8bb0b55ef65b5d240deaa226cff2993b7ca
nichenqin <nichenqin@hotmail.com>
2024-06-06 09:38:18 +0800

    wip: reference mutate reference field value

--- packages/table/src/modules/schema/fields/variants/reference-field/reference-field-value.vo.ts
+++ packages/table/src/modules/schema/fields/variants/reference-field/reference-field-value.vo.ts
@@ -1,4 +1,26 @@
 import { ValueObject } from "@undb/domain"
+import { z } from "@undb/zod"
+import { recordId } from "../../../../records/record/record-id.vo"
+
+const referenceFieldValueItem = recordId
+
+export const mutateReferenceFieldValueSchema = z.union([
+  referenceFieldValueItem.array().optional().nullable(),
+  z.object({
+    type: z.literal("set"),
+    value: referenceFieldValueItem.array().nullable(),
+  }),
+  z.object({
+    type: z.literal("append"),
+    value: referenceFieldValueItem.array().nullable().or(referenceFieldValueItem),
+  }),
+  z.object({
+    type: z.literal("prepend"),
+    value: referenceFieldValueItem.array().nullable().or(referenceFieldValueItem),
+  }),
+])
+
+export type IMutateReferenceFieldValueSchema = z.infer<typeof mutateReferenceFieldValueSchema>
 
 export class ReferenceFieldValue extends ValueObject<string[]> {
   constructor(value: string[]) {
