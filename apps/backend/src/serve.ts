3e0e5e9e446dfb89b4a80bd0302ba6a0d2c01fa2
nichenqin <nichenqin@hotmail.com>
2024-06-23 13:01:17 +0800

    chore: Update serve.ts to remove unnecessary await keyword

--- apps/backend/src/serve.ts
+++ apps/backend/src/serve.ts
@@ -1,7 +1,7 @@
 import { app } from "./app"
 
 export const serve = async () => {
-  await app.listen(Bun.env.PORT ?? 4000, () => {
+  app.listen(Bun.env.PORT ?? 4000, () => {
     app.decorator.logger.info(`App is running at ${app.server?.hostname}:${app.server?.port}`)
   })
 }
