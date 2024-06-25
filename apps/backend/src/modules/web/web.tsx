e7ae4b5558dad4f7cb75d0be8adec812de83ff33
nichenqin <nichenqin@hotmail.com>
2024-06-25 21:30:52 +0800

    feat: Add static plugin for serving static assets

--- apps/backend/src/modules/web/web.tsx
+++ apps/backend/src/modules/web/web.tsx
@@ -1,9 +1,13 @@
+import staticPlugin from "@elysiajs/static"
 import { singleton } from "@undb/di"
 import Elysia from "elysia"
 
 @singleton()
 export class Web {
   route() {
-    return new Elysia().get("/", () => Bun.file("dist/index.html")).get("/t/*", () => Bun.file("dist/index.html"))
+    return new Elysia()
+      .use(staticPlugin({ prefix: "/", assets: "dist" }))
+      .get("/", () => Bun.file("dist/index.html"))
+      .get("/t/*", () => Bun.file("dist/index.html"))
   }
 }
