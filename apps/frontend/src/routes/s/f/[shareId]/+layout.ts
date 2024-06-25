1bc765d867b44a7787ace4ba70d6e3c01b0683ab
nichenqin <nichenqin@hotmail.com>
2024-06-25 20:54:40 +0800

    chore: build

--- apps/frontend/src/routes/s/f/[shareId]/+layout.ts
+++ apps/frontend/src/routes/s/f/[shareId]/+layout.ts
@@ -1,6 +1,8 @@
 import { GetFormShareDataStore } from "$houdini"
 import type { LayoutLoad } from "./$types"
 
+export const prerender = "auto"
+
 export const load: LayoutLoad = async (event) => {
   const getFormShareData = new GetFormShareDataStore()
 
