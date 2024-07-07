ddc3ad02202792fa74cb4054e090234f5f36423e
nichenqin <nichenqin@hotmail.com>
2024-07-07 10:43:08 +0800

    feat: user field value cell

--- apps/frontend/src/lib/components/blocks/field-value/user-field.svelte
+++ apps/frontend/src/lib/components/blocks/field-value/user-field.svelte
@@ -3,6 +3,7 @@
   import * as HoverCard from "$lib/components/ui/hover-card"
   import UserCard from "../user/user-card.svelte"
   import type { ISingleUserFieldValue, IUserFieldDisplayValue } from "@undb/table"
+  import * as Avatar from "$lib/components/ui/avatar"
 
   export let value: ISingleUserFieldValue = null
   export let displayValue: IUserFieldDisplayValue = null
@@ -18,10 +19,17 @@
         <span
           data-field-value={value}
           class={cn(
-            "inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10",
+            "relative inline-flex items-center rounded-full bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10",
           )}
         >
-          {displayValue?.username}
+          <Avatar.Root class="absolute left-0 h-6 w-6 border">
+            <Avatar.Image src="" alt={displayValue.username} />
+            <Avatar.Fallback>{displayValue.username.slice(0, 2)}</Avatar.Fallback>
+          </Avatar.Root>
+
+          <span class="ml-5">
+            {displayValue?.username}
+          </span>
         </span>
       {/if}
     </HoverCard.Trigger>
