d444309f2b00f6ff953cfd4c3628844fb0813795
nichenqin <nichenqin@hotmail.com>
2024-07-20 22:20:02 +0800

    chore: docker file

--- apps/frontend/src/lib/components/blocks/table-header/table-header.svelte
+++ apps/frontend/src/lib/components/blocks/table-header/table-header.svelte
@@ -203,22 +203,5 @@
         </Tabs.Trigger>
       </Tabs.List>
     </Tabs.Root>
-
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild let:builder>
-        <Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
-          <CircleUser class="h-5 w-5" />
-          <span class="sr-only">Toggle user menu</span>
-        </Button>
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Content align="end">
-        <DropdownMenu.Label>My Account</DropdownMenu.Label>
-        <DropdownMenu.Separator />
-        <DropdownMenu.Item>Settings</DropdownMenu.Item>
-        <DropdownMenu.Item>Support</DropdownMenu.Item>
-        <DropdownMenu.Separator />
-        <DropdownMenu.Item>Logout</DropdownMenu.Item>
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
   </div>
 </header>
