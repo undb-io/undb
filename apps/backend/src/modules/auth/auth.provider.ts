f258e15291407fa4e1f68a41408543cd3e6634d4
nichenqin <nichenqin@hotmail.com>
2024-08-08 10:14:22 +0800

    feat: support local sqlite database

--- apps/backend/src/modules/auth/auth.provider.ts
+++ apps/backend/src/modules/auth/auth.provider.ts
@@ -1,39 +1,60 @@
-import { LibSQLAdapter } from "@lucia-auth/adapter-sqlite"
-import { container, inject } from "@undb/di"
-import { sqlite } from "@undb/persistence"
-import { Lucia } from "lucia"
+import { BunSQLiteAdapter, LibSQLAdapter } from "@lucia-auth/adapter-sqlite"
+import { container, inject, instanceCachingFactory } from "@undb/di"
+import { env } from "@undb/env"
+import { Client } from "@undb/persistence"
+import { SQLITE_CLIENT } from "@undb/persistence/src/client"
+import Database from "bun:sqlite"
+import { Adapter, Lucia } from "lucia"
 
 export const LUCIA_PROVIDER = Symbol.for("LUCIA_PROVIDER")
+export const injectLucia = () => inject(LUCIA_PROVIDER)
 
-const adapter = new LibSQLAdapter(sqlite, {
-  user: "undb_user",
-  session: "undb_session",
-})
-
-export const lucia = new Lucia(adapter, {
-  sessionCookie: {
-    attributes: {
-      secure: Bun.env.NODE_ENV === "PRODUCTION", // set `Secure` flag in HTTPS
+const createLuciaWithAdapter = (adapter: Adapter) => {
+  const lucia = new Lucia(adapter, {
+    sessionCookie: {
+      attributes: {
+        secure: Bun.env.NODE_ENV === "PRODUCTION", // set `Secure` flag in HTTPS
+      },
     },
-  },
-  getSessionAttributes: (attributes) => {
-    return {
-      spaceId: attributes.space_id,
-    }
-  },
-  getUserAttributes: (attributes) => {
-    return {
-      emailVerified: Boolean(attributes.email_verified),
-      email: attributes.email,
-      username: attributes.username,
-      avatar: attributes.avatar,
-    }
-  },
-})
+    getSessionAttributes: (attributes) => {
+      return {
+        spaceId: attributes.space_id,
+      }
+    },
+    getUserAttributes: (attributes) => {
+      return {
+        emailVerified: Boolean(attributes.email_verified),
+        email: attributes.email,
+        username: attributes.username,
+        avatar: attributes.avatar,
+      }
+    },
+  })
+
+  return lucia
+}
+
+const createTursoLucia = (sqlite: Client) => {
+  const adapter = new LibSQLAdapter(sqlite, {
+    user: "undb_user",
+    session: "undb_session",
+  })
+
+  return createLuciaWithAdapter(adapter)
+}
+
+const createSqliteLucia = (sqlite: Database) => {
+  const adapter = new BunSQLiteAdapter(sqlite, {
+    user: "undb_user",
+    session: "undb_session",
+  })
+
+  return createLuciaWithAdapter(adapter)
+}
 
 declare module "lucia" {
   interface Register {
-    Lucia: typeof lucia
+    Lucia: ReturnType<typeof createTursoLucia>
     DatabaseSessionAttributes: DatabaseSessionAttributes
     DatabaseUserAttributes: {
       email: string
@@ -47,6 +68,13 @@ declare module "lucia" {
   }
 }
 
-container.register(LUCIA_PROVIDER, { useValue: lucia })
-
-export const injectLucia = () => inject(LUCIA_PROVIDER)
+container.register(LUCIA_PROVIDER, {
+  useFactory: instanceCachingFactory((c) => {
+    if (env.UNDB_DB_PROVIDER === "sqlite") {
+      const sqlite = c.resolve<Database>(SQLITE_CLIENT)
+      return createSqliteLucia(sqlite)
+    }
+    const sqlite = c.resolve<Client>(SQLITE_CLIENT)
+    return createTursoLucia(sqlite)
+  }),
+})
--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,7 +1,7 @@
 import { createEnv } from "@t3-oss/env-core"
 import { z } from "@undb/zod"
 
-const dbEnv = createEnv({
+const tursoEnv = createEnv({
   server: {
     UNDB_DB_TURSO_URL: z.string().optional(),
     UNDB_DB_TURSO_AUTH_TOKEN: z.string().optional(),
@@ -64,14 +64,8 @@ const storageEnv = createEnv({
   emptyStringAsUndefined: true,
 })
 
-export const env = createEnv({
-  shared: {
-    LOG_LEVEL: z.enum(["info", "debug", "error"]).default("info"),
-  },
-  clientPrefix: "UNDB_PUBLIC_",
-  client: {},
+const emailEnv = createEnv({
   server: {
-    UNDB_BASE_URL: z.string().optional(),
     UNDB_MAIL_HOST: z.string().optional(),
     UNDB_MAIL_PORT: z.string().optional(),
     UNDB_MAIL_DEFAULT_FROM: z.string().optional(),
@@ -86,5 +80,26 @@ export const env = createEnv({
   },
   runtimeEnv: import.meta.env,
   emptyStringAsUndefined: true,
-  extends: [dbEnv, oauthEnv, storageEnv, s3Env, minioEnv],
+})
+
+const dbEnv = createEnv({
+  server: {
+    UNDB_DB_PROVIDER: z.enum(["sqlite", "turso"]).default("sqlite").optional(),
+  },
+  runtimeEnv: import.meta.env,
+  emptyStringAsUndefined: true,
+})
+
+export const env = createEnv({
+  shared: {
+    LOG_LEVEL: z.enum(["info", "debug", "error"]).default("info"),
+  },
+  clientPrefix: "UNDB_PUBLIC_",
+  client: {},
+  server: {
+    UNDB_BASE_URL: z.string().optional(),
+  },
+  runtimeEnv: import.meta.env,
+  emptyStringAsUndefined: true,
+  extends: [tursoEnv, dbEnv, oauthEnv, storageEnv, s3Env, emailEnv, minioEnv],
 })
