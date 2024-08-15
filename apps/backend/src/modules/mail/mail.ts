8567d9313e808f041121dff80011d06dad7541d2
nichenqin <nichenqin@hotmail.com>
2024-08-15 22:56:00 +0800

    fix: fix mailgun

--- apps/backend/src/modules/mail/mail.ts
+++ apps/backend/src/modules/mail/mail.ts
@@ -2,14 +2,13 @@ import { singleton } from "@undb/di"
 import { env } from "@undb/env"
 import { createLogger } from "@undb/logger"
 import { IMailService, ISendInput } from "@undb/mail"
+import MailgunTransport from "mailgun-nodemailer-transport"
 import { createTransport } from "nodemailer"
-import mg from "nodemailer-mailgun-transport"
-import SMTPConnection from "nodemailer/lib/smtp-connection"
 import { compile } from "./templates/compile"
 
 function createMailerTransport() {
   if (env.UNDB_MAIL_PROVIDER === "nodemailer" || !env.UNDB_MAIL_PROVIDER) {
-    const options: SMTPConnection.Options = {
+    const options = {
       host: env.UNDB_MAIL_HOST,
       port: 465,
       // port: env.UNDB_MAIL_PORT ? parseInt(env.UNDB_MAIL_PORT, 10) : undefined,
@@ -25,9 +24,9 @@ function createMailerTransport() {
   }
 
   return createTransport(
-    mg({
+    new MailgunTransport({
       auth: {
-        api_key: env.UNDB_MAILGUN_API_KEY!,
+        apiKey: env.UNDB_MAILGUN_API_KEY!,
         domain: env.UNDB_MAILGUN_DOMAIN!,
       },
     }),
