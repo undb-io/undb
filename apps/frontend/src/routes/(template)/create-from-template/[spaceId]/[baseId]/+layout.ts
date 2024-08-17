7adc3613fc7505412c6e7bd0d77e95849f9c5209
nichenqin <nichenqin@hotmail.com>
2024-08-17 11:55:26 +0800

    fix: fix route prerender

--- apps/frontend/src/routes/(template)/create-from-template/[spaceId]/[baseId]/+layout.ts
+++ apps/frontend/src/routes/(template)/create-from-template/[spaceId]/[baseId]/+layout.ts
@@ -1,6 +1,9 @@
 import { GetCreateFromTemplateDataStore } from "$houdini"
 import type { LayoutLoad } from "./$types"
 
+export const ssr = false
+export const prerender = "auto"
+
 export const load: LayoutLoad = async (event) => {
   const { spaceId, baseId } = event.params
 
