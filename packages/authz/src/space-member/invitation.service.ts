f2cef6058c3b48844f8ba1d5bf60f135332141d4
nichenqin <nichenqin@hotmail.com>
2024-08-19 11:31:00 +0800

    chore: invitation space id

--- packages/authz/src/space-member/invitation.service.ts
+++ packages/authz/src/space-member/invitation.service.ts
@@ -2,6 +2,7 @@ import { singleton } from "@undb/di"
 import { env } from "@undb/env"
 import { createLogger } from "@undb/logger"
 import { injectMailService, type IMailService } from "@undb/mail"
+import { injectSpaceService, type ISpaceService } from "@undb/space"
 import type { InvitationDTO } from "./dto"
 
 export interface IInvitationMailService {
@@ -14,20 +15,25 @@ export class InvitationMailService implements IInvitationMailService {
   constructor(
     @injectMailService()
     private readonly svc: IMailService,
+    @injectSpaceService()
+    private readonly spaceService: ISpaceService,
   ) {}
 
   async invite(invitation: InvitationDTO, username: string): Promise<void> {
     this.logger.info(invitation, "sending invitation mail...")
 
+    const space = (await this.spaceService.getSpace({ spaceId: invitation.spaceId })).expect("space not found")
+
     await this.svc.send({
       template: "invite",
       to: invitation.email,
-      subject: "You have been invited to join the workspace",
+      subject: `You have been invited to join the space ${space.name.value}`,
       data: {
         invite_sender_name: username,
+        space_name: space.name.value,
         email: invitation.email,
         action_url: new URL(`/invitation/${invitation.id}/accept`, env.UNDB_BASE_URL).toString(),
-        help_url: "",
+        help_url: "https://undb.io",
       },
     })
   }
