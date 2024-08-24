740c5003899dfe40e076602924f1862c971edda9
nichenqin <nichenqin@hotmail.com>
2024-08-24 11:59:24 +0800

    feat: readonly form ui

--- apps/frontend/src/lib/components/blocks/base/share-base-nav.svelte
+++ apps/frontend/src/lib/components/blocks/base/share-base-nav.svelte
@@ -25,79 +25,91 @@
   $: viewId = $page.params.viewId
   $: baseTables = base.tables.filter((t) => t !== null) ?? []
 
+  $: active = !tableId && !viewId
   let open: Record<string, boolean> = {}
 </script>
 
-<a href={`/s/b/${shareId}`} class={cn("group flex h-8 items-center justify-between pl-4 pr-2 transition-all")}>
-  <div class={cn("flex h-full flex-1 items-center font-light")}>
-    <HardDriveIcon class="mr-2 h-4 w-4" />
-    {base.name}
-  </div>
-</a>
-{#each baseTables as table}
-  {#if table}
-    {@const active = table.id === tableId}
-    {@const views = table.views.filter((view) => !view.isDefault)}
-    <Collapsible.Root bind:open={open[table.id]}>
-      <div
-        class={cn(
-          "group flex h-8 cursor-pointer items-center justify-between rounded-md pl-8 pr-2 transition-all",
-          active && !viewId ? "bg-primary/90" : "hover:bg-gray-100",
-        )}
-      >
-        <a
-          href={`/s/b/${shareId}/t/${table.id}`}
+<nav class="grid items-start gap-1 px-1.5 text-sm font-medium">
+  <a
+    href={`/s/b/${shareId}`}
+    class={cn(
+      "group flex h-8 items-center justify-between pl-4 pr-2 transition-all",
+      active && "bg-primary/90 text-background rounded-md",
+    )}
+  >
+    <div class={cn("flex h-full flex-1 items-center font-light")}>
+      <HardDriveIcon class="mr-2 h-4 w-4" />
+      {base.name}
+    </div>
+  </a>
+  {#each baseTables as table}
+    {#if table}
+      {@const active = table.id === tableId}
+      {@const views = table.views.filter((view) => !view.isDefault)}
+      <Collapsible.Root bind:open={open[table.id]}>
+        <div
           class={cn(
-            "text-primary flex h-full flex-1 items-center font-light",
-            active && !viewId && "text-background font-medium",
+            "group flex h-8 cursor-pointer items-center justify-between rounded-md pl-8 pr-2 transition-all",
+            active && !viewId ? "bg-primary/90" : "hover:bg-gray-100",
           )}
         >
-          <DatabaseIcon class="mr-2 h-4 w-4" />
-          {table.name}
-        </a>
-        <div class="flex items-center gap-2 opacity-0 transition-all group-hover:opacity-100">
-          {#if views.length > 0}
-            <Collapsible.Trigger
-              on:click={(e) => {
-                e.preventDefault()
-                e.stopPropagation()
-                open[table.id] = !open[table.id]
-              }}
-              class={cn(
-                "flex h-5 w-5 items-center justify-center rounded-md hover:bg-gray-200",
-                active && !viewId && "hover:bg-primary",
-              )}
-            >
-              <ChevronRightIcon
-                class={cn(
-                  "text-muted-foreground h-4 w-4 transition-all",
-                  open[table.id] && "rotate-90",
-                  active && "text-background",
-                )}
-              />
-            </Collapsible.Trigger>
-          {/if}
-        </div>
-      </div>
-      <Collapsible.Content>
-        {#each views as view}
-          {@const active = view.id === viewId}
-          <div
+          <a
+            href={`/s/b/${shareId}/t/${table.id}`}
             class={cn(
-              "group flex h-8 items-center justify-between rounded-sm pl-14 pr-2 transition-all",
-              active ? "bg-primary/90" : "hover:bg-gray-100",
+              "text-primary flex h-full flex-1 items-center font-light",
+              active && !viewId && "text-background font-medium",
             )}
           >
-            <a
-              class={cn("flex h-full flex-1 items-center text-xs font-light", active && "text-background font-medium")}
-              href={`/s/b/${shareId}/t/${table.id}/${view.id}`}
-            >
-              <SheetIcon class="mr-2 h-4 w-4" />
-              {view.name}
-            </a>
+            <DatabaseIcon class="mr-2 h-4 w-4" />
+            {table.name}
+          </a>
+          <div class="flex items-center gap-2 opacity-0 transition-all group-hover:opacity-100">
+            {#if views.length > 0}
+              <Collapsible.Trigger
+                on:click={(e) => {
+                  e.preventDefault()
+                  e.stopPropagation()
+                  open[table.id] = !open[table.id]
+                }}
+                class={cn(
+                  "flex h-5 w-5 items-center justify-center rounded-md hover:bg-gray-200",
+                  active && !viewId && "hover:bg-primary",
+                )}
+              >
+                <ChevronRightIcon
+                  class={cn(
+                    "text-muted-foreground h-4 w-4 transition-all",
+                    open[table.id] && "rotate-90",
+                    active && "text-background",
+                  )}
+                />
+              </Collapsible.Trigger>
+            {/if}
           </div>
-        {/each}
-      </Collapsible.Content>
-    </Collapsible.Root>
-  {/if}
-{/each}
+        </div>
+        <Collapsible.Content>
+          {#each views as view}
+            {@const active = view.id === viewId}
+            <div
+              class={cn(
+                "group flex h-8 items-center justify-between rounded-sm pl-14 pr-2 transition-all",
+                active ? "bg-primary/90" : "hover:bg-gray-100",
+              )}
+            >
+              <a
+                class={cn(
+                  "flex h-full flex-1 items-center text-xs font-light",
+                  active && "text-background font-medium",
+                )}
+                href={`/s/b/${shareId}/t/${table.id}/${view.id}`}
+              >
+                <SheetIcon class="mr-2 h-4 w-4" />
+                {view.name}
+              </a>
+            </div>
+          {/each}
+        </Collapsible.Content>
+      </Collapsible.Root>
+    {/if}
+  {/each}
+</nav>
---
+++ apps/frontend/src/lib/components/blocks/table-header/share-table-header.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+  import * as Tabs from "$lib/components/ui/tabs"
+  import { DatabaseIcon, FormInputIcon, HardDriveIcon, SheetIcon, TextCursorInputIcon } from "lucide-svelte"
+  import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js"
+  import { getTable } from "$lib/store/table.store"
+  import { tab, isFormTab, formId } from "$lib/store/tab.store"
+  import RecordUpdating from "../record-updating/record-updating.svelte"
+  import { derived } from "svelte/store"
+  import { page } from "$app/stores"
+  import { getBaseById } from "$lib/store/base.store"
+
+  const table = getTable()
+  $: base = $getBaseById($table.baseId)
+
+  $: viewId = derived([page], ([$page]) => {
+    return $page.params.viewId
+  })
+  $: view = $table.views.getViewById($viewId)
+  $: forms = $table.forms?.props ?? []
+
+  $: currentFormId = $formId ? $formId : forms[0]?.id
+  $: currentForm = forms.find((form) => form.id === currentFormId)
+</script>
+
+<header class="bg-muted/40 flex h-12 items-center gap-4 border-b px-4 py-3 lg:px-6">
+  <div class="relative flex w-full flex-1 items-center justify-between">
+    <div class="relative flex items-center gap-4">
+      <Breadcrumb.Root>
+        <Breadcrumb.List>
+          {#if base}
+            <Breadcrumb.Item>
+              <Breadcrumb.Link class="flex items-center gap-2">
+                <HardDriveIcon class="h-4 w-4" />
+                {base.name}
+              </Breadcrumb.Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Separator />
+          {/if}
+          <Breadcrumb.Item>
+            <Breadcrumb.Link class="flex items-center gap-2">
+              <DatabaseIcon class="h-4 w-4" />
+              {$table.name.value}
+            </Breadcrumb.Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Separator />
+          {#if $isFormTab}
+            {#if forms.length}
+              <Breadcrumb.Item>
+                <FormInputIcon class="h-4 w-4" />
+                Forms
+              </Breadcrumb.Item>
+              <Breadcrumb.Separator />
+              <Breadcrumb.Item>
+                {currentForm?.name}
+              </Breadcrumb.Item>
+            {/if}
+          {:else}
+            <Breadcrumb.Item class="text-xs">
+              <Breadcrumb.Page class="flex items-center gap-1">
+                {view.name.value}
+              </Breadcrumb.Page>
+            </Breadcrumb.Item>
+          {/if}
+        </Breadcrumb.List>
+      </Breadcrumb.Root>
+
+      <RecordUpdating />
+    </div>
+
+    {#if forms.length}
+      <Tabs.Root
+        value={$tab === "data" || !$tab ? "data" : $tab}
+        onValueChange={(value) => {
+          if (!$tab && value === "data") {
+            return
+          }
+
+          tab.set(value === "data" ? "" : (value ?? ""))
+        }}
+        class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+      >
+        <Tabs.List>
+          <Tabs.Trigger value="data">
+            <SheetIcon class="mr-2 h-4 w-4" />
+            Data
+          </Tabs.Trigger>
+          <Tabs.Trigger value="form">
+            <TextCursorInputIcon class="mr-2 h-4 w-4" />
+            Forms
+          </Tabs.Trigger>
+        </Tabs.List>
+      </Tabs.Root>
+    {/if}
+  </div>
+</header>
