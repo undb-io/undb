48d2c721ac2492c84cb14c9029602b2a88efb383
nichenqin <nichenqin@hotmail.com>
2024-09-17 16:56:11 +0800

    fix: fix import table data

--- apps/frontend/src/lib/components/blocks/import-table/import-table-dialog.svelte
+++ apps/frontend/src/lib/components/blocks/import-table/import-table-dialog.svelte
@@ -2,11 +2,13 @@
   import * as Dialog from "$lib/components/ui/dialog"
   import { IMPORT_TABLE_MODAL, isModalOpen, toggleModal } from "$lib/store/modal.store"
   import ImportTable from "./import-table.svelte"
+
+  export let tableNames: string[]
 </script>
 
 <Dialog.Root open={$isModalOpen(IMPORT_TABLE_MODAL)} onOpenChange={(open) => toggleModal(IMPORT_TABLE_MODAL)}>
   <Dialog.Content class="max-w-3xl">
     <Dialog.Header>Import Data - CSV / JSON / Excel</Dialog.Header>
-    <ImportTable />
+    <ImportTable {tableNames} />
   </Dialog.Content>
 </Dialog.Root>
