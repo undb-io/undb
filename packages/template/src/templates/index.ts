d56c76fe75f9d7ab6a43b0cf204f5dc695c594ab
Sophie <wanshufen0414@gmail.com>
2024-09-24 18:17:28 +0800

    chore: officeInventoryManagement template

--- packages/template/src/templates/index.ts
+++ packages/template/src/templates/index.ts
@@ -1,8 +1,9 @@
 import type { IBaseTemplateDTO } from "../dto"
 import { default as projectManagement } from "./projectManagement.base.json"
 import { default as todoList } from "./todoList.base.json"
+import { default as officeInventoryManagement } from "./officeInventoryManagement.base.json"
 import { default as eventPlaningList } from "./eventPlaning.base.json"
 
-const templates = { todoList, projectManagement, eventPlaningList } as Record<string, IBaseTemplateDTO>
+const templates = { todoList, projectManagement, officeInventoryManagement, eventPlaningList } as Record<string, IBaseTemplateDTO>
 
 export { templates }
