f7e0037d15bab4fe8909608b9b7aeb87739928e9
Sophie <wanshufen0414@gmail.com>
2024-09-24 18:50:55 +0800

    chore: add records to officeInventoryManagement template

--- packages/command-handlers/src/handlers/create-from-template.command-handler.ts
+++ packages/command-handlers/src/handlers/create-from-template.command-handler.ts
@@ -21,7 +21,7 @@ export class CreateFromTemplateCommandHandler
   async execute(command: CreateFromTemplateCommand): Promise<ICreateFromTemplateCommandOutput> {
     this.logger.info(`create from template command received: ${command.templateName}`)
 
-    const template = templates["projectManagement"]
+    const template = templates["officeInventoryManagement"]
 
     const spaceId = mustGetCurrentSpaceId()
     const result = await this.templateService.createBase(template, spaceId)
