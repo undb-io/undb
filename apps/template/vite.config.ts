e30aa9f4fdd3e3bc501d766e45bcdadb865eb1b1
nichenqin <nichenqin@hotmail.com>
2024-09-28 11:27:31 +0800

    fix: remove template

--- apps/template/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  server: {
-    port: 5735,
-  },
-  plugins: [sveltekit()],
-});
