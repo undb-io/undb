bb1be4d9fcf2f4ad8a7835d0bfde105bc68e6596
nichenqin <nichenqin@hotmail.com>
2024-10-11 10:34:45 +0800

    feat: aggregate config dialog

--- packages/queries/src/get-aggregates.query.ts
+++ packages/queries/src/get-aggregates.query.ts
@@ -1,5 +1,5 @@
 import { Query, type QueryProps } from "@undb/domain"
-import { aggregateResult, fieldId, getAggregatesDTO } from "@undb/table"
+import { aggregateResult, fieldId, getAggregatesDTO, type IViewAggregate } from "@undb/table"
 import { z } from "@undb/zod"
 
 export const getAggregatesQuery = getAggregatesDTO
@@ -13,10 +13,12 @@ export type IGetAggregatesOutput = z.infer<typeof getAggregatesOutput>
 export class GetAggregatesQuery extends Query implements IGetAggregatesQuery {
   public readonly tableId: string
   public readonly viewId?: string
+  public readonly aggregate?: IViewAggregate
 
   constructor(props: QueryProps<IGetAggregatesQuery>) {
     super()
     this.tableId = props.tableId
     this.viewId = props.viewId
+    this.aggregate = props.aggregate
   }
 }
