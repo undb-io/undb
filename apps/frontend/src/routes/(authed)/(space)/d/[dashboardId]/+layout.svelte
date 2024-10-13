56ac4f4769eaa23435188a09e3090c1dbd46a0be
nichenqin <nichenqin@hotmail.com>
2024-10-13 20:03:40 +0800

    feat: dashboard store

--- apps/frontend/src/routes/(authed)/(space)/d/[dashboardId]/+layout.svelte
+++ apps/frontend/src/routes/(authed)/(space)/d/[dashboardId]/+layout.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+  import { DashboardFactory } from "@undb/dashboard"
   import type { LayoutData } from "./$types"
+  import { setDashboard } from "$lib/store/dashboard.store"
+  import { writable } from "svelte/store"
 
   export let data: LayoutData
 
   $: dashboardStore = data.dashboardStore
   $: dashboard = $dashboardStore.data?.dashboard
+
+  $: if (dashboard) {
+    const dashboardDo = DashboardFactory.fromJSON(dashboard)
+    setDashboard(writable(dashboardDo))
+  }
 </script>
 
 <svelte:head>
