a402fe6ce8c7a6217486284a532eb7d96297b999
nichenqin <nichenqin@hotmail.com>
2024-10-24 18:46:27 +0800

    chore: add @tailwindcss/typography

--- apps/frontend/src/lib/components/blocks/field-value/long-text-field.svelte
+++ apps/frontend/src/lib/components/blocks/field-value/long-text-field.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import type { LongTextField } from "@undb/table"
+
   export let value: string | undefined = undefined
   export let placeholder: string | undefined = undefined
+
+  export let field: LongTextField
 </script>
 
 {#if !value}
@@ -8,5 +12,13 @@
     {placeholder || ""}
   </div>
 {:else}
-  <div class={$$restProps.class}>{value}</div>
+  <div class={$$restProps.class}>
+    {#if field.allowRichText}
+      <article class="prose-sm max-w-full">
+        {@html value}
+      </article>
+    {:else}
+      {value}
+    {/if}
+  </div>
 {/if}
