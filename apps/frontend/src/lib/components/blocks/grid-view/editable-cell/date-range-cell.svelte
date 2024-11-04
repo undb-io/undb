c14fefe9523099e4e45a5b8d3894424bdbb0d958
nichenqin <nichenqin@hotmail.com>
2024-11-04 14:53:42 +0800

    fix: fix styleg

--- apps/frontend/src/lib/components/blocks/grid-view/editable-cell/date-range-cell.svelte
+++ apps/frontend/src/lib/components/blocks/grid-view/editable-cell/date-range-cell.svelte
@@ -33,6 +33,7 @@
 
   export let field: DateRangeField
   export let isEditing = false
+  export let isSelected = false
   let formatter = field.formatter
 
   export let onValueChange: (
@@ -57,60 +58,70 @@
 </script>
 
 <div class={$$restProps.class}>
-  <Popover.Root openFocus bind:open>
-    <Popover.Trigger class="h-full w-full text-left outline-none ring-0">
-      {#if internalValue && internalValue.start}
-        {#if internalValue.end}
-          {formatter(internalValue.start.toDate())} - {formatter(internalValue.end.toDate())}
-        {:else}
-          {formatter(internalValue.start.toDate())}
+  {#if isEditing}
+    <Popover.Root openFocus bind:open>
+      <Popover.Trigger class="h-full w-full text-left outline-none ring-0">
+        {#if internalValue && internalValue.start}
+          {#if internalValue.end}
+            {formatter(internalValue.start.toDate())} - {formatter(internalValue.end.toDate())}
+          {:else}
+            {formatter(internalValue.start.toDate())}
+          {/if}
+        {:else if value?.[0]}
+          {formatter(new Date(value[0]))}
         {/if}
-      {:else if value?.[0]}
-        {formatter(new Date(value[0]))}
-      {/if}
-    </Popover.Trigger>
-    <Popover.Content class="w-auto p-0">
-      <RangeCalendar
-        onValueChange={(v) => {
-          const start = v.start?.toDate(getLocalTimeZone())
-          const end = v.end?.toDate(getLocalTimeZone())
+      </Popover.Trigger>
+      <Popover.Content class="w-auto p-0">
+        <RangeCalendar
+          onValueChange={(v) => {
+            const start = v.start?.toDate(getLocalTimeZone())
+            const end = v.end?.toDate(getLocalTimeZone())
 
-          const startDate = start ? start.toISOString() : undefined
-          const endDate = end ? end.toISOString() : undefined
+            const startDate = start ? start.toISOString() : undefined
+            const endDate = end ? end.toISOString() : undefined
 
-          value = [startDate, endDate]
-          onValueChange(value)
-          if (startDate && endDate) {
-            $updateCell.mutate({
-              tableId,
-              id: recordId,
-              values: { [field.id.value]: value },
-            })
-          }
-        }}
-        value={internalValue}
-        placeholder={internalValue?.start}
-        initialFocus
-        numberOfMonths={2}
-      />
-
-      <div class="border-t p-2">
-        <Button
-          variant="outline"
-          class="w-full"
-          on:click={() => {
-            value = undefined
+            value = [startDate, endDate]
             onValueChange(value)
-            $updateCell.mutate({
-              tableId,
-              id: recordId,
-              values: { [field.id.value]: value },
-            })
+            if (startDate && endDate) {
+              $updateCell.mutate({
+                tableId,
+                id: recordId,
+                values: { [field.id.value]: value },
+              })
+            }
           }}
-        >
-          Clear
-        </Button>
-      </div>
-    </Popover.Content>
-  </Popover.Root>
+          value={internalValue}
+          placeholder={internalValue?.start}
+          initialFocus
+          numberOfMonths={2}
+        />
+
+        <div class="border-t p-2">
+          <Button
+            variant="outline"
+            class="w-full"
+            on:click={() => {
+              value = undefined
+              onValueChange(value)
+              $updateCell.mutate({
+                tableId,
+                id: recordId,
+                values: { [field.id.value]: value },
+              })
+            }}
+          >
+            Clear
+          </Button>
+        </div>
+      </Popover.Content>
+    </Popover.Root>
+  {:else if internalValue && internalValue.start}
+    {#if internalValue.end}
+      {formatter(internalValue.start.toDate())} - {formatter(internalValue.end.toDate())}
+    {:else}
+      {formatter(internalValue.start.toDate())}
+    {/if}
+  {:else if value?.[0]}
+    {formatter(new Date(value[0]))}
+  {/if}
 </div>
