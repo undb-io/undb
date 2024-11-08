526c2dbb4d6552860b4bf3c6f2ca2895401b1ee0
nichenqin <nichenqin@hotmail.com>
2024-11-08 12:38:40 +0800

    chore: add remote template

--- packages/template/src/templates/index.ts
+++ packages/template/src/templates/index.ts
@@ -4,6 +4,7 @@ import { default as eventPlaningList } from "./eventPlaning.base.json"
 import { default as hr } from "./hr.base.json"
 import { default as officeInventoryManagement } from "./officeInventoryManagement.base.json"
 import { default as projectManagement } from "./projectManagement.base.json"
+import { default as remoteWorkManagement } from "./remoteWorkManagement.base.json"
 import { default as salesCrm } from "./salesCrm.base.json"
 import { default as socialMediaContent } from "./socialMediaContent.base.json"
 import { default as test } from "./test.base.json"
@@ -20,7 +21,7 @@ const templates = {
   socialMediaContent,
   hr,
   agileDevelopment,
+  remoteWorkManagement,
 } as const
 
 export { templates }
-
