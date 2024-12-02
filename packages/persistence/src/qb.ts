a35c2db3e4dda12a812e27b3c8d5be894e9c812e
nichenqin <nichenqin@hotmail.com>
2024-12-02 16:43:36 +0800

    feat: try to add sql.js

--- packages/persistence/src/qb.ts
+++ packages/persistence/src/qb.ts
@@ -1,41 +1,9 @@
 import type { Client } from "@libsql/client"
 import { LibsqlDialect } from "@libsql/kysely-libsql"
-import { createLogger } from "@undb/logger"
 import { Database as SqliteDatabase } from "bun:sqlite"
-import { Kysely, ParseJSONResultsPlugin, sql, type Dialect, type RawBuilder } from "kysely"
+import { Kysely, sql, type RawBuilder } from "kysely"
 import { BunSqliteDialect } from "kysely-bun-sqlite"
-import { type Database } from "./db"
-
-export function createQueryBuilderWithDialect(dialect: Dialect) {
-  const logger = createLogger("qb")
-
-  return new Kysely<Database>({
-    dialect,
-    plugins: [new ParseJSONResultsPlugin()],
-    log: (event) => {
-      if (event.level == "query") {
-        logger.debug(
-          {
-            query: event.query.sql,
-            params: event.query.parameters,
-            duration: event.queryDurationMillis,
-          },
-          "kysely.query",
-        )
-      } else if (event.level == "error") {
-        logger.error(
-          {
-            error: event.error,
-            query: event.query.sql,
-            params: event.query.parameters,
-            duration: event.queryDurationMillis,
-          },
-          "kysely.error",
-        )
-      }
-    },
-  })
-}
+import { createQueryBuilderWithDialect } from "./qb.util"
 
 export function createTursoQueryBuilder(client: Client) {
   return createQueryBuilderWithDialect(
