ad8cdbd8bc7b17cffbddd58b28e132478f7a962c
nichenqin <nichenqin@hotmail.com>
2024-12-16 09:57:10 +0800

    chore: add playground

--- apps/backend/src/modules/web/web.tsx
+++ apps/backend/src/modules/web/web.tsx
@@ -21,6 +21,7 @@ export class Web {
       .use(staticPlugin({ prefix: "/", assets: "dist" }))
       .use(staticPlugin({ prefix: "/assets", assets: "assets" }))
       .get("/", () => getIndex())
+      .get("/playground", () => getIndex())
       .get("/playground/*", () => getIndex())
       .get("/t/*", () => getIndex())
       .get("/dashboards/*", () => getIndex())
