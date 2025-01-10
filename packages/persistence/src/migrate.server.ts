8da5934f6a932d16f59a255a42737c9f5c0e9535
nichenqin <nichenqin@hotmail.com>
2025-01-10 10:45:17 +0800

    chore: upgrade dependencies

--- packages/persistence/src/migrate.server.ts
+++ packages/persistence/src/migrate.server.ts
@@ -6,12 +6,12 @@ import { drizzle as sqliteDrizzle } from "drizzle-orm/bun-sqlite"
 import { migrate as sqliteMigrate } from "drizzle-orm/bun-sqlite/migrator"
 import { drizzle as libsqlDrizzle } from "drizzle-orm/libsql"
 import { migrate as libsqlMigrate } from "drizzle-orm/libsql/migrator"
-import { SQLITE_CLIENT } from "./db-client"
+import { DATABASE_CLIENT } from "./db-client"
 import { DrizzleLogger } from "./db.logger"
 
 export async function dbMigrate() {
   if (env.UNDB_DB_PROVIDER === "sqlite" || !env.UNDB_DB_PROVIDER) {
-    const sqlite = container.resolve<Database>(SQLITE_CLIENT)
+    const sqlite = container.resolve<Database>(DATABASE_CLIENT)
     const db = sqliteDrizzle(sqlite, {
       logger: new DrizzleLogger(),
     })
@@ -20,7 +20,7 @@ export async function dbMigrate() {
     return
   }
 
-  const sqlite = container.resolve<Client>(SQLITE_CLIENT)
+  const sqlite = container.resolve<Client>(DATABASE_CLIENT)
   const db = libsqlDrizzle(sqlite, {
     logger: new DrizzleLogger(),
   })
