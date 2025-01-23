c717b0f9f2f8204ca2313ae77bf523efd93db74e
nichenqin <nichenqin@hotmail.com>
2025-01-23 10:34:52 +0800

    chore: use text bun lock

--- apps/backend/src/modules/file/storage/s3-object.storaget.ts
+++ apps/backend/src/modules/file/storage/s3-object.storaget.ts
@@ -1,8 +1,7 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import { singleton } from "@undb/di"
 import { env } from "@undb/env"
 import { IObjectStorage, IPresign, IPutObject } from "@undb/table"
+import { S3Client } from "bun"
 import { nanoid } from "nanoid"
 import * as path from "node:path"
 import { v7 } from "uuid"
@@ -10,10 +9,8 @@ import { v7 } from "uuid"
 @singleton()
 export class S3ObjectStorage implements IObjectStorage {
   s3Client = new S3Client({
-    credentials: {
-      accessKeyId: env.UNDB_S3_ACCESS_KEY_ID!,
-      secretAccessKey: env.UNDB_S3_SECRET_ACCESS_KEY!,
-    },
+    accessKeyId: env.UNDB_S3_ACCESS_KEY_ID!,
+    secretAccessKey: env.UNDB_S3_SECRET_ACCESS_KEY!,
     endpoint: env.UNDB_S3_STORAGE_ENDPOINT,
     region: env.UNDB_S3_STORAGE_REGION!,
   })
@@ -23,12 +20,8 @@ export class S3ObjectStorage implements IObjectStorage {
     const token = nanoid(16)
     const ext = path.extname(fileName)
     const name = id + ext
-    const command = new PutObjectCommand({
-      Bucket: env.UNDB_STORAGE_PRIVATE_BUCKET,
-      Key: name,
-    })
 
-    const url = await getSignedUrl(this.s3Client, command, { expiresIn: 24 * 60 * 60 })
+    const url = this.s3Client.presign(name, { bucket: env.UNDB_STORAGE_PRIVATE_BUCKET, expiresIn: 24 * 60 * 60 })
 
     return {
       id,
@@ -39,14 +32,7 @@ export class S3ObjectStorage implements IObjectStorage {
   }
 
   async getPreviewUrl(fileName: string): Promise<string> {
-    const command = new GetObjectCommand({
-      Bucket: env.UNDB_STORAGE_PRIVATE_BUCKET,
-      Key: fileName,
-    })
-
-    return getSignedUrl(this.s3Client, command, {
-      expiresIn: 24 * 60 * 60,
-    })
+    return this.s3Client.presign(fileName, { bucket: env.UNDB_STORAGE_PRIVATE_BUCKET, expiresIn: 24 * 60 * 60 })
   }
 
   put(buffer: Buffer, path: string, originalname: string, mimeType: string): Promise<IPutObject> {
